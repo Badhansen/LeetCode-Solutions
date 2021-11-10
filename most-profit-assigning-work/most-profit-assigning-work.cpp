16e6d5b2c952afd5fc056bea15a13bbe079a2271
Badhan Sen <badhan.cse@gmail.com>
2021-11-10 23:35:14 +0600

    Time: 116 ms (27.66%), Space: 44.7 MB (10.47%) - LeetHub

---
+++ most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        priority_queue<pair<int, int>> work;
+        multiset<int> workers;
+        int jobLen = difficulty.size();
+        int workerLen = worker.size();
+        int answer = 0;
+        
+        for(int i = 0; i < jobLen; i++){
+            work.push({profit[i], difficulty[i]});    
+        }
+        
+        for(int i = 0; i < workerLen; i++){
+            workers.insert(worker[i]);
+        }
+        
+        while(workers.size() && !work.empty()){
+            pair<int, int> wTop = work.top();
+            auto pos = workers.upper_bound(wTop.second);
+            
+            if(pos == workers.end()){
+                pos--;
+                if(*pos >= wTop.second){
+                    answer += wTop.first;
+                    workers.erase(pos);
+                }
+                else
+                    work.pop();
+            }
+            else{
+                answer += wTop.first;
+                workers.erase(pos);
+            }
+        }
+        
+        return answer;
+    }
+};
