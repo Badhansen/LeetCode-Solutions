2d6be29f49d88104cfa3639ac21daa33dac446af
Badhan Sen <badhan.cse@gmail.com>
2021-11-20 23:31:12 +0600

    Time: 16 ms (63.92%), Space: 8.3 MB (70.48%) - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,29 @@
+// @Author: KING-SEN
+// Programming Language Used: C++
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int result = 0;
+        int length = s.size();
+        unordered_map<int, int> seen;
+        int start = 0;
+        
+        for(int i = 0; i < length; i++){
+            if(seen.count(s[i])){
+                if(seen[s[i]] >= start){
+                    start = seen[s[i]];
+                }
+            }
+            result = max(result, i - start + 1);
+            seen[s[i]] = i + 1;
+        }
+        
+        return result;
+    }
+};
+
+// Time Complexity: O(N), Where N is length of the string 
+// Space Complexity: O(min(N, M)), where N is the length of the string and M is the size of the character set for HashMap
+// Space Complexity: O(m), where m is the size of the charset.
+
