368abb89e6baeb7848e9591669448aa5897fc938
Badhan Sen <badhan.cse@gmail.com>
2021-11-23 00:18:51 +0600

    Time: 160 ms (99.88%), Space: 85.7 MB (93.62%) - LeetHub

--- snapshot-array/snapshot-array.cpp
+++ snapshot-array/snapshot-array.cpp
@@ -1,39 +1,29 @@
-#define MAX_SNAP 50005
-
 class SnapshotArray {
 private:
-    map<int, map<int, int>> memo;
-    int snapIndex;
+    vector<pair<int, int>>changeList;
+    int snapId;
+    vector<int>snapList;
 public:
-    SnapshotArray(int length) {
-        for(int i = 0; i < length; i++){
-            map<int, int> mp;
-            mp[0] = 0;
-            memo[i] = mp;
-        }
-        snapIndex = 0;
-    }
-    
-    void set(int index, int val) {
-        memo[index][snapIndex] = val;
-    }
-    
-    int snap() {
-        snapIndex++;
-        return snapIndex - 1;
-    }
-    
-    int get(int index, int snap_id) {
-        auto pos = memo[index].upper_bound(snap_id);
-        pos--;
-        return pos->second;
-    }
-};
+	SnapshotArray(int length) {
+		snapId = 0;
+	}
 
-/**
- * Your SnapshotArray object will be instantiated and called as such:
- * SnapshotArray* obj = new SnapshotArray(length);
- * obj->set(index,val);
- * int param_2 = obj->snap();
- * int param_3 = obj->get(index,snap_id);
- */
+	void set(int index, int val) {
+		changeList.push_back(make_pair(index, val));
+	}
+
+	int snap() {
+		snapList.push_back(changeList.size());
+		return snapList.size()-1;
+	}
+
+	int get(int index, int snap_id) {
+		int start = snapList[snap_id];
+		for (int i = start - 1; i >= 0; i--) {
+			if (changeList[i].first == index) {
+				return changeList[i].second;
+			}
+		}
+		return 0;
+	}
+};
