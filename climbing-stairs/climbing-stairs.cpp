028d27a9b10ac54182d00f7d0603afcd8b4cefc1
Badhan Sen <badhan.cse@gmail.com>
2021-11-29 01:10:17 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (25.98%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dp;
+    int ways(int pos, int n){
+        if(pos > n) return 0;
+        if(pos == n) return 1;
+        
+        int& ret = dp[pos];
+        
+        if(ret != -1) 
+            return ret;
+        
+        ret = ways(pos + 1, n) + ways(pos + 2, n);
+        return ret;
+    }
+    int climbStairs(int n) {
+        dp.resize(n + 1, -1);
+        return ways(0, n);
+    }
+};
