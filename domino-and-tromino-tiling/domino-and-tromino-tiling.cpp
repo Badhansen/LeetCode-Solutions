ce32bac0a1ae850c25574b800f925b91da1e6f4d
Badhan Sen <badhan.cse@gmail.com>
2021-12-11 01:13:27 +0600

    Time: 0 ms (100.00%), Space: 6.6 MB (31.89%) - LeetHub

--- domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -3,26 +3,22 @@ const int mod = 1000000007;
 class Solution {
 private:
     vector<long> dp;
-    vector<long> memo;
 public:
-    long p(int n){
-        if(n == 2)
-            return 1ll;
-        if(memo[n] != -1)
-            return memo[n];
-        return memo[n] = (p(n - 1) + f(n - 2)) % mod; 
-    }
     long f(int n){
-        if(n == 1 || n == 2)
-            return n;
         if(dp[n] != -1)
             return dp[n];
-        
-        return dp[n] = (f(n - 1) + f(n - 2) + 2 * p(n - 1)) % mod;
+        return dp[n] = (2 * f(n - 1) + f(n - 3)) % mod;
     }
     int numTilings(int n) {
-        memo.resize(n + 1, -1);
+        if(n == 1 || n == 2) 
+            return n;
+        
         dp.resize(n + 1, -1);
+        
+        dp[0] = dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        
         return (int)f(n);
     }
 };
