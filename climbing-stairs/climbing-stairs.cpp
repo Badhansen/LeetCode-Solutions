b7b23730eb1dfc23be6be6a3c42b6109d691e6eb
Badhan Sen <badhan.cse@gmail.com>
2021-12-17 20:50:16 +0600

    Time: 0 ms (100.00%), Space: 6.3 MB (13.73%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
-public:
+private:
     vector<int> dp;
+public:
     int ways(int pos, int n){
         if(pos > n) return 0;
         if(pos == n) return 1;
-        
         int& ret = dp[pos];
-        
         if(ret != -1) 
             return ret;
-        
         ret = ways(pos + 1, n) + ways(pos + 2, n);
         return ret;
     }
