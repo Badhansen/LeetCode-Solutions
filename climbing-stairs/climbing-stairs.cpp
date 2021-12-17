5766fee8b28eb8c85fe9bbae30349c70e2b438c7
Badhan Sen <badhan.cse@gmail.com>
2021-12-18 01:46:34 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (81.52%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,17 @@
 class Solution {
-private:
-    vector<int> dp;
 public:
     int climbStairs(int n) {
-        dp.resize(n + 1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
+        if(n == 1) 
+            return n;
+        int first = 1, second = 1, third;
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            third = first + second;
+            first = second;
+            second = third;
         }
-        return dp[n];
+        return third;
     }
 };
 // Assume that N = number of elements
 // Time Complexity: O(N)
-// Space Complexity: O(N)
+// Space Complexity: O(1)
