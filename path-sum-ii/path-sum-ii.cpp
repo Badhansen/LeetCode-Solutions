58bdd519c67b9b83aa54894acbfb4787913cec49
Badhan Sen <badhan.cse@gmail.com>
2021-12-20 23:09:31 +0600

    Time: 8 ms (80.04%), Space: 19.9 MB (60.05%) - LeetHub

--- path-sum-ii/path-sum-ii.cpp
+++ path-sum-ii/path-sum-ii.cpp
@@ -1,34 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void solve(TreeNode* root, int sum, int targetSum, vector<int> path, vector<vector<int>>& answer){
-        if(root == NULL) return;
-        
-        path.push_back(root->val);
-        
-        if(root->left == NULL && root->right == NULL){
-            if(sum + root->val == targetSum){
-                answer.push_back(path);
-            }
-            return;
-        }
-        solve(root->left, sum + root->val, targetSum, path, answer);
-        solve(root->right, sum + root->val, targetSum, path, answer);
-    }
+    vector<vector<int>> ans;
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> answer;
         vector<int> path;
-        solve(root, 0, targetSum, path, answer);
-        return answer;
+        dfs(root, targetSum, path);
+        return ans;
+    }
+    void dfs(TreeNode* root, int targetSum, vector<int>& path) {
+        if (root == nullptr) return;
+        path.push_back(root->val);
+        targetSum -= root->val;
+        if (root->left == nullptr && root->right == nullptr) { // Is leaf node
+            if (targetSum == 0) // Found valid path
+                ans.push_back(path);
+        } else {
+            dfs(root->left, targetSum, path);
+            dfs(root->right, targetSum, path);
+        }
+        path.pop_back(); // backtrack
     }
 };
