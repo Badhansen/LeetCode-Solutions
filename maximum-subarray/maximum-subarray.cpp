65b506f84a6b1ee3709f02457f621632910badea
Badhan Sen <badhan.cse@gmail.com>
2021-12-20 21:07:02 +0600

    Time: 140 ms (26.71%), Space: 67.7 MB (91.04%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,16 +1,38 @@
+// @Author: KING-SEN
+// Programmig Language Used: C++
+
 class Solution {
 public:
+    int findBestSubArray(vector<int>& nums, int start, int end){
+        if(start > end)
+            return INT_MIN;
+        int mid = (start + end) / 2;
+        int currSum = 0;
+        int leftSum = 0;
+        int rightSum = 0;
+        for(int i = mid - 1; i >= start; i--){
+            currSum += nums[i];
+            leftSum = max(leftSum, currSum);
+        }
+        
+        currSum = 0;
+        
+        for(int i = mid + 1; i <= end; i++){
+            currSum += nums[i];
+            rightSum = max(rightSum, currSum);
+        }
+        
+        int bestSum = nums[mid] + leftSum + rightSum;
+        int leftHalf = findBestSubArray(nums, start, mid - 1);
+        int rightHalf = findBestSubArray(nums, mid + 1, end);
+        
+        return max({bestSum, leftHalf, rightHalf});
+    }
     int maxSubArray(vector<int>& nums) {
         int len = nums.size();
-        int sum = 0;
-        int maxSum = nums[0];
-        for(int i = 0; i < len; i++){
-            sum += nums[i];
-            if(sum > maxSum)
-                maxSum = sum;
-            if(sum < 0)
-                sum = 0;
-        }
-        return maxSum;
+        return findBestSubArray(nums, 0, len - 1);
     }
 };
+
+// Time Complexity: O(N * log N) here N is the length of the array
+// Space complexity: O(log N)
