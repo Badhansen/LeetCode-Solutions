ed294fd0f404efe1619379d3f85a564c5b4fefcf
Badhan Sen <badhan.cse@gmail.com>
2021-12-23 20:55:11 +0600

    Time: 116 ms (30.77%), Space: 44.3 MB (6.99%) - LeetHub

--- minimum-height-trees/minimum-height-trees.cpp
+++ minimum-height-trees/minimum-height-trees.cpp
@@ -1,69 +1,68 @@
 class Solution {
 private:
     vector<vector<int>> graph;
+    vector<int> vis;
+    int height;
+    int vertex;
 public:
-    int bfs(int src, int n){
-        vector<int> dis(n, 0);
-        vector<int> vis(n, 0);
-        queue<int> q;
-        q.push(src);
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            vis[curr] = 1;
-            for(int v : graph[curr]){
-                if(vis[v] == 0){
-                    dis[v] = dis[curr] + 1;
-                    q.push(v);
-                }
+    void dfs(int u, int level){
+        vis[u] = 1;
+        for(auto &v : graph[u]){
+            if(vis[v] == 0){
+                dfs(v, level + 1);
             }
         }
-        int maxDis = INT_MIN, vertex;
-        for(int i = 0; i < n; i++){
-            if(dis[i] > maxDis){
-                maxDis = dis[i];
-                vertex = i;
+        if(level > height){
+            height = level;
+            vertex = u;
+        }
+        vis[u] = 0;
+    }
+    void pathFind(int u, int &target, vector<int>& path, vector<int>& ans){
+        if(u == target){
+            ans = path;
+            return;
+        }
+        vis[u] = 1;
+        for(auto &v : graph[u]){
+            if(vis[v] == 0){
+                path.push_back(v);
+                pathFind(v, target, path, ans);
+                path.pop_back();
             }
         }
-        return vertex;
     }
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         graph.resize(n);
+        vis.resize(n);
+        height = 0;
+        
         for(auto &e : edges){
             graph[e[0]].push_back(e[1]);
             graph[e[1]].push_back(e[0]);
         }
-        int start = bfs(0, n);
-        int end = bfs(start, n);
-        vector<int> parent(n);
-        vector<int> vis(n, 0);
-        queue<int> q;
-        parent[start] = -1;
-        q.push(start);
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            vis[curr] = 1;
-            for(int v : graph[curr]){
-                if(vis[v] == 0){
-                    parent[v] = curr;
-                    q.push(v);
-                }
-            }
-        }
+        
+        dfs(0, 0);
+        
+        int start = vertex;
+        height = 0;
+        
+        dfs(start, 0);
+        
+        int end = vertex;
+        
         vector<int> path;
-        while(end != -1){
-            path.push_back(end);
-            end = parent[end];
-        }
-        if(path.size() & 1){
-            return {path[path.size() / 2]};
+        vector<int> ans;
+        
+        path.push_back(start);
+        
+        pathFind(start, end, path, ans);
+        
+        if(ans.size() & 1){
+            return {ans[ans.size() / 2]};
         }
         else{
-            return {path[path.size() / 2], path[path.size() / 2 - 1]};
+            return {ans[ans.size() / 2], ans[ans.size() / 2 - 1]};
         }
     }
 };
-
-// Time Complexity: O(N), only 2 DFS calls is made each requiring O(N) time.
-// Space Complexity: O(N)
