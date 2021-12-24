3243b06940154e615de499274d2584c80470493b
Badhan Sen <badhan.cse@gmail.com>
2021-12-24 15:15:15 +0600

    Time: 0 ms (100.00%), Space: 11.9 MB (93.27%) - LeetHub

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int cnt = 2;
-        do{
-            cnt--;
-        }while(cnt && next_permutation(nums.begin(), nums.end()));
+        next_permutation(nums.begin(), nums.end());
     }
 };
