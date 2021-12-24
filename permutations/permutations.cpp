d31c01b219bf2e747375e456cef732bc6dbc327d
Badhan Sen <badhan.cse@gmail.com>
2021-12-24 16:44:25 +0600

    Time: 0 ms (100.00%), Space: 7.5 MB (98.74%) - LeetHub

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-    void solve(int pos, int n, vector<int>& nums, vector<vector<int>>& ans){
-        if(pos == n){
-            ans.push_back(nums);
-            return;
-        }
-        for(int i = pos; i < n; i++){
-            swap(nums[pos], nums[i]);
-            solve(pos + 1, n, nums, ans);
-            swap(nums[pos], nums[i]);
-        }
-    }
     vector<vector<int>> permute(vector<int>& nums) {
-        int len = nums.size();
-        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        solve(0, len, nums, ans);
+        sort(nums.begin(), nums.end());
+        do{
+            ans.push_back(nums);
+        }while(next_permutation(nums.begin(), nums.end()));
         return ans;
     }
 };
