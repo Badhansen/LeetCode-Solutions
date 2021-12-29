5c312e216f39b31488be609c6b74bf4f80a75352
Badhan Sen <badhan.cse@gmail.com>
2021-12-29 19:30:06 +0600

    Time: 3 ms (6.41%), Space: 6 MB (43.09%) - LeetHub

--- excel-sheet-column-title/excel-sheet-column-title.cpp
+++ excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     string convertToTitle(int n) {
-        if(n == 0)
-            return "";
-        return convertToTitle((n - 1) / 26) + char((n - 1) % 26 + 'A');
+        string ans;
+        while(n--){
+            ans.push_back(char(n % 26 + 'A'));
+            n /= 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
