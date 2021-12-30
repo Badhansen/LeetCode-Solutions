c97b7566ee326e54dd5d9deb8b318a85143a1ee7
Badhan Sen <badhan.cse@gmail.com>
2021-12-30 21:55:50 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (83.19%) - LeetHub

--- smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
+++ smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
-        int num;
-        int len = 1;
-        int rem = 1;
-        unordered_set<int> seen;
-        while(rem % k != 0){
-            num = rem * 10 + 1;
-            rem = num % k;
-            if(seen.find(rem) != seen.end())
-                return -1;
-            else 
-                seen.insert(rem);
-            len++;
+        int rem = 0;
+        for(int len = 1; len <= k; len++){
+            rem = (rem * 10 + 1) % k;
+            if(rem == 0)
+                return len;
         }
-        return len;
+        return -1;
     }
 };
 
-// Time: O(K)
-// Space: O(K)
