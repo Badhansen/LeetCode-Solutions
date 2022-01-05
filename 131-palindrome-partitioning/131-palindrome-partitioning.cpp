2a765f836701a803231c311498d244854fbc0858
Badhan Sen <badhan.cse@gmail.com>
2022-01-05 22:20:21 +0600

    Time: 215 ms (24.20%), Space: 75.8 MB (39.85%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,30 +1,27 @@
-// Solution by other 
 class Solution {
+private:
+    vector<string> res;
+    vector<vector<string>> ans;
+    vector<vector<bool>> dp;
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> currentList;
-        dfs(result, s, 0, currentList);
-        return result;
-    }
-
-    void dfs(vector<vector<string>> &result, string &s, int start, vector<string> &currentList) {
-        if (start >= s.length()) result.push_back(currentList);
-        for (int end = start; end < s.length(); end++) {
-            if (isPalindrome(s, start, end)) {
-                // add current substring in the currentList
-                currentList.push_back(s.substr(start, end - start + 1));
-                dfs(result, s, end + 1, currentList);
-                // backtrack and remove the current substring from currentList
-                currentList.pop_back();
+    void dfs(string& s, int start, vector<string>& res){
+        if(start >= s.size()){
+            ans.push_back(res);
+            return;
+        }
+        for(int end = start; end < s.size(); end++){
+            if(s[start] == s[end] && (end - start <= 2 || dp[start + 1][end - 1])){
+                dp[start][end] = true;
+                res.push_back(s.substr(start, end - start + 1));
+                dfs(s, end + 1, res);
+                res.pop_back();
             }
         }
     }
-
-    bool isPalindrome(string &s, int low, int high) {
-        while (low < high) {
-            if (s[low++] != s[high--]) return false;
-        }
-        return true;
+    vector<vector<string>> partition(string s) {
+        int len = s.size();
+        dp.resize(len, vector<bool>(len, false));
+        dfs(s, 0, res);
+        return ans;
     }
 };
