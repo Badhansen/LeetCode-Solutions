0a929e248ba6d9cc29df28d5ee02b6726a2f55d1
Badhan Sen <badhan.cse@gmail.com>
2022-01-05 22:50:53 +0600

    Time: 113 ms (64.48%), Space: 49.4 MB (79.56%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,27 +1,26 @@
 class Solution {
-private:
-    vector<string> res;
-    vector<vector<string>> ans;
-    vector<vector<bool>> dp;
 public:
-    void dfs(string& s, int start, vector<string>& res){
+    void dfs(string& s, int start, vector<string>& res, vector<vector<string>>& ans, vector<vector<bool>>& dp){
         if(start >= s.size()){
-            ans.push_back(res);
+            ans.emplace_back(res);
             return;
         }
         for(int end = start; end < s.size(); end++){
             if(s[start] == s[end] && (end - start <= 2 || dp[start + 1][end - 1])){
                 dp[start][end] = true;
                 res.emplace_back(s.substr(start, end - start + 1));
-                dfs(s, end + 1, res);
+                dfs(s, end + 1, res, ans, dp);
                 res.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         int len = s.size();
-        dp.resize(len, vector<bool>(len, false));
-        dfs(s, 0, res);
+        vector<string> res;
+        vector<vector<string>> ans;
+        vector<vector<bool>> dp(len, vector<bool>(len, false));
+        
+        dfs(s, 0, res, ans, dp);
         return ans;
     }
 };
