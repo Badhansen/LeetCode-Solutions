ab6877748897376b62abe91803d57479cad85d3d
Badhan Sen <badhan.cse@gmail.com>
2022-01-07 02:20:15 +0600

    Time: 0 ms (100.00%), Space: 6.1 MB (23.25%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.cpp
+++ 670-maximum-swap/670-maximum-swap.cpp
@@ -12,9 +12,7 @@ public:
         for(int i = 0; i < str.size(); i++){
             for(int j = str.size() - 1; j > i; j--){
                 swap(str[i], str[j]);
-                stringstream temp(str);
-                int mNum;
-                temp >> mNum;
+                int mNum = stoi(str);
                 answer = max(answer, mNum);
                 swap(str[i], str[j]);
             }
