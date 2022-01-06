5f3777575d327a6d3c0df0ceadbc5af2c6c1e846
Badhan Sen <badhan.cse@gmail.com>
2022-01-06 21:35:23 +0600

    Time: 0 ms (100.00%), Space: 7 MB (69.27%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -18,6 +18,13 @@ public:
         return go(a->next, b->next->next);
     }
     ListNode* middleNode(ListNode* head) {
-        return go(head, head);
+        ListNode* fast, *slow;
+        fast = head;
+        slow = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
     }
 };
