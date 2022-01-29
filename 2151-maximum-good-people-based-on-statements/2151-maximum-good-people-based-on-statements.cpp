f4b3750983c9034a71c7514d300d5a1eaa6b2a7f
Badhan Sen <badhan.cse@gmail.com>
2022-01-30 00:52:53 +0600

    Time: 249 ms (57.19%), Space: 8.5 MB (24.56%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -1,24 +1,36 @@
 class Solution {
+private:
+    int n;
+    int ans;
 public:
-    int maximumGood(vector<vector<int>>& statements) {
-        int len = statements.size();
-        int total = 1 << len, ans = 0;
-        for(int t = 0; t < total; t++){
-            bool flag = true;
-            for(int i = 0; i < len && flag; i++){
-                bool ok = t & (1 << i);
-                if(ok){
-                    for(int j = 0; j < len; j++){
-                        bool mask = t & (1 << j);
-                        if(statements[i][j] != 2 && statements[i][j] != mask){
-                            flag = false;
-                            break;
-                        }
-                    }
+    bool valid(vector<vector<int>>& statements, vector<int>& res){
+        for(int i = 0; i < n; i++){
+            if(res[i] == 1){
+                for(int j = 0; j < n; j++){
+                    if(statements[i][j] != 2 && statements[i][j] != res[j])
+                        return false;
                 }
             }
-            if(flag) ans = max(ans, __builtin_popcount(t));
         }
+        return true;
+    }
+    void dfs(int pos, int count, vector<vector<int>>& statements, vector<int>& res){
+        if(pos == n){
+            if(valid(statements, res))
+                ans = max(ans, count);
+            return;
+        }
+        res.push_back(0);
+        dfs(pos + 1, count, statements, res);
+        res.back() = 1;
+        dfs(pos + 1, count + 1, statements, res);
+        res.pop_back();
+        
+    }
+    int maximumGood(vector<vector<int>>& statements) {
+        n = statements.size();
+        vector<int> res;
+        dfs(0, 0, statements, res);
         return ans;
     }
 };
