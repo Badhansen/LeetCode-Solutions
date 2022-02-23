cf4e28aecaca872ff0928e2708db49afb05f993e
Badhan Sen <badhan.cse@gmail.com>
2022-02-24 00:54:43 +0600

    Time: 244 ms (60.51%), Space: 10.6 MB (14.13%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
+    int solve(int curr, vector<int>& dp, vector<int>& nums){
+        if(curr == 0){
+            return 1;
+        }
+        
+        int &ret = dp[curr];
+        
+        if(ret != -1){
+            return ret;
+        }
+        
+        ret = 1;
+        
+        for(int i = curr - 1; i >= 0; i--){
+            if(nums[curr] > nums[i]){
+                ret = max(ret, 1 + solve(i, dp, nums));
+            }
+        }
+        
+        return ret;
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n + 1, 1);
+        vector<int> dp(n + 1, -1);
         int ans = 0;
         
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
-            ans = max(ans, dp[i]);
+            ans = max(ans, solve(i, dp, nums));
         }
-        
+
         return ans;
     }
 };
