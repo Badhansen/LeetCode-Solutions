6a145f9f4c7ab92fb4e8d5ed40a673b6605d95e4
Badhan Sen <badhan.cse@gmail.com>
2022-02-24 01:23:33 +0600

    Time: 10 ms (98.56%), Space: 11 MB (77.06%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,4 +1,3 @@
-/*
 class Solution {
 private:
     vector<int> dp;
@@ -32,31 +31,3 @@ public:
         return solve(0, target, nums);
     }
 };
-*/
-class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
-        int length = nums.size();
-        int sum = 0;
-        
-        for(auto num : nums)
-            sum += num;
-        
-        if(sum & 1) return false;
-        
-        sum /= 2;
-        vector<bool> dp(sum + 1, false);
-        
-        dp[0] = true;
-        
-        for(int i = 0; i < length; i++){
-            for(int s = sum; s >= nums[i]; s--){
-                dp[s] = dp[s] | dp[s - nums[i]];
-            }
-        }
-        
-        return dp[sum];
-    }
-};
-
-
