2b47084fa445c052617aacdfbbf64f49c89ac67e
Badhan Sen <badhan.cse@gmail.com>
2022-03-06 21:32:07 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (82.70%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,14 +1,10 @@
-const int MOD = 1e9 + 7;
-
 class Solution {
 public:
     int countOrders(int n) {
-        long ans = 1;
-        for(int i = 1; i <= n; i++){
-            ans = ans * i;
-            ans = ans * (2 * i - 1);
-            ans %= MOD;
+        long res = 1, mod = 1e9 + 7;
+        for(int i = 1; i <= n; ++i){
+            res = res * (i * 2 - 1) * i % mod;
         }
-        return ans;
+        return res;
     }
 };
