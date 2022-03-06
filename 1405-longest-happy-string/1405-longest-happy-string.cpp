45bab346c0cc71348cff570aee94d92bf8f4def9
Badhan Sen <badhan.cse@gmail.com>
2022-03-07 02:20:58 +0600

    Time: 0 ms (100.00%), Space: 6.1 MB (45.01%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -0,0 +1,46 @@
+// @Author: KING-SEN
+
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>> qt;
+        if(a != 0){
+            qt.push({a, 'a'});
+        }
+        if(b != 0){
+            qt.push({b, 'b'});
+        }
+        if(c != 0){
+            qt.push({c, 'c'});
+        }
+        string ans;
+        int size = 0;
+        while(!qt.empty()){
+            auto top = qt.top();
+            qt.pop();
+            if(size > 1 && ans[size - 1] == top.second && ans[size - 2] == top.second){
+                if(qt.empty()){
+                    return ans;
+                }
+                auto temp = qt.top();
+                qt.pop();
+                ans.push_back(temp.second);
+                size++;
+                temp.first--;
+                if(temp.first > 0){
+                    qt.push(temp);
+                }
+                qt.push(top);
+            }
+            else{
+                ans.push_back(top.second);
+                size++;
+                top.first--;
+                if(top.first > 0){
+                    qt.push(top);
+                }
+            }
+        }
+        return ans;
+    }
+};
