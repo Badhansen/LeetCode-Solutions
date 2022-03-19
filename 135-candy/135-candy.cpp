783972c897caa7c5830c3008d8d1a8d1496c28a1
Badhan Sen <badhan.cse@gmail.com>
2022-03-19 23:49:25 +0600

    Time: 64 ms (8.35%), Space: 25.5 MB (5.07%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int len = ratings.size();
-        vector<int> candy(len, 1);
-        for(int i = 0; i < len - 1; i++){
-            if(ratings[i] < ratings[i + 1]){
-                candy[i + 1] = candy[i] + 1;
+        int n = ratings.size();
+        vector<int> result(n, 1); // first rule
+        set<pair<int,int>> Children;
+        // n log(n)
+        for(int i = 0; i < n; i++){
+        Children.insert({ratings[i], i});
+        }
+        while(!Children.empty()){
+            pair<int,int> u = *Children.begin();
+            Children.erase(Children.begin());
+            int left = u.second - 1, right = u.second + 1;
+            int mx = 0;
+            // rule 2 for left neighour
+            if(left >= 0 && ratings[left] < ratings[u.second]){
+              mx = max(mx, result[left] + 1);
             }
-        }
-        for(int i = len - 1; i > 0; i--){
-            if(ratings[i - 1] > ratings[i]){
-                candy[i - 1] = max(candy[i - 1], candy[i] + 1);
+            // rule for right neighour 
+            if(right < n && ratings[right] < ratings[u.second]){
+              mx = max(mx, result[right] + 1);
             }
+            result[u.second] = max(result[u.second], mx);
+            }
+            int answer = 0;
+            for(int i = 0; i < n; i++){
+            answer += result[i];
         }
-        int ans = 0;
-        for(auto el : candy){
-            ans += el;
-        }
-        
-        return ans;
+        return answer;
     }
 };
