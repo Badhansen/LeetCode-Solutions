22026c2a701520e9d7aac7f77c0be4fe3770ca8e
Badhan Sen <badhan.cse@gmail.com>
2022-03-20 22:52:27 +0600

    Time: 261 ms (15.36%), Space: 111.6 MB (72.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        vector<int> countA(7), countB(7), same(7);
         int n = tops.size();
-        for(int i = 0; i < n; i++){
-            countA[tops[i]]++;
-            countB[bottoms[i]]++;
-            if(tops[i] == bottoms[i]){
-                same[tops[i]]++;
+        int rotationA = 0, rotationB = 0;
+        for(int i = 0; i < n && (tops[i] == tops[0] || bottoms[i] == tops[0]); i++){
+            if(tops[i] != tops[0]){
+                rotationA++;
+            }
+            if(bottoms[i] != tops[0]){
+                rotationB++;
+            }
+            if(i == n - 1){
+                return min(rotationA, rotationB);
             }
         }
-        for(int i = 1; i < 7; i++){
-            if (countA[i] + countB[i] - same[i] == n){
-                return n - max(countA[i], countB[i]);
+        rotationA = 0, rotationB = 0;
+        for(int i = 0; i < n && (tops[i] == bottoms[0] || bottoms[i] == bottoms[0]); i++){
+            if(bottoms[i] != bottoms[0]){
+                rotationA++;
+            }
+            if(tops[i] != bottoms[0]){
+                rotationB++;
+            }
+            if(i == n - 1){
+                return min(rotationA, rotationB);
             }
         }
         return -1;
