4558047d21afebe01ffe7b5592936aeac75f155e
Badhan Sen <badhan.cse@gmail.com>
2022-03-25 00:48:31 +0600

    Time: 1962 ms (5.00%), Space: 11.9 MB (51.37%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,33 +1,41 @@
+// @Author: KING-SEN
+// Programming Language Used: C++
+
 class Solution {
-private:
-    int dp[1001][1001];
 public:
-    int solve(int x, int y, string& s){
-        if(x >= y){
-            return 1;
-        }
+    int dp[1001][1001];
+    int solve(int x, int y, string& S)
+    {  
+        if(x > y) return 0;
         int &ret = dp[x][y];
-        if(ret != -1){
+        if(ret != -1){ 
             return ret;
         }
-        if(s[x] == s[y]){
-            ret = solve(x + 1, y - 1, s);
+        if(S[x] == S[y]){
+            int val = solve(x + 1, y - 1, S);
+            if(val >= (y - 1) - (x + 1) + 1){
+                ret = 2 - (x == y) + val;
+            }
         }
-        else{
-            ret = 0;
-        }
+        ret = max({ret, solve(x + 1, y, S), solve(x, y - 1, S)});
         return ret;
     }
     string longestPalindrome(string s) {
-        memset(dp, -1, sizeof dp); 
-        int start, maxx = 0;
+        memset(dp, -1, sizeof dp);
+        int ret = solve(0, s.size() - 1, s);
+        int startIndex;
+        
         for(int i = 0; i < s.size(); i++){
-            for(int j = s.size() - 1; j >= i; j--){
-                if(solve(i, j, s) && j - i + 1 > maxx){
-                    start = i, maxx = j - i + 1;
+            for(int j = 0; j < s.size(); j++){
+                if(ret == dp[i][j]){
+                    startIndex = j - ret + 1;
+                    break;
                 }
             }
         }
-        return s.substr(start, maxx);
+        return s.substr(startIndex, ret);
     }
 };
+
+// Time Complexity: O(N^2), where N is the lenght of the string
+// Space complexity: O(N^2), we used 2d vector array to store the intermediate result
