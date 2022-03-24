3df039340178612feeefcfe204cf50a58e355ae7
Badhan Sen <badhan.cse@gmail.com>
2022-03-25 01:18:44 +0600

    Time: 46 ms (99.87%), Space: 22 MB (79.17%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -2,31 +2,26 @@
 
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int pos, int bflag, string& s){
-        if(pos >= s.size()){
-            return 0;
-        }
-        
-        int &ret = dp[pos][bflag];
-        if(ret != -1){
-            return ret;
-        }
-        ret = INT_MAX;
-        if(bflag){
-            ret = (s[pos] == 'a') + solve(pos + 1, bflag, s);   
-        }
-        else{
-            if(s[pos] == 'b'){
-                ret = solve(pos + 1, 1, s);
-            }
-            ret = min(ret, (s[pos] == 'b') + solve(pos + 1, 0, s));
-        }
-        return ret;
-    }
     int minimumDeletions(string s) {
-        int len = s.size();
-        dp.resize(len + 1, vector<int> (2, -1));
-        return solve(0, 0, s);
+        int n = s.length();
+        int count_b = 0;
+        int count_a = 0;
+        int res_b_a = 0;
+        for (int i=0; i<n; ++i){
+            if (s[i] == 'a'){
+                ++count_a;
+            } else {
+                ++count_b;
+            }
+            res_b_a = min(
+                res_b_a,
+                count_b - count_a
+            );
+        }
+        return res_b_a + count_a;
     }
 };
+
+// https://stackoverflow.com/questions/31162367/significance-of-ios-basesync-with-stdiofalse-cin-tienull
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);
