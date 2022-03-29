bcb2e6e0fcd4508e80861fe73cc8e806d14054e5
Badhan Sen <badhan.cse@gmail.com>
2022-03-29 19:25:22 +0600

    Time: 214 ms (27.87%), Space: 61.3 MB (61.36%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -0,0 +1,20 @@
+// @Author: KING-SEN
+// Programming Language Used: C++
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int len = nums.size();
+        for(int i = 0; i < len; i++){
+            int id = abs(nums[i]);
+            if(nums[id] < 0){
+                return id;
+            }
+            nums[id] *= -1;
+        }
+        return 0;
+    }
+};
+
+// Time Complexity: O(N), Here N = length of the array
+// Space Complexity: O(1) 
