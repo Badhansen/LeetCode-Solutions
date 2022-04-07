827fcbca6f3c7e4a71d537fa124579b9d7408d06
Badhan Sen <badhan.cse@gmail.com>
2022-04-07 21:29:16 +0600

    Time: 1156 ms (24.75%), Space: 10.8 MB (20.27%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
-    int dp[3005][5][305];
-    int solve(int pos, int count, int sum, vector<int>& arr){
-        if(pos >= arr.size()){
-            if(sum == 0 && count == 3){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-        if(count > 3 || sum < 0){
-            return 0;
-        }
-        if(count == 3 && sum == 0){
-            return 1;
-        }
-        int &ret = dp[pos][count][sum];
-        if(ret != -1){
-            return ret;
-        }
-        ret = 0;
-        ret = (solve(pos + 1, count + 1, sum - arr[pos], arr) % mod + 
-            solve(pos + 1, count, sum, arr) % mod) % mod;
-        return ret;
-        
-    }
     int threeSumMulti(vector<int>& arr, int target) {
-        memset(dp, -1, sizeof dp);
-        return solve(0, 0, target, arr);
+        int len = arr.size(), ans = 0;
+        unordered_map<int, int> count;
+
+        for(int i = 0; i < len; i++) {
+            ans = (ans + count[target - arr[i]]) % mod;
+            for(int j = 0; j < i; j++){
+                count[arr[i] + arr[j]]++;
+            }
+        }
+        return ans;
     }
 };
