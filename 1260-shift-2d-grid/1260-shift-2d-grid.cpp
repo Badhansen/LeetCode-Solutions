8637ee2938bcf1cc8c00776c019d24df6d8c6d9a
Badhan Sen <badhan.cse@gmail.com>
2022-04-11 15:04:04 +0600

    Time: 22 ms (92.02%), Space: 15 MB (19.94%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int row = grid.size(), col = grid[0].size();
-        deque<int> res;
+        vector<int> res;
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 res.push_back(grid[i][j]);
             }
         }
         k = k % (row * col);
-        while(k--){
-            int item = res.back();
-            res.pop_back();
-            res.push_front(item);
-        }
-        for(int i = 0; i < row; i++){
+        reverse(res.begin(), res.end());
+        rotate(res.begin(), res.begin() + k, res.end());
+        reverse(res.begin(), res.end());
+        for(int i = 0, idx = 0; i < row; i++){
             for(int j = 0; j < col; j++){
-                grid[i][j] = res.front();
-                res.pop_front();
+                grid[i][j] = res[idx++];
             }
         }
         return grid;
