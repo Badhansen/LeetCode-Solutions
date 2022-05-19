bac4f5f9686fb97eeef2cf0eb56e6935c6ff9cd2
Badhan Sen <badhan.cse@gmail.com>
2022-05-20 03:00:52 +0600

    Time: 53 ms (73.92%), Space: 15.2 MB (85.92%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -2,52 +2,32 @@ class Solution {
 private:
     int dirx[4] = {0, 0, 1, -1};
     int diry[4] = {1, -1, 0, 0};
-    vector<vector<int>> graph;
-    vector<int> indegree, dist;
-    vector<bool> visited;
+    int dp[201][201];
 public:
-    int dfs(int u){
-        visited[u] = true;
-        dist[u] = 1;
-        for(int v : graph[u]){
-            if(!visited[v]){
-                dist[u] = max(dist[u], 1 + dfs(v));
-            }
-            else{
-                dist[u] = max(dist[u], 1 + dist[v]);
+    int solve(int r, int c, vector<vector<int>>& matrix){
+        if(r == matrix.size() || c == matrix[0].size()){
+            return 0;
+        }
+        int& ret = dp[r][c];
+        if(ret != -1){
+            return ret;
+        }
+        ret = 1;
+        for(int d = 0; d < 4; d++){
+            int nr = dirx[d] + r;
+            int nc = diry[d] + c;
+            if(nr >= 0 && nr < matrix.size() && nc >= 0 && nc < matrix[0].size() && matrix[r][c] < matrix[nr][nc]){
+                ret = max(ret, 1 + solve(nr, nc, matrix));
             }
         }
-        return dist[u];
+        return ret;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        int node = row * col;
+        memset(dp, -1, sizeof(dp));
         int ans = 0;
-        graph.resize(node);
-        indegree.resize(node, 0);
-        dist.resize(node, 0);
-        visited.resize(node, false);
-        for(int r = 0; r < row; r++){
-            for(int c = 0; c < col; c++){
-                for(int d = 0; d < 4; d++){
-                    int x = dirx[d] + r;
-                    int y = diry[d] + c;
-                    if(x >= 0 && x < row && y >= 0 && y < col && matrix[r][c] < matrix[x][y]){
-                        // int oneDindex = (row * collumn count) + column; // Indexes
-                        int from = r * col + c;
-                        int to = x * col + y;
-                        graph[from].push_back(to);
-                        indegree[to]++;
-                    }
-                }
-            }
-        }
-        for(int i = 0; i < node; i++){
-            if(indegree[i] == 0){
-                fill(visited.begin(), visited.end(), false);
-                dfs(i);
-                ans = max(ans, dist[i]);
+        for(int r = 0; r < matrix.size(); r++){
+            for(int c = 0; c < matrix[0].size(); c++){
+                ans = max(ans, solve(r, c, matrix));
             }
         }
         return ans;
