2091eb2df75a16a1fc58a8486db469ff507d3ddf
Badhan Sen <badhan.cse@gmail.com>
2022-05-21 14:38:17 +0600

    Time: 96 ms (69.12%), Space: 9.9 MB (97.58%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,3 +1,4 @@
+/*
 //@Author: KING-SEN
 // Programming Language Used: C++
 
@@ -29,3 +30,18 @@ public:
 
 // Time Complexity: O(Amount * N), N = denomination count.In the worst case the recursive tree of the algorithm has of height S and the algorithm solves only Amount subproblems because  it caches precalculated solutions in a table. Each subproblem is computed with N iterations. Therefore there is O(Amount * n) time complexity
 // Space Complexity:  O(Amount), where we use extra space for the memoization table for amount.
+*/
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int dp[amount + 1];
+        for(int i = 0; i<=amount; i++) dp[i] = 1e9;
+        dp[0] = 0;
+        for(int i = 1; i<=amount; i++){
+            for(int j=0; j<coins.size(); j++){
+                if(coins[j] <= i) dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+        return dp[amount] == 1e9 ? -1 : dp[amount];
+    }
+};
