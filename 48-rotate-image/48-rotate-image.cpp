d3ed68b40fe8d78498695224510013718efe0301
Badhan Sen <badhan.cse@gmail.com>
2022-05-21 16:06:33 +0600

    Time: 6 ms (18.98%), Space: 6.9 MB (92.81%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,37 @@
+//@Author: KING-SEN
+/*
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        if(matrix.size() == 0 || matrix.size() != matrix[0].size()) return;
+        reverse(matrix.begin(), matrix.end());
+        int n = matrix.size();
+        for(int row = 0; row < n; row++){
+            for(int col = row + 1; col < n; col++){
+                swap(matrix[row][col], matrix[col][row]);
+            }
+        }
+    }
+};
+*/
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        if(matrix.size() == 0 || matrix.size() != matrix[0].size()) return;
+        int n = matrix.size();
+        for(int i = 0; i < n / 2; i++){
+            for(int j = i; j < n - i - 1; j++){
+                // save the top element
+                int top = matrix[i][j];
+                // top <- left
+                matrix[i][j] = matrix[n - j - 1][i];
+                // left <- bottom
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                // bottom <- right
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                // right <- top
+                matrix[j][n - i - 1] = top;
+            }
+        }
+    }
+};
