060dd8f1693b637a043eeaddf31bf40f4d05ba13
Badhan Sen <badhan.cse@gmail.com>
2022-05-27 02:33:24 +0600

    Time: 12 ms (58.28%), Space: 17.4 MB (54.75%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -9,31 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+// @Author: KING-SEN
+// 1 last try
+
+class Solution {
+private:
+    int maxWidth;
+    unordered_map<int, long long> seen;
+public:
+    void dfs(TreeNode* root, int depth, int node){
+        if(!root) return;
+        if(!seen.count(depth)){
+            seen[depth] = node;
+        }
+        int newNode = seen[depth];
+        maxWidth = max(maxWidth, node - newNode + 1);
+        dfs(root->left, depth + 1, 2ll * node + 1);
+        dfs(root->right, depth + 1, 2ll * node + 2);
+    }
+    int widthOfBinaryTree(TreeNode* root) {
+        dfs(root, 0, 0);
+        return maxWidth;
+    }
+};
+
+// Time: O(N), N = number of nodes
+// Space: O(N), for recursion stack and store the node in map
+*/
+
+// @Author: KING-SEN
+// 1 last try
+
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL)
-            return 0;
-        int answer = 1;
-        queue<pair<TreeNode*, long long>> qt;
-        qt.push({root, 0});
-        while(qt.empty() == false){
-            int _size = qt.size();
-            int start = qt.front().second;
-            int end = qt.back().second;
-            answer = max(answer, end - start + 1);
-            for(int i = 0; i < _size; i++){
-                TreeNode* t = qt.front().first;
-                long long idx = qt.front().second - start;
-                qt.pop();
-                if(t->left){
-                    qt.push({t->left, idx * 2 + 1});
-                }
-                if(t->right){
-                    qt.push({t->right, idx * 2 + 2});
-                }
+        int maxWidth = 0;
+        queue<pair<TreeNode*, unsigned long long>> tree;
+        tree.push({root, 0});
+        while(!tree.empty()){
+            int size = tree.size();
+            int left = tree.front().second;
+            int right = tree.back().second;
+            maxWidth = max(maxWidth, right - left + 1);
+            for(int i = 0; i < size; i++){
+                auto node = tree.front();
+                tree.pop();
+                if(node.first->left) tree.push({node.first->left, 2ll * node.second + 1});
+                if(node.first->right) tree.push({node.first->right, 2ll * node.second + 2});
             }
         }
-        return answer;
+        return maxWidth;
     }
 };
+
+// Time: O(N), N = number of nodes
+// Space: O(N), for recursion stack and store the node in map
