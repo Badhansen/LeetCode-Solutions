cdcf8018ba8461ebb134f31ccdc6a734cf072f7d
Badhan Sen <badhan.cse@gmail.com>
2022-05-27 23:36:45 +0600

    Time: 3 ms (74.41%), Space: 8.9 MB (11.82%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -4,22 +4,18 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
         int n = citations.size();
-        int st = 0, ed = n - 1, ans = 0;
-        while(st <= ed){
-            int mid = (st + ed) / 2;
-            if(citations[mid] >= n - mid){
-                ans = n - mid;
-                ed = mid - 1;
-            }
-            else{
-                st = mid + 1;
-            }
+        vector<int> papers(n + 1, 0);
+        for(auto &c : citations){
+            papers[min(n, c)]++;
+        }
+        int ans = n;
+        for(int sum = papers[n]; ans > sum; sum += papers[ans]){
+            ans--;
         }
         return ans;
     }
 };
 
-// Time: O(N log N + log N) ~ O(N log N), N = number of elements
+// Time: O(N), N = number of elements
 // Space: O(1)
