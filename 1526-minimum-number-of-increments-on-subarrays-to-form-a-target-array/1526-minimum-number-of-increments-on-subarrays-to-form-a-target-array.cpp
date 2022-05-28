150a22e79c332fc76c4f5c449964512e8bcb5195
Badhan Sen <badhan.cse@gmail.com>
2022-05-29 00:54:09 +0600

    Time: 179 ms (42.81%), Space: 73.1 MB (63.62%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,89 +1,16 @@
 // @Author: KING-SEN
 // 1 last try
 
-struct Node{
-    int index;
-    int val;
-    Node(){}
-    Node(int i, int v){
-        index = i;
-        val = v;
-    }
-};
-class SegTree{
-private:
-    vector<Node> tree;
-    vector<int> nums;
+class Solution {
 public:
-    SegTree(){}
-    SegTree(int n, vector<int>& val){
-        tree.resize(4 * n);
-        nums = val;
-        build_tree(0, 0, n - 1);
-    }
-    Node merge(Node& a, Node& b){
-        if(a.val == b.val){
-            return a.index < b.index ? a : b;
+    int minNumberOperations(vector<int>& target) {
+        int ans = target[0], n = target.size();
+        for(int i = 1; i < n; i++){
+            if(target[i] > target[i - 1]) ans += target[i] - target[i - 1];
         }
-        return a.val < b.val ? a : b;
-    }
-    void build_tree(int node, int slow, int shigh){
-        if(slow == shigh){
-            tree[node] = Node(slow, nums[slow]);
-            return;
-        }
-        
-        int left = 2 * node + 1;
-        int right = 2 * node + 2;
-        int mid = (slow + shigh) / 2;
-        
-        build_tree(left, slow, mid);
-        build_tree(right, mid + 1, shigh);
-        
-        tree[node] = merge(tree[left], tree[right]);
-    }
-    
-    Node query(int node, int slow, int shigh, int i, int j){
-        if(i > shigh || j < slow || slow > shigh)
-            return Node(-1, INT_MAX);
-        if (slow >= i && shigh <= j)
-            return tree[node];
-        
-        int left = 2 * node + 1;
-        int right = 2 * node + 2;
-        int mid = (slow + shigh) / 2;
-        
-        Node lmin = query(left, slow, mid, i, j);
-        Node rmin = query(right, mid + 1, shigh, i, j);
-        
-        return merge(lmin, rmin);
+        return ans;
     }
 };
 
-class Solution {
-private:
-    SegTree *mintree;
-    int n;
-public:
-    void init(int n, vector<int>& val){
-        mintree = new SegTree(n, val);
-        this->n = n;
-    }
-    int divide(int st, int ed, int operations){
-        if(st > ed) return 0;
-        Node m = mintree->query(0, 0, n - 1, st, ed);
-        
-        int op = m.val - operations;
-        int left = divide(st, m.index - 1, m.val);
-        int right = divide(m.index + 1, ed, m.val);
-        
-        return op + left + right;
-    }
-    int minNumberOperations(vector<int>& target) {
-        n = target.size();
-        init(n, target);
-        return divide(0, n - 1, 0);
-    }
-};
-// Time: O(N * log N), N = Length of array
-// Space: O(4 * N)
+// Time: O(N), N = Length of array
+// Space: O(1)
