6bb4c49d790b5d929495f6663128cf16e0e3f039
Badhan Sen <badhan.cse@gmail.com>
2022-05-28 17:34:42 +0600

    Time: 30 ms (89.15%), Space: 6.1 MB (81.59%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
@@ -2,22 +2,21 @@
 // 1 last try
 
 class Solution {
-private:
-    int dp[205][205];
 public:
-    int solve(int low, int high){
-        if(low >= high) return 0;
-        if(dp[low][high] != -1) return dp[low][high];
-        int globalMin = INT_MAX;
-        for(int x = low; x <= high; x++){
-            int localMax = x + max(solve(low, x - 1), solve(x + 1, high));
-            globalMin = min(localMax, globalMin);
-        }
-        return dp[low][high] = globalMin; 
-    }
     int getMoneyAmount(int n) {
-        memset(dp, -1, sizeof dp); 
-        return solve(1, n);
+        int dp[205][205];
+        memset(dp, 0, sizeof dp);
+        for(int l = n; l > 0; l--){
+            for(int r = l + 1; r <= n; r++){
+                dp[l][r] = INT_MAX;
+                for(int pick = l; pick <= r; pick++){
+                    int left = dp[l][pick - 1];
+                    int right = dp[pick + 1][r];
+                    dp[l][r] = min(dp[l][r], pick + max(left, right));
+                }
+            }
+        }
+        return dp[1][n];
     }
 };
 
