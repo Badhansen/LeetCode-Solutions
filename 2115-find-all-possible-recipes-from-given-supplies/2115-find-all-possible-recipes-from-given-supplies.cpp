3b7b2c0b262da5f27ecdf1717af20d756f3b9bb3
Badhan Sen <badhan.cse@gmail.com>
2022-06-05 00:40:55 +0600

    Time: 1056 ms (13.91%), Space: 161.3 MB (14.06%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    unordered_map<string, vector<string>> graph;
+    unordered_map<string, int> visited;
+    unordered_map<string, int> present;
+public:
+    bool dfs(string src){
+        if(graph[src].size() == 0) return present.count(src);
+        visited[src] = 1;
+        for(auto dest : graph[src]){
+            if(visited[dest] == 1) return false;
+            if(visited[dest] == 0 && !dfs(dest)){
+                return false;
+            }
+        }
+        visited[src] = 2;
+        return true;
+    }
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        int n = recipes.size();
+        for(auto s : supplies) present[s] = 1;
+        for(int i = 0; i < n; i++){
+            for(auto &node : ingredients[i]){
+                graph[recipes[i]].push_back(node);
+            }
+        }
+        vector<string> answer;
+        for(auto &r : recipes){
+            if(dfs(r)){
+                answer.push_back(r);
+            }
+        }
+        return answer;
+    }
+};
