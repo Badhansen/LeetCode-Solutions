031a55d0e07ecd288a58a02e36d385cbb2ce2e06
Badhan Sen <badhan.cse@gmail.com>
2022-06-05 17:09:55 +0600

    Time: 454 ms (79.79%), Space: 60 MB (34.97%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -1,7 +1,9 @@
+typedef long long LL;
+
 class Solution {
 private:
     vector<int> power;
-    unordered_map<int, int> count;
+    unordered_map<int, LL> count;
     int mod = 1e9 + 7;
 public:
     void init(){
@@ -11,11 +13,11 @@ public:
             power[i] = power[i - 1] * 2;
         }
     }
-    int bigmod(int a, int b, int mod){ // O(log N)
+    LL bigmod(LL a, LL b, LL mod){ // O(log N)
         if(b == 0) return 1 % mod;
-        int x = bigmod(a, b / 2, mod);
-        x = (1ll * x * x) % mod;
-        if(b % 2 == 1) x = (1ll * x * a) % mod;
+        LL x = bigmod(a, b / 2, mod);
+        x = (x * x) % mod;
+        if(b % 2 == 1) x = (x * a) % mod;
         return x;
     }
     int countPairs(vector<int>& deliciousness) {
@@ -23,21 +25,21 @@ public:
         for(auto &num : deliciousness){ // O(N)
             count[num]++;
         }
-        int answer = 0;
+        LL answer = 0;
         for(auto [key, val] : count){ // O(N * 21) ~ O(N)
             for(int i = 0; i <= 21; i++){
                 int nkey = power[i] - key;
                 if(count.count(nkey)){
                     if(key == nkey){
-                        answer = (1ll * answer + (1ll * count[key] * (count[nkey] - 1))) % mod;
+                        answer = (answer + (count[key] * (count[nkey] - 1))) % mod;
                     }
                     else{
-                        answer = (1ll * answer + (1ll * count[key] * count[nkey])) % mod;
+                        answer = (answer + (count[key] * count[nkey])) % mod;
                     }
                 }
             }  
         }
-        answer = (1ll * answer * bigmod(2, mod - 2, mod)) % mod;
+        answer = (answer * bigmod(2, mod - 2, mod)) % mod;
         return answer;
     }
 };
