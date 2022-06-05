f86ab38bf81c7eb7d62f9f86840e4a10768d21f4
Badhan Sen <badhan.cse@gmail.com>
2022-06-05 11:02:58 +0600

    Time: 1615 ms (11.11%), Space: 110.9 MB (33.33%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.cpp
+++ 2296-design-a-text-editor/2296-design-a-text-editor.cpp
@@ -7,14 +7,14 @@ public:
         pos = 0;
     }
     
-    void addText(string text) {
+    void addText(string text) { // O(N)
         _text.insert(pos, text);
         pos += text.size();
     }
     
-    int deleteText(int k) {
+    int deleteText(int k) { // O(K * N)
         int count = 0;
-        while(pos != 0 && count < k){
+        while(pos != 0 && count < k){ // O(N)
             _text.erase(pos - 1, 1);
             pos--;
             count++;
@@ -22,7 +22,7 @@ public:
         return count;
     }
     
-    string cursorLeft(int k) {
+    string cursorLeft(int k) { // O(K)
         pos = max(pos - k, 0);
         string res = "";
         for(int i = pos - 10; i < pos; i++){
@@ -31,7 +31,7 @@ public:
         return res;
     }
     
-    string cursorRight(int k) {
+    string cursorRight(int k) { // O(K)
         pos = min(pos + k, (int)_text.size());
         string res = "";
         for(int i = pos - 10; i < pos; i++){
