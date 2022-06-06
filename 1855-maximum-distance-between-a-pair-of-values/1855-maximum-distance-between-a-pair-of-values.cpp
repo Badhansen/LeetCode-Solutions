1ea6ba37b7fa05d9bb5c77dbc647e66649f61afe
Badhan Sen <badhan.cse@gmail.com>
2022-06-06 21:51:47 +0600

    Time: 393 ms (6.44%), Space: 98.4 MB (75.65%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -1,23 +1,18 @@
+// @Author: KING-SEN
+// 1 last try
+
 class Solution {
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        vector<int> minval(n, INT_MAX), maxval(m, INT_MIN);
-        for(int i = 0; i < n; i++){
-            minval[i] = min(minval[i], nums1[i]);
-        }
-        for(int i = 0; i < m; i++){
-            maxval[i] = max(maxval[i], nums2[i]);
-        }
-        int cnt1 = 0, cnt2 = 0;
-        int ans = 0;
-        while(cnt1 < n && cnt2 < m){
-            if(minval[cnt1] <= maxval[cnt2]){
-                ans = max(ans, cnt2 - cnt1);
-                cnt2++;
+        int i = 0, j = 0, ans = 0;
+        while(i < n && j < m){
+            if(nums1[i] > nums2[j]){
+                i++;
             }
-            else if(minval[cnt1] > maxval[cnt2]){
-                cnt1++, cnt2++;
+            else{
+                ans = max(ans, j - i);
+                j++;
             }
         }
         return ans;
@@ -25,4 +20,4 @@ public:
 };
 
 // Time: O(N + M) // N = length of nums1, M = length of nums2
-// Space: O(N + M)
+// Space: O(1)
