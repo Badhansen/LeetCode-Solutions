1c9453c8bdc064184af685795b8bdf5cbdb28067
Badhan Sen <badhan.cse@gmail.com>
2022-06-07 03:36:15 +0600

    Time: 36 ms (91.66%), Space: 29.8 MB (92.46%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
@@ -11,18 +11,29 @@
  */
 class Solution {
 private:
-    unordered_map<string, int> count;
+    unordered_map<string, int> stoIndex;
+    unordered_map<int, int> count;
     vector<TreeNode*> res;
+    int index = 1;
 public:
-    string dfs(TreeNode* root){ // O(N)
-        if(!root) return "";
-        // O(1) ~ O(N)
-        string node = to_string(root->val) + 
-                    "," + dfs(root->left) +
-                    "," + dfs(root->right);
-        count[node]++;
-        if(count[node] == 2) res.push_back(root);
-        return node;
+    int dfs(TreeNode* root){ // O(N)
+        if(!root) return 0;
+        
+        int leftId = dfs(root->left);
+        int rightId = dfs(root->right);
+        
+        string node = to_string(leftId) + "," + to_string(root->val) + "," + to_string(rightId);
+        
+        int id = stoIndex[node];
+        if(id == 0){
+            id = index++;
+            stoIndex[node] = id;
+        }
+        
+        count[id]++;
+        if(count[id] == 2) res.push_back(root);
+        
+        return id;
     }
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
         dfs(root);
@@ -30,5 +41,5 @@ public:
     }
 };
 
-// Time: O(N^2), N = number of nodes
+// Time: O(N), N = number of nodes
 // Spce: O(N)
