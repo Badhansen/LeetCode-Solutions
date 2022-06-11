b37371b65c6341990546f125f53aed9a79c1ff98
Badhan Sen <badhan.cse@gmail.com>
2022-06-11 15:41:14 +0600

    Time: 530 ms (32.72%), Space: 107.3 MB (46.10%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -20,7 +20,6 @@ public:
             if(pos < n && left[pos] == x) ans = pos + 1;
             else ans = INT_MAX;
         }
-        //cout << ans << endl;
         
         pos = lower_bound(right.begin(), right.end(), x) - right.begin();
         
@@ -30,7 +29,7 @@ public:
         else{
             if(pos < n && right[pos] == x) ans = min(ans, pos + 1);
         }
-        //cout << ans << endl;
+
         for(int i = 1; i <= n; i++){
             int rem = x - left[i - 1];
             if(rem <= 0) break;
@@ -39,13 +38,13 @@ public:
             if(left[i - 1] + right[pos] == x) ans = min(ans, i + pos + 1);
         }
         
-        for(int i = 1; i <= n; i++){
-            int rem = x - right[i - 1];
-            if(rem <= 0) break;
-            pos = lower_bound(left.begin(), left.end(), rem) - left.begin();
-            if(pos == n) continue;
-            if(right[i - 1] + left[pos] == x) ans = min(ans, i + pos + 1);
-        }
+        // for(int i = 1; i <= n; i++){
+        //     int rem = x - right[i - 1];
+        //     if(rem <= 0) break;
+        //     pos = lower_bound(left.begin(), left.end(), rem) - left.begin();
+        //     if(pos == n) continue;
+        //     if(right[i - 1] + left[pos] == x) ans = min(ans, i + pos + 1);
+        // }
         
         return ans > n ? -1 : ans;
     }
