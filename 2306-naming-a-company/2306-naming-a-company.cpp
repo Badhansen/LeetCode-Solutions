846b2587beb6638c265b210b8d29baccee5ef40f
Badhan Sen <badhan.cse@gmail.com>
2022-06-12 14:24:43 +0600

    Time: 1466 ms (100.00%), Space: 78.9 MB (100.00%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_set<string> seen(ideas.begin(), ideas.end());
+        long long res = 0;
+        long long count[26][26] = {0};
+        for(auto idea : ideas){
+            string st = idea;
+            for(char i = 'a'; i <= 'z'; i++){
+                idea[0] = i;
+                count[st[0] - 'a'][i - 'a'] += seen.count(idea) == 0;
+            }
+        }
+        for(int i = 0; i < 26; i++){
+            for(int j = 0; j < 26; j++){
+                res += count[i][j] * count[j][i];
+            }
+        }
+        return res;
+    }
+};
