7ff92f2a69d99d587d5760ba4223083d0d889c80
Badhan Sen <badhan.cse@gmail.com>
2022-06-16 01:00:44 +0600

    Time: 112 ms (70.55%), Space: 19.7 MB (41.93%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -3,15 +3,15 @@ public:
     int longestStrChain(vector<string>& words) {
         vector<string> wlen[17];
         unordered_map<string, int> dist;
-        for(auto &word : words){
+        for(auto &word : words){ // O(N)
             wlen[word.size()].push_back(word);
             dist[word] = 1;
         }
         int ans = 1;
-        for(int i = 16; i > 0; i--){
-            for(auto &word : wlen[i]){
-                for(int l = 0; l < word.size(); l++){
-                    string nword = word.substr(0, l) + word.substr(l + 1);
+        for(int i = 16; i > 0; i--){ // O(16)
+            for(auto &word : wlen[i]){ // O(N)
+                for(int l = 0; l < word.size(); l++){ // O(L)
+                    string nword = word.substr(0, l) + word.substr(l + 1); // O(L)
                     if(dist.count(nword)){
                         dist[nword] = max(dist[nword], dist[word] + 1);
                         ans = max(ans, dist[nword]);
@@ -23,5 +23,5 @@ public:
     }
 };
 
-// Time: O(16 * total word length)
-// Space: O(total word length)
+// Time: O(N * L ^ 2)
+// Space: O(N)
