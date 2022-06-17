81c1ad5f328b07e0e72d74fe0dd1931f61bb971e
Badhan Sen <badhan.cse@gmail.com>
2022-06-17 23:26:31 +0600

    Time: 15 ms (64.53%), Space: 21.1 MB (87.57%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -4,6 +4,53 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+#define NO_CAMERA       0
+#define HAS_CAMERA      2
+#define NOT_NEEDED      1
+
+class Solution {
+private:
+    int ans = 0;
+public:
+    int dfs(TreeNode *root) {
+        if (!root) return NOT_NEEDED;
+        
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        
+        if (l == NO_CAMERA || r == NO_CAMERA) {
+            ans++;
+            return HAS_CAMERA;
+        } 
+        else if (l == HAS_CAMERA || r == HAS_CAMERA) {
+            return NOT_NEEDED;
+        } 
+        else {
+            return NO_CAMERA;
+        }
+    }
+    int minCameraCover(TreeNode* root) {
+        if (dfs(root) == NO_CAMERA) ans++;
+        return ans;
+    }
+};
+
+// Time: O(N), N = number of nodes
+// Space: O(recursion stack)
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
@@ -19,7 +66,7 @@
           /
          () <- Node covered with camera 
 */
-
+/*
 class Solution {
 private:
     int camera;
