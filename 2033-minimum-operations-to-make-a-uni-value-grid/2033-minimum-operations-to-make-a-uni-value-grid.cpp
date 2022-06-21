67cd82da3a49be558891993e77b95502ba43d44c
Badhan Sen <badhan.cse@gmail.com>
2022-06-22 03:57:40 +0600

    Time: 263 ms (98.70%), Space: 85.8 MB (71.64%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,3 +1,7 @@
+/*
+Here, we change every number to be a median. Note that we can find a median in O(n) using a quickselect algo (nth_element).
+*/
+
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
@@ -5,9 +9,9 @@ public:
         for(auto &row : grid){
             values.insert(values.end(), row.begin(), row.end());
         }
-        sort(values.begin(), values.end());
         int n = values.size();
         int mid = n / 2;
+        nth_element(values.begin(), values.begin() + mid, values.end());
         int median = values[mid];
         int result = 0;
         for(int i = 0; i < n; i++){
@@ -18,3 +22,6 @@ public:
         return result;
     }
 };
+
+// Time: O(N), N = all the elements of grid
+// Space: O(N)
