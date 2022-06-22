6e391002c19ef33f121ec3fc6d90128070a7a26f
Badhan Sen <badhan.cse@gmail.com>
2022-06-23 00:36:07 +0600

    Time: 4 ms (19.80%), Space: 6.2 MB (55.25%) - LeetHub

---
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        stringstream ss(s);
+        string word;
+        int count = 0;
+        while(ss >> word) count++;
+        return count;
+    }
+};
