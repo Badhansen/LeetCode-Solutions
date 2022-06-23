6ceecd4d7ae9806151aae615e74b6463a86fd126
Badhan Sen <badhan.cse@gmail.com>
2022-06-24 02:52:08 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (76.09%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        bool flag = false;
+        if(x == INT_MIN || x == INT_MAX) return 0;
+        if(x < 0){
+            flag = true;
+            x = -x;
+        }
+        int res = 0;
+        while(x){
+            int r = x % 10;
+            if(res && INT_MAX / res < 10) return 0;
+            res = res * 10 + r;
+            x /= 10;
+        }
+        if(flag) return -res;
+        return res;
+    }
+};
