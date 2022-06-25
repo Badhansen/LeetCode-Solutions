9fe0f342c15d98d4f1aadbe54e41b570fee08b84
Badhan Sen <badhan.cse@gmail.com>
2022-06-26 05:05:31 +0600

    Time: 53 ms (21.53%), Space: 27 MB (53.45%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int count = 0;
-        for(int i = 0; i < nums.size() - 1; i++){
-            if(nums[i] > nums[i + 1]){
-                if(count != 0) return false;
-                else count++;
-                if(i > 0 && nums[i + 1] < nums[i - 1]){
-                    nums[i + 1] = nums[i];
-                }
+        int cnt = 0; //the number of changes
+        for(int i = 1; i < nums.size() && cnt <= 1; i++){
+            if(nums[i-1] > nums[i]){
+                cnt++;
+                //modify nums[i-1] of a priority
+                if(i-2<0 || nums[i-2] <= nums[i]) nums[i-1] = nums[i];
+                else nums[i] = nums[i-1]; //have to modify nums[i]
             }
         }
-        return true;
+        return cnt <= 1;
     }
 };
