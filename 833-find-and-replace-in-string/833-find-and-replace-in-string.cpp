57a1770830e26dec2d45362f530d0e50af48f3b1
Badhan Sen <badhan.cse@gmail.com>
2022-06-30 01:42:23 +0600

    Time: 8 ms (52.58%), Space: 11 MB (42.20%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
@@ -5,7 +5,8 @@ private:
     vector<long long> p_pow;
     vector<long long> hash;
 public:
-    void init(string text){
+    // Rabin-Karp Algorithm for string matching
+    void init(string text){ 
         int n = text.size();
         p_pow.resize(n + 1);
         hash.resize(n + 1, 0);
@@ -17,6 +18,7 @@ public:
             hash[i + 1] = (hash[i] + (text[i] - 'a' + 1) * p_pow[i]) % mod;
         }
     }
+    // Calculate the pattern string hash
     vector<long long> calculateSourceHash(vector<string>& sources){
         int index = 0, n = sources.size();
         vector<long long> shash(n + 1);
@@ -30,29 +32,32 @@ public:
         return shash;
     }
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        init(s);
-        vector<long long> shash = calculateSourceHash(sources);
+        init(s); // O(N)
+        vector<long long> shash = calculateSourceHash(sources); // O(K * L)
         vector<pair<int, int>> nindices;
+        string res;
         int n = s.size(), k = indices.size(), prev = 0;
+        
         for(int i = 0; i < k; i++){
             nindices.push_back({indices[i], i});
         }
+        
+        // Sort the with respect to index value with O(K log K)
         sort(nindices.begin(), nindices.end(), [](auto &a, auto &b){
            return a.first < b.first; 
         });
-        string res;
-        for(int i = 0; i < k; i++){
+        
+        for(int i = 0; i < k; i++){ // O(K)
             int index = nindices[i].second, val = nindices[i].first;
-            // for(int j = prev; j < val; j++){
-            //     res += s[j];
-            //     prev++;
-            // }
-            res += s.substr(prev, val - prev);
-            prev += val - prev;
+            
+            res += s.substr(prev, val - prev); // Adding the substring up to the current indices with O(Length) time 
+            prev += val - prev; // Updating the previous index for substring addition
+            
             int sLen = sources[index].size();
             long long curr_hash;
+            
             if(val + sLen - 1 < n){
-                curr_hash = (hash[val + sLen] + mod - hash[val]) % mod;
+                curr_hash = (hash[val + sLen] + mod - hash[val]) % mod; // Calculating the substring hash with O(1) time
             }
             else{
                 curr_hash = -1;
@@ -65,10 +70,7 @@ public:
                 prev = val;
             }
             if(i == k - 1){
-                // for(int j = prev; j < n; j++){
-                //     res += s[j];
-                // }
-                res += s.substr(prev, n);
+                res += s.substr(prev, n); // Have to add all the remaing character with O(Length) time
             }
         }
         return res;
