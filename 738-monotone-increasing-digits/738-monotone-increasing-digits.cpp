f06d2bad94c0b8f78bcb3db1754de27b1c6638f5
Badhan Sen <badhan.cse@gmail.com>
2022-07-02 03:57:13 +0600

    Time: 2 ms (50.10%), Space: 6.1 MB (23.83%) - LeetHub

--- 738-monotone-increasing-digits/738-monotone-increasing-digits.cpp
+++ 738-monotone-increasing-digits/738-monotone-increasing-digits.cpp
@@ -1,3 +1,37 @@
+class Solution {
+public:
+    vector<int> intToVec(int n){
+        if(n == 0) return {0};
+        vector<int> res;
+        while(n){
+            int rem = n % 10;
+            res.push_back(rem);
+            n /= 10;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    int monotoneIncreasingDigits(int n) {
+        vector<int> digit = intToVec(n);
+        int len = digit.size();
+        int isEnd = len - 1, result = 0;
+        for(int i = len - 1; i > 0; i--){
+            if(digit[i - 1] > digit[i]){
+                isEnd = i - 1;
+                digit[i - 1]--;
+            }
+        }
+        for(int i = isEnd + 1; i < len; i++){
+            digit[i] = 9;
+        }
+        for(int i = 0; i < len; i++){
+            result = result * 10 + digit[i];   
+        }
+        return result;
+    }
+};
+
+/*
 // bfs approach
 
 class Solution {
@@ -23,3 +57,4 @@ public:
         return result;
     }
 };
+*/
