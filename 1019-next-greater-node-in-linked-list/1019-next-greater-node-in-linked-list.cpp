a0871edcf14de82386279607ae8f11479d19f6a0
Badhan Sen <badhan.cse@gmail.com>
2022-07-02 04:18:24 +0600

    Time: 128 ms (61.41%), Space: 40.9 MB (75.65%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getLength(ListNode* head){
+        ListNode* curr = head;
+        int len = 0;
+        while(head){
+            head = head->next;
+            len++;
+        }
+        return len;
+    }
+    vector<int> nextLargerNodes(ListNode* head) {
+        int len = getLength(head);
+        vector<int> answer(len, 0);
+        stack<pair<int, int>> nextLarge;
+        for(int i = 0; i < len; i++){
+            while(!nextLarge.empty() && head->val > nextLarge.top().first){
+                answer[nextLarge.top().second] = head->val;
+                nextLarge.pop();
+            }
+            nextLarge.push({head->val, i});
+            head = head->next;
+        }
+        return answer;
+    }
+};
