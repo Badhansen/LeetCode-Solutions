75b9b9021b640b0c289c8e56748745c62a43247a
Badhan Sen <badhan.cse@gmail.com>
2022-07-02 04:32:24 +0600

    Time: 96 ms (91.04%), Space: 43.8 MB (18.28%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -13,25 +13,16 @@ private:
     vector<int> answer;
     stack<pair<int, int>> nextLarge;
 public:
-    int getLength(ListNode* head){
-        ListNode* curr = head;
-        int len = 0;
-        while(head){
-            head = head->next;
-            len++;
-        }
-        return len;
-    }
     vector<int> nextLargerNodes(ListNode* head) {
-        int len = getLength(head);
-        answer.resize(len, 0);
-        for(int i = 0; i < len; i++){
-            while(!nextLarge.empty() && head->val > nextLarge.top().first){
-                answer[nextLarge.top().second] = head->val;
+        int index = 0;
+        for(ListNode* curr = head; curr; curr = curr->next){
+            while(!nextLarge.empty() && curr->val > nextLarge.top().first){
+                answer[nextLarge.top().second] = curr->val;
                 nextLarge.pop();
             }
-            nextLarge.push({head->val, i});
-            head = head->next;
+            nextLarge.push({curr->val, index});
+            answer.push_back(0);
+            index++;
         }
         return answer;
     }
