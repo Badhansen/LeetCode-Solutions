c59766e0b62979a9f5f5e75f0abf10cc3b3af729
Badhan Sen <badhan.cse@gmail.com>
2022-07-02 21:32:39 +0600

    Time: 213 ms (69.61%), Space: 85 MB (86.34%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,3 +1,34 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size(), hot = 0;
+        vector<int> ans(n, 0);
+        for(int currday = n - 1; currday >= 0; currday--){
+            int currTemp = temperatures[currday];
+            if(currTemp >= hot){
+                hot = currTemp;
+                continue;
+            }
+            int days = 1;
+            while(temperatures[currday + days] <= currTemp){
+                days += ans[currday + days];
+            }
+            ans[currday] = days;
+        }
+        return ans;
+    }
+};
+
+// Time: O(N), N = Length of temperatures
+// Space: O(1)
+/*   
+      0  1  2  3 4  5   6  7
+    [73, 74, 75, 71, 69, 72, 76, 73]   // for index 2, sum = 1 + ans[2 + 1] ~ 2 -> 3 + ans[2 + 3] ~ 1 -> 2 + ans[2 + 4]  
+      1   1   4   2   1  1   0   0
+               
+*/
+
+/*
 // @Author: KING-SEN
 // 1 last try
 
@@ -20,3 +51,5 @@ public:
 
 // Time: O(N), N = Length of temperatures
 // Space: O(N)
+
+*/
