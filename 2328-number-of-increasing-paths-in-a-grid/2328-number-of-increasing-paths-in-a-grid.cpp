8468cdc7f9c66f9b9c44359d1f0f2cf6242e8a1a
Badhan Sen <badhan.cse@gmail.com>
2022-07-03 17:08:30 +0600

    Time: 809 ms (50.00%), Space: 70.7 MB (50.00%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,6 +1,40 @@
 class Solution {
 private:
     const int mod = 1e9 + 7;
+    vector<array<int, 3>> h;
+    vector<vector<int>> dp;
+    int mv[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+public:
+    int countPaths(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        dp.resize(n, vector<int>(m));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                h.push_back({grid[i][j], i, j});
+            }
+        }
+        sort(h.begin(), h.end());
+        int ans = 0;
+        for (auto v : h) {
+            int i = v[1], j = v[2];
+            dp[i][j] = 1;
+            for (int k = 0; k < 4; k++) {
+                int ei = i + mv[k][0], ej = j + mv[k][1];
+                if (ei < 0 || ej < 0 || ei >= n || ej >= m) continue;
+                if (grid[ei][ej] >= grid[i][j]) continue;
+                dp[i][j] = (dp[i][j] + dp[ei][ej]) % mod;
+            }
+            ans = (ans + dp[i][j]) % mod;
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+private:
+    const int mod = 1e9 + 7;
     int x[4] = {0, 0, 1, -1};
     int y[4] = {1, -1, 0, 0};
     int dp[1005][1005];
@@ -34,3 +68,4 @@ public:
         return ans;
     }
 };
+*/
