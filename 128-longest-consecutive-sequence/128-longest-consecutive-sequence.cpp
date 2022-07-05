709245dd099995a5ed1c2b384df1b707272323ae
Badhan Sen <badhan.cse@gmail.com>
2022-07-06 00:10:58 +0600

    Time: 300 ms (48.17%), Space: 49.7 MB (52.56%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,57 +1,25 @@
-class UnionFind{
-private:
-    vector<int> parent;
-    vector<int> size;
-public:
-    UnionFind(int n){
-        parent.resize(n);
-        size.resize(n, 1);
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-        }
-    }
-    int FindRoot(int r){
-        if(parent[r] == r)
-            return r;
-        return parent[r] = FindRoot(parent[r]);
-    }
-    void Union(int a, int b){
-        a = FindRoot(a);
-        b = FindRoot(b);
-        if(a != b){
-            if(a < b) swap(a, b);
-            size[a] += size[b];
-            parent[b] = a;
-        }
-    }
-    int getSize(int index){
-        return size[index];
-    }
-    int getParent(int index){
-        return parent[index];
-    }
-};
 class Solution {
 private:
-    UnionFind* root;
+    unordered_map<int, bool> seen;
 public:
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
         if(!n) return 0;
-        root = new UnionFind(n);
-        unordered_map<int, int> keep;
         for(int index = 0; index < n; index++){
-            keep[nums[index]] = index;
+            seen[nums[index]] = index;
         }
-        for(auto &num : nums){
-            if(keep.find(num + 1) != keep.end()){
-                root->Union(keep[num + 1], keep[num]);
+        int longestStreak = 0;
+        for(auto iterator : seen){
+            if(!seen.count(iterator.first - 1)){
+                int currNum = iterator.first;
+                int currStreak = 1;
+                while(seen.count(currNum + 1)){
+                    currNum++;
+                    currStreak++;
+                }
+                longestStreak = max(longestStreak, currStreak);
             }
         }
-        int maxCount = 1;
-        for(int i = 0; i < n; i++){
-            maxCount = max(maxCount, root->getSize(i));
-        }
-        return maxCount;
+        return longestStreak;
     }
 };
