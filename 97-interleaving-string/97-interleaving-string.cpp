8904ef156469f20525c7d0d5234512f90c569bc6
Badhan Sen <badhan.cse@gmail.com>
2022-07-08 03:16:35 +0600

    Time: 5 ms (65.51%), Space: 6.3 MB (89.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,5 +1,35 @@
 class Solution {
 private:
+    vector<bool> cache;
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int len1 = s1.size(), len2 = s2.size(), len3 = s3.size();
+        if(len1 + len2 != len3){
+            return false;
+        }
+        cache.resize(len2 + 1);
+        for(int l1 = 0; l1 <= len1; l1++){
+            for(int l2 = 0; l2 <= len2; l2++){
+                if(l1 == 0 && l2 == 0){
+                    cache[l2] = true;
+                }
+                else if(l1 == 0){
+                    cache[l2] = s2[l2 - 1] == s3[l1 + l2 - 1] && cache[l2 - 1]; 
+                }
+                else if(l2 == 0){
+                    cache[l2] = s1[l1 - 1] == s3[l1 + l2 - 1] && cache[l2]; 
+                }
+                else{
+                    cache[l2] = (s1[l1 - 1] == s3[l1 + l2 - 1] && cache[l2]) || (s2[l2 - 1] == s3[l1 + l2 - 1] && cache[l2 - 1]);
+                }
+            }
+        }
+        return cache[len2];
+    }
+};
+/*
+class Solution {
+private:
     vector<vector<bool>> cache;
 public:
     bool isInterleave(string s1, string s2, string s3) {
@@ -32,7 +62,7 @@ public:
         return cache[len1][len2];
     }
 };
-
+*/
 /*
 class Solution {
 private:
