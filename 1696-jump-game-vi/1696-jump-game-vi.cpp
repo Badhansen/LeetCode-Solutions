e0fd8becc064f6c1d44af7213f05f0d71effac57
Badhan Sen <badhan.cse@gmail.com>
2022-07-09 16:15:27 +0600

    Time: 509 ms (14.11%), Space: 90.6 MB (45.19%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    priority_queue<pair<int, int>> que;
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxval = nums[0];
+        que.push({nums[0], 0});
+        for(int index = 1; index < n; index++){
+            while(!que.empty() && index - que.top().second > k){
+                que.pop();
+            }
+            pair<int, int> topval = que.top();
+            maxval = nums[index] + topval.first;
+            que.push({maxval, index});
+        }
+        return maxval;
+    }
+};
