52b5462edb134293a2256e92391c424a6406428a
Badhan Sen <badhan.cse@gmail.com>
2022-07-09 16:51:52 +0600

    Time: 189 ms (94.76%), Space: 77.4 MB (93.99%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,5 +1,30 @@
 class Solution {
 private:
+    deque<int> que;
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        que.push_back(0);
+        for(int index = 1; index < n; index++){
+            nums[index] = nums[index] + nums[que.front()];
+            while(!que.empty() && nums[index] >= nums[que.back()]){
+                que.pop_back();
+            }
+            que.push_back(index);
+            while(index - que.front() >= k){
+                que.pop_front();
+            }
+        }
+        return nums[n - 1];
+    }
+};
+
+// Time: O(N)
+// Space: O(K)
+
+/*
+class Solution {
+private:
     priority_queue<pair<int, int>> que;
 public:
     int maxResult(vector<int>& nums, int k) {
@@ -20,3 +45,4 @@ public:
 };
 // Time: O(N log K)
 // Space: O(K)
+*/
