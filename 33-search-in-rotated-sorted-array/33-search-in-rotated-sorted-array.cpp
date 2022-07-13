445cf90a1ed6113f3f5dba986512bb536b82294a
Badhan Sen <badhan.cse@gmail.com>
2022-07-14 01:43:07 +0600

    Time: 5 ms (60.95%), Space: 11 MB (75.00%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,5 +1,56 @@
 class Solution {
 public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            // Left sorted portion
+            if (nums[left] <= nums[mid]) {
+                // [4, 5, 6, 7, 0, 1, 2, 3]
+                //           m     t
+                /*
+                if (target > nums[mid]) {
+                    left = mid + 1;
+                } else if (target < nums[left]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+                */
+                if (target > nums[mid] or target < nums[left]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                // [4, 5, 6, 7, 0, 1, 2, 3]
+                //    2(t)        1(t)  m  
+                /*
+                if (target < nums[mid]) {
+                    right = mid - 1;
+                } else if (target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+                */
+                if (target < nums[mid] or target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+/*
+class Solution {
+public:
     int BinarySearch(vector<int>& nums, int left, int right, int target){
         while (left <= right) {
             int mid = left + (right - left) / 2;
@@ -52,3 +103,4 @@ public:
         }
     }
 };
+*/
