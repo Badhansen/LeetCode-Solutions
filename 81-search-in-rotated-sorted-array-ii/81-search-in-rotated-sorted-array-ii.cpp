7a3bfaa3a5e6ba919bb8b13ef02624f14ef828eb
Badhan Sen <badhan.cse@gmail.com>
2022-07-14 02:30:53 +0600

    Time: 20 ms (7.00%), Space: 14 MB (38.21%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,53 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int len = nums.size();
-        if(len == 0){
-            return false;
-        }
-        int low = 0, high = len - 1;
-        while(low <= high){
-            while(low < high && nums[low] == nums[low + 1]){
-                low++;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            while(left < right && nums[left] == nums[left + 1]){
+                left++;
             }
-            while(low < high && nums[high] == nums[high - 1]){
-                high--;
+            while(left < right && nums[right] == nums[right - 1]){
+                right--;
             }
-            int mid = (low + high) / 2;
-            if(nums[mid] == target){
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
                 return true;
             }
-            if(nums[low] <= nums[mid]){
-                if(target >= nums[low] && target < nums[mid]){
-                    high = mid - 1;
+            // Left sorted portion
+            if (nums[left] <= nums[mid]) {
+                // [4, 5, 6, 7, 0, 1, 2, 3]
+                //           m     t
+                /*
+                if (target > nums[mid]) {
+                    left = mid + 1;
+                } else if (target < nums[left]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
-                else{
-                    low = mid + 1;
+                */
+                if (target > nums[mid] or target < nums[left]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
-            }
-            else{
-                if(target > nums[mid] && target <= nums[high]){
-                    low = mid + 1;
+            } else {
+                // [4, 5, 6, 7, 0, 1, 2, 3]
+                //    2(t)        1(t)  m  
+                /*
+                if (target < nums[mid]) {
+                    right = mid - 1;
+                } else if (target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
-                else{
-                    high = mid - 1;
+                */
+                if (target < nums[mid] or target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
             }
         }
