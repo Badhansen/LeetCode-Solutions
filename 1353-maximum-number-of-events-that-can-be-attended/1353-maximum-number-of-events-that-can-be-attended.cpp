5559ba4f044f9dfd1c4cf7fa0a64055d1e3072ce
Badhan Sen <badhan.cse@gmail.com>
2022-07-14 23:40:48 +0600

    Time: 655 ms (40.34%), Space: 70.8 MB (92.99%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -6,6 +6,9 @@ public:
         int days = 1, left = 0, right = events.size();
         int result = 0;
         while (!event_queue.empty() || left < right) {
+            if (event_queue.empty()) {
+               days = events[left][0]; 
+            }
             while (left < right && events[left][0] == days) {
                 event_queue.push(events[left][1]);
                 left++;
@@ -23,6 +26,36 @@ public:
     }
 };
 /*
-Time O(d + nlogn), where D is the range of A[i][1]
+    Time: O(N log N)
+    Space: O(N)
+*/
+/*
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end());
+        priority_queue<int, vector<int>, greater<int>> event_queue;
+        int days = 1, left = 0, right = events.size();
+        int result = 0;
+        while (!event_queue.empty() || left < right) {
+            while (left < right && events[left][0] == days) {
+                event_queue.push(events[left][1]);
+                left++;
+            }
+            while (!event_queue.empty() && event_queue.top() < days) {
+                event_queue.pop();
+            }
+            if (!event_queue.empty()) {
+                event_queue.pop();
+                result++;
+            } 
+            days++;
+        }
+        return result;
+    }
+};
+
+/*
+Time O(d + nlogn), where days is the range of A[i][1]
 Space O(N)
 */
