ff7617014bd04f3249f3f34fb1e655c23ea93223
Badhan Sen <badhan.cse@gmail.com>
2022-07-14 23:25:40 +0600

    Time: 590 ms (53.76%), Space: 70.9 MB (34.89%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end());
+        priority_queue<int, vector<int>, greater<int>> event_queue;
+        int days = 1, left = 0, right = events.size();
+        int result = 0;
+        while (!event_queue.empty() || left < right) {
+            while (left < right && events[left][0] == days) {
+                event_queue.push(events[left][1]);
+                left++;
+            }
+            while (!event_queue.empty() && event_queue.top() < days) {
+                event_queue.pop();
+            }
+            if (!event_queue.empty()) {
+                event_queue.pop();
+                result++;
+            } 
+            days++;
+        }
+        return result;
+    }
+};
