d34084f171c9876414543bd0ac5eba7388a1d93c
Badhan Sen <badhan.cse@gmail.com>
2022-07-17 01:09:57 +0600

    Time: 0 ms (100.00%), Space: 9.7 MB (75.39%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int maxLength(vector<string>& arr) {
+        vector<bitset<26>> results;
+        int answer = 0;
+        for (auto& s : arr) {
+            bitset<26> mask;
+            for (auto& c : s) {
+                mask.set(c - 'a');
+            }
+            int count = mask.count();
+            if (count != s.size()) continue;
+            for (int i = results.size() - 1; i >= 0; i--) {
+                auto& bits = results[i];
+                if ((bits & mask).any()) continue;
+                int total = count + bits.count();
+                answer = max(answer, total);
+                results.push_back(bits | mask);
+            }
+            results.push_back(mask);
+            answer = max(answer, count);
+        }
+        return answer;
+    }
+};
+
+/*
+class Solution {
+public:
+    int maxLength(vector<string>& arr) {
         int n = arr.size();
         int result = 0;
         for (int i = 0; i < (1 << n); i++) {
@@ -20,3 +47,4 @@ public:
         return result;
     }
 };
+*/
