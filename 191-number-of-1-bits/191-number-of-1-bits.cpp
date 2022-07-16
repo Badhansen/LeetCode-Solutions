f40cdeb0e79f1711d8d1e52c2700d913b5c61756
Badhan Sen <badhan.cse@gmail.com>
2022-07-16 23:33:50 +0600

    Time: 0 ms (100.00%), Space: 6 MB (47.68%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
+        int res = 0;
+        while (n) {
+            n &= (n - 1);
+            res++;
+        }
+        return res;
     }
 };
