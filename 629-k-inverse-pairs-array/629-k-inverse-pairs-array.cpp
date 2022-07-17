8e94dc8f4a682da99f04804ebc6e03c2477d3072
Badhan Sen <badhan.cse@gmail.com>
2022-07-17 20:31:31 +0600

    Time: 41 ms (69.23%), Space: 14.1 MB (62.18%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -4,17 +4,17 @@ const int kMaxK = 1005;
 
 class Solution {
 private:
-    int cache[kMaxN][kMaxK] = {1};
+    int dp[kMaxN][kMaxK] = {1};
 public:
     int kInversePairs(int n, int k) {
         for (int N = 1; N <= n; ++N) {
             for (int K = 0; K <= k; ++K) {
-                for (int i = 0; i <= min(K, N - 1); ++i) { 
-                    cache[N][K] = (cache[N][K] + cache[N - 1][K - i]) % kMod;
-                }
+                dp[N][K] = (dp[N - 1][K] + (K > 0 ? dp[N][K - 1] : 0)) % kMod;
+                if (K >= N)
+                    dp[N][K] = (1000000007 + dp[N][K] - dp[N - 1][K - N]) % kMod;
             }
         }
-        return cache[n][k];
+        return dp[n][k];
 	}
 };
 
