29f76d8e18a235fe446aaa9f2390106fe3e98446
Badhan Sen <badhan.cse@gmail.com>
2022-07-21 01:18:53 +0600

    Time: 170 ms (95.54%), Space: 46 MB (80.62%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -4,6 +4,36 @@
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
+        vector<pair<int, int>> next[26];
+        int ans = 0;
+        for (int i = 0; i < words.size(); i++) {
+            next[words[i][0] - 'a'].push_back({i, 0});
+        }
+        for (auto c : s) {
+            vector<pair<int, int>> curr = next[c - 'a'];
+            next[c - 'a'].clear();
+            for (auto [index, len] : curr) {
+                if (words[index][len] == c) {
+                    len++;
+                    if(len == words[index].size()) {
+                        ans++;
+                    } else {
+                        next[words[index][len] - 'a'].push_back({index, len});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// Time: O(L + SUM(words length))
+// Space: O(SUM(words length))
+
+/*
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
         vector<pair<string, int>> next[26];
         int ans = 0;
         for(auto &word : words){ // O(W)
@@ -25,5 +55,6 @@ public:
     }
 };
 
-// Time: O(L * SUM(words length))
+// Time: O(L + SUM(words length))
 // Space: O(SUM(words length))
+*/
