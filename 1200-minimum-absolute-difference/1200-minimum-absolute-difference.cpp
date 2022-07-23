76b98a6911b39142634ea1018f6526074464524f
Badhan Sen <badhan.cse@gmail.com>
2022-07-23 19:58:48 +0600

    Time: 90 ms (73.38%), Space: 32.3 MB (32.20%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -1,4 +1,26 @@
+// Sort + 1 Traversal
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> answer;
+        int diff, min_diff = INT_MAX; 
+        for(int i = 1; i < arr.size(); i++){
+            diff = arr[i] - arr[i - 1];
+            if (diff == min_diff) {
+                answer.push_back({arr[i - 1], arr[i]});
+            } else if (diff < min_diff) {
+                min_diff = diff;
+                answer = {};
+                answer.push_back({arr[i - 1], arr[i]});
+            }
+        }
+        return answer;
+    }
+};
+
 // Sort + 2 Traversal
+/*
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
@@ -18,3 +40,4 @@ public:
         return answer;
     }
 };
+*/
