51c39c632d18f1311f7b4f671d9f6551afcd607c
Badhan Sen <badhan.cse@gmail.com>
2022-07-30 03:34:07 +0600

    Time: 3 ms (66.30%), Space: 6.1 MB (75.38%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
+        int result = 0, start = 0, mid, end = 2;
+        if (s.size() <= end) return 0;
+        while (end < s.size()) {
+            mid = start + 1;
+            if (s[start] != s[mid] && s[mid] != s[end] && s[end] != s[start]) {
+                result++;
+            }
+            start++;
+            end++;
+        }
+        return result;
+    }
+};
+/*
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
         int result = 0;
         for (int i = 0; i < s.size(); i++) {
             set<char> count;
@@ -12,3 +29,4 @@ public:
         return result;
     }
 };
+*/
