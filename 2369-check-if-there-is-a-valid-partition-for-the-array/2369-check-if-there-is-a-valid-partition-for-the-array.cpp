1dc6f5078dbd08584dd3e600be741f6d07eb0f0b
Badhan Sen <badhan.cse@gmail.com>
2022-08-08 02:43:02 +0600

    Time: 225 ms (66.67%), Space: 83.7 MB (22.22%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,4 +1,19 @@
 class Solution {
+public:
+    bool validPartition(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> cache = {true, false, nums[0] == nums[1], false};
+        for (int i = 2; i < n; i++) {
+            bool two = nums[i] == nums[i - 1];
+            bool three = (two && nums[i - 1] == nums[i - 2]) ||
+                        (nums[i] - 1 == nums[i - 1] && nums[i - 1] - 1 == nums[i - 2]);
+            cache[(i + 1) % 4] = (two && cache[(i - 1) % 4]) || (three && cache[(i - 2) % 4]);
+        }
+        return cache[n % 4];
+    }
+};
+/*
+class Solution {
 private:
     vector<int> cache;
     int n;
@@ -22,3 +37,4 @@ public:
         return dfs(0, nums);
     }
 };
+*/
