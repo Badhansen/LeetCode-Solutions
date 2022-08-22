603b5a1d64ee5e22247b85ad2a28e291141fbff3
Badhan Sen <badhan.cse@gmail.com>
2022-08-23 01:38:12 +0600

    Time: 600 ms (80.00%), Space: 117.1 MB (20.00%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,7 +1,7 @@
 class SegTree {
 private:
     int size;
-    vector<int> tree;
+    vector<long long> tree, lazy;
 public:
     SegTree(int n) {
         size = 1;
@@ -9,21 +9,39 @@ public:
             size *= 2;
         }
         tree.resize(2 * size, 0);
+        lazy.resize(2 * size, 0);
+    }
+    void build(vector<int> &v, int n, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            if (lx < n) {
+                tree[x] = v[lx];
+            }
+            return;
+        }
+        int m = (lx + rx) / 2;
+        build(v, n, 2 * x + 1, lx, m);
+        build(v, n, 2 * x + 2, m, rx);
+        tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
+    }
+    void build(vector<int> &v, int n) {
+        build(v, n, 0, 0, size);
     }
     void updateRange(int l, int r, int val, int x, int lx, int rx) {
         if (lx >= r || l >= rx) return;
         if (lx >= l && rx <= r) {
+            lazy[x] += val;
             tree[x] += val;
             return;
         }
         int m = (lx + rx) / 2;
         updateRange(l, r, val, 2 * x + 1, lx, m);
         updateRange(l, r, val, 2 * x + 2, m, rx);
+        tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
     }
     void updateRange(int l, int r, int val) {
         updateRange(l, r, val, 0, 0, size);
     }
-    int get(int i, int x, int lx, int rx) {
+    long long get(int i, int x, int lx, int rx) {
         if (rx - lx == 1) {
             return tree[x];
         }
@@ -35,21 +53,26 @@ public:
         else{
             res = get(i, 2 * x + 2, m, rx);
         }
-        return res + tree[x];
+        return res + lazy[x];
     }
-    int get(int i) {
+    long long get(int i) {
         return get(i, 0, 0, size);
     }
     ~SegTree() {
         tree.clear();
+        lazy.clear();
     }
 };
-
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
+        vector<int> value(n);
+        for (int i = 0; i < n; i++) {
+            value[i] = s[i] - 'a';
+        }
         SegTree root(n);
+        root.build(value, n);
         for (auto st : shifts) {
             if (st[2] == 1) {
                 root.updateRange(st[0], st[1] + 1, 1);
@@ -60,7 +83,6 @@ public:
         for (int i = 0; i < n; i++) {
             int val = root.get(i) % 26;
             if (val < 0) val += 26;
-            val += (s[i] - 'a');
             val %= 26;
             s[i] = 'a' + val;
         }
