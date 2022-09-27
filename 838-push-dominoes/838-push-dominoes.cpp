68d1bee70df6d705abe13332a7f1ba6dfa47d3a0
Badhan Sen <badhan.cse@gmail.com>
2022-09-27 11:02:20 +0600

    Time: 146 ms (12.83%), Space: 21.5 MB (28.60%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -0,0 +1,44 @@
+// not my solution
+/*
+We just need to consider the two closest non-period characters.
+left_r_dis[i] is the smallest distance between dominoes[i] and its left R.
+right_l_dis[i] is the smallest distance between dominoes[i] and its right L.
+*/
+
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int n = dominoes.size();
+        vector<int> left_r_dis(n + 1, 0);
+        vector<int> right_l_dis(n + 1, 0);
+        int r_pos = n + 1;
+        for (int i = 0; i < n; ++i) {
+            if (dominoes[i] == 'R') {
+                r_pos = i;
+            }
+            else if (dominoes[i] == 'L') {
+                r_pos = n + 1;
+            }
+            left_r_dis[i] = r_pos == n + 1 ? n + 1 : i - r_pos;
+        }
+        int l_pos = -1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (dominoes[i] == 'L') {
+                l_pos = i;
+            }
+            else if (dominoes[i] == 'R') {
+                l_pos = -1;
+            }
+            right_l_dis[i] = l_pos == -1 ? n + 1 : l_pos - i;
+        }
+        
+        string result = dominoes;
+        for (int i = 0; i < n; ++i) {
+            if (left_r_dis[i] < right_l_dis[i]) result[i] = 'R';
+            else if (left_r_dis[i] > right_l_dis[i]) result[i] = 'L';
+            //else
+        }
+
+        return result;
+    } 
+};
