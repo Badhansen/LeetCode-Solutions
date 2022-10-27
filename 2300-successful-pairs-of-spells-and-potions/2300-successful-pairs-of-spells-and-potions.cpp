aa5182a81b5d734f3e9e83ac24f91b6c82200c84
Badhan Sen <badhan.cse@gmail.com>
2022-10-28 01:14:38 +0600

    Time: 283 ms (88.31%), Space: 100 MB (21.83%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -2,6 +2,22 @@ class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         sort(potions.begin(), potions.end());
+        int n  = spells.size(), m = potions.size();
+        vector<int> answer;
+        for(auto s : spells){
+            long long div = success / s;
+            if (success % s != 0) div++;
+            int count = lower_bound(potions.begin(), potions.end(), div) - potions.begin();
+            answer.push_back(m - count);
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
         int length = potions.size();
         vector<int> answer;
         for (auto s : spells) {
@@ -12,3 +28,4 @@ public:
         return answer;
     }
 };
+*/
