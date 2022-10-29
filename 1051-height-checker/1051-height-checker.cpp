8774b9118b788c39770dbfa309635dc61dd3a33c
Badhan Sen <badhan.cse@gmail.com>
2022-10-29 11:16:47 +0600

    Time: 3 ms (76.91%), Space: 8.2 MB (25.41%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,14 +1,36 @@
+const int kMax = 105;
+
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> expected = heights;
-        sort(expected.begin(), expected.end());
-        int count = 0;
+        // vector<int> expected = heights;
+        // sort(expected.begin(), expected.end());
+        // int count = 0;
+        // for (int i = 0; i < heights.size(); i++) {
+        //     if (expected[i] != heights[i]) {
+        //         count++;
+        //     }
+        // }
+        // return count;
+        
+        int mismatch = 0; 
+        vector<int> count(kMax, 0);
         for (int i = 0; i < heights.size(); i++) {
-            if (expected[i] != heights[i]) {
-                count++;
+            count[heights[i]]++;
+        }
+
+        int i = 1, j = 0; 
+        while(i < 101) {
+            if (count[i] == 0) {
+                i++;
+            }
+            else {
+                if (i != heights[j]) {
+                    mismatch++;
+                }
+                j++; count[i]--;
             }
         }
-        return count;
+        return mismatch;
     }
 };
