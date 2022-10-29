c8b82647a92a236765f0b232b8f6d660de333aa4
Badhan Sen <badhan.cse@gmail.com>
2022-10-29 11:53:28 +0600

    Time: 20 ms (79.23%), Space: 17.3 MB (93.97%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -0,0 +1,28 @@
+// @Author: KING-SEN
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> result;
+        int length = asteroids.size();
+        for (int i = 0; i < length; i++) {
+            bool blows = false;
+            while (result.size() && asteroids[i] < 0 && result.back() > 0 && !blows) {
+                if (abs(asteroids[i]) < result.back()) {
+                    blows = true;
+                } else {
+                    if (abs(asteroids[i]) == result.back()) {
+                        blows = true;
+                        result.pop_back();
+                    } else {
+                        result.pop_back();
+                    }
+                }
+            }
+            if (!blows) {
+                result.push_back(asteroids[i]);
+            }
+        }
+        return result;
+    }
+};
