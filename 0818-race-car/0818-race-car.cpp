3a6e065e102e2efef30d26ced60bbd081c078004
Badhan Sen <badhan.cse@gmail.com>
2022-11-01 22:16:51 +0600

    Time: 937 ms (17.87%), Space: 63.5 MB (22.95%) - LeetHub

---
+++ 0818-race-car/0818-race-car.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int racecar(int target) {
+        std::queue<std::array<int, 2>> que;
+        std::map<std::array<int, 2>, bool> discovered;
+        que.push({0, 1});
+        discovered.insert({{0, 1}, true});
+        int level = 0;
+        while (!que.empty()) {
+            int que_size = que.size();
+            while (que_size--) {
+                auto node = que.front();
+                que.pop();
+                if (node[0] == target) {
+                    return level;
+                }
+                for (int i = 0; i < 2; i++) {
+                    int position = node[0];
+                    int speed = node[1];
+                    if (i == 0) {
+                        position += speed;
+                        speed *= 2;
+                    } else {
+                        speed = speed > 0 ? -1 : 1;
+                    }
+                    if (position < 0 || position > 2 * target) continue;
+                    if (discovered.insert({{position, speed}, true}).second) {
+                        que.push({position, speed});
+                    }
+                }
+                
+            }
+            level++;
+        }
+        return level;
+    }
+};
