a0c48a83cd9f201fecbe0182240a2f54952b24d8
Badhan Sen <badhan.cse@gmail.com>
2022-11-06 02:38:00 +0600

    Time: 306 ms (93.84%), Space: 134.8 MB (61.34%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,30 @@
+// @Author: KING-SEN
+// Programming Language Used: C++
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int length = nums.size();
+        deque<int> seen;
+        vector<int> answer;
+        for(int i = 0; i < length; i++){
+            if (!seen.empty() && seen.front() == i - k) {
+                seen.pop_front();
+            }
+            while (!seen.empty() && nums[i] > nums[seen.back()]) {
+                seen.pop_back();
+            }
+            seen.push_back(i);
+            if (i >= k - 1) {
+                answer.push_back(nums[seen.front()]);
+            }
+        }
+        return answer;
+    }
+};
+
+// Time Complexity: O(N), here N = Length of the array, we are using deque and since each element is processed 
+// exactly twice - it's index added and then removed from the deque.
+// So overall time complexity will be N
+
+// Space Complexity: O(K), here K = Length of the window. At any times there will be K elements in the deque. So Space complexity will be O(K).
