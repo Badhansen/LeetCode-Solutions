9121350ab42df4d77f52e82aa1e296739b6de84a
Badhan Sen <badhan.cse@gmail.com>
2022-11-11 02:19:53 +0600

    Time: 10 ms (68.30%), Space: 7.2 MB (98.57%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0; 
+        int right = s.size() - 1;
+        while (left < right) {
+            while (!isalnum(s[left]) && left < right) {
+                left++;
+            }
+            while (!isalnum(s[right]) && left < right) {
+                right--;
+            }
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            left++, right--;
+        }
+        return true;
+    }
+};
