5b9d23060beaacc0d4048c4ab602348de01e8a95
Badhan Sen <badhan.cse@gmail.com>
2022-11-15 22:02:02 +0600

    Time: 26 ms (81.66%), Space: 12.2 MB (62.21%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,5 +1,59 @@
 // @Author: KING-SEN
 
+class UnionFind{
+private:
+    vector<int> parent;
+    vector<int> size;
+public:
+    UnionFind(int n){
+        parent.resize(n);
+        size.resize(n, 1);
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+    }
+    int FindRoot(int r){
+        if(parent[r] == r)
+            return r;
+        return parent[r] = FindRoot(parent[r]);
+    }
+    bool Union(int a, int b){
+        a = FindRoot(a);
+        b = FindRoot(b);
+        if(a != b){
+            if (size[a] < size[b])
+		        swap(a, b);
+            parent[b] = a;
+            size[a] += size[b];
+            return true;
+        }
+        return false;
+    }
+    int getSize(int index){
+        return size[index];
+    }
+    int getParent(int index){
+        return parent[index];
+    }
+};
+class Solution {
+private:
+    
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        UnionFind find_root(n);
+        int components = n;
+        for (auto e : edges) {
+            if (find_root.Union(e[0], e[1])) {
+                components--;
+            } else {
+                return false;
+            }
+        }
+        return components == 1;
+    }
+};
+/*
 class Solution {
 private:
     vector<bool> visited;
@@ -40,3 +94,4 @@ public:
         return true;
     }
 };
+*/
