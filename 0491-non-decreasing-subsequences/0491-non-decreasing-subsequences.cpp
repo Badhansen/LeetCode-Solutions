f17979df02da2e4bdd7878b523b6b2f17b612b4f
Badhan Sen <badhan.cse@gmail.com>
2023-01-21 02:10:37 +0600

    Time: 224 ms (17.93%), Space: 94.7 MB (12.06%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
+    void backtrack(int pos, vector<int>& nums, vector<int> path, set<vector<int>> &answer) {
+        if (pos >= nums.size()) {
+            if (path.size() >= 2) {
+               answer.insert(path);
+            }
+            return;
+        }
+        if (path.empty() || path.back() <= nums[pos]) {
+            path.push_back(nums[pos]);
+            backtrack(pos + 1, nums, path, answer);
+            path.pop_back();
+        }
+        backtrack(pos + 1, nums, path, answer);
+    }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         set<vector<int>> answer;
-        int n = nums.size(); 
-        for (int i = 0; i < (1 << n); i++) {
-            vector<int> path;
-            for (int j = 0; j < n; j++) {
-                if (i & (1 << j)) {
-                    path.push_back(nums[j]);
-                }
-            }
-            bool flag = true;
-            for (int j = 1; j < path.size(); j++) {
-                if (path[j] < path[j - 1]) {
-                    flag = false;
-                }
-            }
-            if (flag && path.size() >= 2) {
-                answer.insert(path);
-            }
-        }
-        return vector<vector<int>> (answer.begin(), answer.end());
+        vector<int> path;
+        backtrack(0, nums, path, answer);
+        return vector(answer.begin(), answer.end());
     }
 };
