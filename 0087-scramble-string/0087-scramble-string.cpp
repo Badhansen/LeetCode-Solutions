f851de0d1752e229da5e35b7ca6e88b57dba3c5a
Badhan Sen <badhan.cse@gmail.com>
2023-03-31 04:08:23 +0600

    Time: 14 ms (89.92%), Space: 6.4 MB (94.88%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int dp[35][35][35];
+    bool vis[35][35][35];
+public:
+    bool solve(int i, int j, int len, string &s1, string &s2) {
+        if (vis[i][j][len]) {
+            return dp[i][j][len];
+        }
+        if (len == 1) {
+            return dp[i][j][len] = s1[i] == s2[j];
+        }
+        for (int k = 1; k < len; k++) {
+            if (solve(i, j, k, s1, s2) && solve(i + k, j + k, len - k, s1, s2)) {
+                return dp[i][j][len] = true;
+            }
+            if (solve(i, j + len - k, k, s1, s2) && solve(i + k, j, len - k, s1, s2)) {
+                return dp[i][j][len] = true;
+            }
+        }
+        vis[i][j][len] = true;
+        return dp[i][j][len] = false;
+    }
+    bool isScramble(string s1, string s2) {
+        int n = s1.size();
+        memset(dp, -1, sizeof(dp));
+        memset(vis, false, sizeof(vis));
+        return solve(0, 0, n, s1, s2);
+    }
+};
