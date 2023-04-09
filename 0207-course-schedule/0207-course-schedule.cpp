09a2f37fbc2b533569caf3f71b0aaa6695204c4b
Badhan Sen <badhan.cse@gmail.com>
2023-04-10 00:37:29 +0600

    Time: 16 ms (95.01%), Space: 13.7 MB (70.95%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,28 +3,27 @@ private:
     vector<vector<int>> graph;
     vector<int> visited;
 public:
-    bool dfs(int u){
-        if(visited[u] == 1)
-            return true;
-        if(visited[u] == 0){
-            visited[u] = 1;
+    bool dfs(int u) {
+        if (visited[u] == 1) return true; // Already visited
+        if (visited[u] == 0) { // Not visited
+            visited[u] = 1; // mark as visited
             for(auto &v : graph[u]){
-                if(dfs(v)){
+                if(dfs(v)){ // if in any path I can find a visited node then there is an cycle
                     return true;
                 }
             }   
         }
-        visited[u] = 2;
+        visited[u] = 2; // Mark as done.
         return false;
     }
     bool canFinish(int n, vector<vector<int>>& pre) {
         graph.resize(n + 1);
         visited.resize(n + 1, 0);
-        for(auto &e : pre){
+        for (auto &e : pre){
             graph[e[1]].push_back(e[0]);
         }
-        for(int i = 0; i < n; i++){
-            if(dfs(i)){
+        for (int i = 0; i < n; i++) {
+            if (dfs(i)) {
                 return false;
             }
         }
