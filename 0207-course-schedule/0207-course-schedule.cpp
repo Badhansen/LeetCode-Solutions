521a547cbf1873a4e5b49c1d02042b8b59fbb5c1
Badhan Sen <badhan.cse@gmail.com>
2023-04-10 00:27:53 +0600

    Time: 21 ms (73.21%), Space: 13.7 MB (65.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,33 @@
-//@Author: KING-SEN
-
 class Solution {
+private:
+    vector<vector<int>> graph;
+    vector<int> visited;
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> graph[numCourses + 1];
-        vector<int> indeg(numCourses + 1, 0);
-        for(auto &el : prerequisites){
-            indeg[el[0]]++;
-            graph[el[1]].push_back(el[0]);
-        }
-        vector<bool> visited(numCourses + 1, false);
-        vector<int> course;
-        for(int i = 0; i < numCourses; i++){
-            if(indeg[i] == 0) course.push_back(i);
-        }
-        
-        int done = 0,id = 0;
-        while(id < course.size()){
-            done++;
-           
-            int curr = course[id];
-            for(auto &v: graph[curr]){
-                if(indeg[v] > 0){
-                    indeg[v]--;
-                    if(indeg[v] == 0) course.push_back(v);
+    bool dfs(int u){
+        if(visited[u] == 1)
+            return true;
+        if(visited[u] == 0){
+            visited[u] = 1;
+            for(auto &v : graph[u]){
+                if(dfs(v)){
+                    return true;
                 }
-            }
-            id++;
+            }   
         }
-        return done == numCourses;
+        visited[u] = 2;
+        return false;
+    }
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        graph.resize(n + 1);
+        visited.resize(n + 1, 0);
+        for(auto &e : pre){
+            graph[e[1]].push_back(e[0]);
+        }
+        for(int i = 0; i < n; i++){
+            if(dfs(i)){
+                return false;
+            }
+        }
+        return true;
     }
 };
