ffafbd58ff68d74774319acd613ae7765fa38904
Badhan Sen <badhan.cse@gmail.com>
2023-05-14 11:55:35 +0600

    Time: 294 ms (56.22%), Space: 120.4 MB (52.33%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,5 +1,5 @@
 // @Author: KING-SEN
-
+/*
 class Solution {
 private:
     vector<vector<int>> cache_;
@@ -35,5 +35,36 @@ public:
         int len = nums1.size();
         cache_.resize(len + 1, vector<int>(2, -1));
         return solve(1, 0, nums1, nums2);
+    }
+};
+*/
+class Solution {
+public:
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.begin(), -1);
+        nums2.insert(nums2.begin(), -1);
+        int len = nums1.size();
+        vector<vector<int>> dp(len + 1, vector<int>(2, 0));
+        for (int index = len - 1; index >= 1; index--) {
+            for (int swapped = 1; swapped >= 0; swapped--) {
+                int answer = INT_MAX;
+                int prev1 = nums1[index - 1];
+                int prev2 = nums2[index - 1];
+                // Previous index swapped or not
+                if (swapped) {
+                    swap(prev1, prev2);
+                }
+                // No swap
+                if (prev1 < nums1[index] && prev2 < nums2[index]) {
+                    answer = dp[index + 1][0];
+                } 
+                // Swap
+                if (prev2 < nums1[index] && prev1 < nums2[index]) {
+                    answer = min(answer, 1 + dp[index + 1][1]);
+                }
+                dp[index][swapped] = answer;
+            }
+        }
+        return dp[1][0];
     }
 };
