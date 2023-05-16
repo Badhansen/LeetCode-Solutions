6e9ae07e78edd362a06da0dd1edbfc50f668c769
Badhan Sen <badhan.cse@gmail.com>
2023-05-17 00:43:53 +0600

    Time: 606 ms (73.02%), Space: 124.7 MB (71.43%) - LeetHub

--- 2345-finding-the-number-of-visible-mountains/2345-finding-the-number-of-visible-mountains.cpp
+++ 2345-finding-the-number-of-visible-mountains/2345-finding-the-number-of-visible-mountains.cpp
@@ -1,5 +1,8 @@
 // @Author: KING-SEN
+/*
+One can convert peak (x, y) of a mountain to an interval representing mountain base: [x - y; x + y]. Then the problem becomes number of intervals not contained in other intervals.
 
+*/
 class Solution {
 public:
     int visibleMountains(vector<vector<int>>& peaks) {
@@ -9,12 +12,15 @@ public:
             peaks[i][0] = x - y;
             peaks[i][1] = x + y;
         }
+        // Sorting is done by left boundary first is ascending order,
+	    // then by right in descending order.
         sort(peaks.begin(), peaks.end(), [](auto& a, auto& b) {
             if (a[0] == b[0]) {
                 return a[1] > b[1];
             }
             return a[0] < b[0];
         });
+        // Count number of intervals that are entirely inside other
         int invisible_count = 0, end = 0;
         for (int i = 0; i < len - 1;) {
             end = peaks[i][1];
