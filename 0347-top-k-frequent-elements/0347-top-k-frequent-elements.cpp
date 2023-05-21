0b7f0e0769c0a6421630637885e0105e805a1190
Badhan Sen <badhan.cse@gmail.com>
2023-05-21 22:25:27 +0600

    Time: 17 ms (61.04%), Space: 15.5 MB (7.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,52 +1,54 @@
-// class Solution {
-// public:
-//     vector<int> topKFrequent(vector<int>& nums, int k) {
-//         int length = nums.size();
-//         unordered_map<int, int> count;
-//         for(int i = 0; i < length; i++){
-//             count[nums[i]]++;
-//         }
-//         vector<vector<int>> buckets(length + 1);
-//         for(auto item : count){
-//             buckets[item.second].push_back(item.first);
-//         }
-//         vector<int> answer;
-//         for(int i = length; i >= 0; i--){
-//             if (answer.size() == k) {
-//                 break;
-//             }
-//             if (!buckets[i].empty()) {
-//                 answer.insert(answer.end(), buckets[i].begin(), buckets[i].end());
-//             }
-//         }
-//         return answer;
-//     }
-// };
-
-// Time: O(n)
-// Space: O(n)
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        int length = nums.size();
         unordered_map<int, int> count;
-        for (auto &n : nums) {
-            count[n]++;
+        for(int i = 0; i < length; i++){
+            count[nums[i]]++;
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
+        vector<vector<int>> buckets(length + 1);
         for (auto [key, val] : count) {
-            que.push({val, key});
-            if (que.size() > k) {
-                que.pop();
-            }
+            buckets[val].push_back(key);
         }
         vector<int> answer;
-        while (!que.empty()) {
-            answer.push_back(que.top().second);
-            que.pop();
+        for (int i = length; i >= 1 && k; i--) {
+            if (buckets[i].size()) {
+                int j = 0;
+                while (k && j < buckets[i].size()) {
+                    answer.push_back(buckets[i][j]);
+                    k--, j++;
+                }
+            }
         }
         return answer;
     }
 };
+
+// Time: O(n)
+// Space: O(n)
+
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int, int> count;
+//         for (auto &n : nums) {
+//             count[n]++;
+//         }
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
+//         for (auto [key, val] : count) {
+//             que.push({val, key});
+//             if (que.size() > k) {
+//                 que.pop();
+//             }
+//         }
+//         vector<int> answer;
+//         while (!que.empty()) {
+//             answer.push_back(que.top().second);
+//             que.pop();
+//         }
+//         return answer;
+//     }
+// };
 
 // Time: O(n log k)
 // Space: O(n + k)
