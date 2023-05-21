055247223114ec713f3ba8803ab18c8201891703
Badhan Sen <badhan.cse@gmail.com>
2023-05-21 21:29:18 +0600

    Time: 143 ms (7.96%), Space: 27.8 MB (11.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -10,10 +10,21 @@
 
 class Solution {
 public:
+    string GetKey(string src) {
+        vector<int> count(26, 0);
+        for (int i = 0; i < src.size(); i++) {
+            count[src[i] - 'a']++;
+        }
+        string key = "";
+        for (int i = 0; i < 26; i++) {
+            key.append(to_string(count[i]) + "a");
+        }
+        return key;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> m;
         for (int i = 0; i < strs.size(); i++) {
-            string key = getKey(strs[i]);
+            string key = GetKey(strs[i]);
             m[key].push_back(strs[i]);
         }
         
@@ -22,19 +33,6 @@ public:
             result.push_back(it->second);
         }
         return result;
-    }
-private:
-    string getKey(string str) {
-        vector<int> count(26);
-        for (int j = 0; j < str.size(); j++) {
-            count[str[j] - 'a']++;
-        }
-        
-        string key = "";
-        for (int i = 0; i < 26; i++) {
-            key.append(to_string(count[i] + 'a'));
-        }
-        return key;
     }
 };
 
