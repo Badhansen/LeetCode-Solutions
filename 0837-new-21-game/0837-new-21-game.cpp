a4b6194e6e7b96c4af09fa93c9b08a0f23f712c8
Badhan Sen <badhan.cse@gmail.com>
2023-05-26 02:20:26 +0600

    Time: 8 ms (30.16%), Space: 11.5 MB (10.32%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts -1) return 1.0;
+        vector<double> dp(n + maxPts + 1);
+        double prob=(double)1/(double)maxPts;
+        double sum=0.0;
+        int j=k-1+maxPts;
+        for(int i=n;i>=0;i--) {
+            if(i>=k) {
+                dp[i]=1.0;
+            }
+            else {
+                dp[i]+=prob*sum;
+                if(j-i==maxPts) {
+                    sum-=dp[j--];
+                }
+            }
+            sum+=dp[i];
+        }
+        return dp[0];
+    }
+};
