33c7a62561462cfbf3d1b3edf1e86c75dd404055
Badhan Sen <badhan.cse@gmail.com>
2023-05-27 19:15:57 +0600

    Time: 143 ms (69.38%), Space: 38 MB (61.83%) - LeetHub

---
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        multiset<int> window;
+        vector<double> answer;
+        
+        for (int i = 0; i < k; i++) {
+            window.insert(nums[i]);
+        }
+        auto it = next(window.begin(), (k - 1) / 2); 
+        for (int i = k; i <= n; i++) {            
+            double median = (k & 1) ? *it : (1ll * *it + *next(it)) / 2.00;
+            answer.push_back(median);
+            if (i == n) {
+                break;
+            }
+            // Insert nums[i]. if nums[i] > mid then mid iterator would not changed. but if I insert a smaller element it will incrase
+            window.insert(nums[i]);
+            if (nums[i] < *it)
+                it--;
+
+            // Erase nums[i-k]. if the erased element is smaller or equal the mid then it will decrease it pointer so we have to increase it
+            if (nums[i-k] <= *it)
+                it++;
+            window.erase(window.lower_bound(nums[i - k]));
+        }
+        return answer;
+    }
+};
