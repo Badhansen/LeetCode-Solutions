38aafa2f29fee827df132a7b306c15091e2da79e
Badhan Sen <badhan.cse@gmail.com>
2023-05-29 02:05:31 +0600

    Time: 313 ms (77.63%), Space: 132.7 MB (84.41%) - LeetHub

--- 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -11,33 +11,52 @@
  */
 class Solution {
 private:
-    unordered_map<int, int> left, right, height;
+    vector<int> start, end, depth;
 public:
-    int Height(TreeNode* root) {
+    int CountNode(TreeNode* root) {
         if (!root) {
             return 0;
         }
-        int lh = Height(root->left);
-        int rh = Height(root->right);
-        left[root->val] = lh;
-        right[root->val] = rh;
-        return max(lh, rh) + 1;
+        return 1 + CountNode(root->left) + CountNode(root->right);
     }
-    void Solve(TreeNode* root, int maxh, int depth) {
-        if (!root) {
-            return;
+    void CalculateDepth(TreeNode* root, int level) {
+        if (root->left) {
+            CalculateDepth(root->left, level + 1);
         }
-        height[root->val] = maxh;
-        Solve(root->left, max(maxh, depth + right[root->val]), depth + 1);
-        Solve(root->right, max(maxh, depth + left[root->val]), depth + 1);
+        if (root->right) {
+            CalculateDepth(root->right, level + 1);
+        }
+        depth[start[root->val]] = level;
+    }
+    void EulerTour(TreeNode* root, int& index) {
+        start[root->val] = index;
+        if (root->left) {
+            index++;
+            EulerTour(root->left, index);
+        }
+        if (root->right) {
+            index++;
+            EulerTour(root->right, index);
+        }
+        end[root->val] = index;
     }
     vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
-        Height(root);
-        Solve(root->left, right[root->val], 1);
-        Solve(root->right, left[root->val], 1);
+        int n = CountNode(root);
+        start.resize(n + 1), end.resize(n + 1), depth.resize(n + 1);
+        int index = 1;
+        EulerTour(root, index);
+        CalculateDepth(root, 0);
+        vector<int> prefix_max(n + 2, 0), suffix_max(n + 2, 0);
+        for (int j = 1; j <= n; j++) {
+            prefix_max[j] = max(depth[j], prefix_max[j - 1]);
+        }
+        for (int j = n; j >= 1; j--) {
+            suffix_max[j] = max(depth[j], suffix_max[j + 1]);
+        }
         vector<int> answer;
         for (auto &q : queries) {
-            answer.push_back(height[q]);
+            int l = start[q], r = end[q];
+            answer.push_back(max(prefix_max[l - 1], suffix_max[r + 1]));
         }
         return answer;
     }
