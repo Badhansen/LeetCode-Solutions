128e1653e71214d7729bc0cdbe654e8a7afc5a5f
Badhan Sen <badhan.cse@gmail.com>
2023-05-30 22:03:08 +0600

    Time: 19 ms (33.07%), Space: 15.9 MB (34.60%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> answer;
+    
+    void solve(int pos, int target, vector<int>& nums, vector<int> path){
+        if(target == 0){
+            answer.push_back(path);
+            return;
+        }
+        if(target < 0){
+            return;
+        }
+        
+        for(int i = pos; i < nums.size(); i++){
+            path.push_back(nums[i]);
+            solve(i, target - nums[i], nums, path); 
+            path.pop_back();   
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> path;
+        solve(0, target, candidates, path);
+        return answer;
+    }
+};
