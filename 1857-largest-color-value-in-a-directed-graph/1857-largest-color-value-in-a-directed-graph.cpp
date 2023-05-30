678c3a61040e7500dfac6a7d9924a223ae98c54e
Badhan Sen <badhan.cse@gmail.com>
2023-05-31 01:43:52 +0600

    Time: 737 ms (22.58%), Space: 183.2 MB (37.00%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,45 +1,40 @@
-// @Author: KING-SEN
-
+// Problem Link: https://leetcode.com/problems/largest-color-value-in-a-directed-graph/submissions/
+// AC
 class Solution {
-private:
-    unordered_set<int> visited, path;
-    vector<vector<int>> graph;
 public:
-    int dfs(int src, string& colors, vector<vector<int>>& count) {
-        if (path.count(src)) {
-            return INT_MAX;
-        }
-        if (visited.count(src)) {
-            return 0;
-        }
-        visited.insert(src);
-        path.insert(src);
-        int color_index = colors[src] - 'a';
-        count[src][color_index] = 1;
-        
-        for (auto dest : graph[src]) {
-            if (dfs(dest, colors, count) == INT_MAX) {
-                return INT_MAX;
-            }
-            for (int i = 0; i < 26; i++) {
-                count[src][i] = max((color_index == i ? 1 : 0) + count[dest][i], count[src][i]);
-            }
-        }
-        path.erase(src);
-        return *max_element(count[src].begin(), count[src].end());
-    }
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n = colors.size();
         int m = edges.size();
-        graph.resize(n);
+        vector<vector<int>> graph(n);
+        vector<int> indegree(n, 0);
+        queue<int> sources;
         vector<vector<int>> count(n, vector<int>(26, 0));
+        int answer = 0, nodeCount = 0;
+        
         for (auto e : edges) {
             graph[e[0]].push_back(e[1]);
+            indegree[e[1]]++;
         }
-        int answer = 0;
         for (int i = 0; i < n; i++) {
-            answer = max(answer, dfs(i, colors, count));
+            if (indegree[i] == 0) {
+                sources.push(i);
+            }
         }
-        return answer == INT_MAX ? -1 : answer;
+        while (!sources.empty()) {
+            int src = sources.front();
+            sources.pop();
+            answer = max(answer, ++count[src][colors[src] - 'a']);
+            nodeCount++;
+            for (int dest : graph[src]) {
+                for (int i = 0; i < 26; i++) {
+                    count[dest][i] = max(count[dest][i], count[src][i]);
+                }
+                indegree[dest]--;
+                if (indegree[dest] == 0) {
+                    sources.push(dest);
+                }
+            }
+        }
+        return nodeCount == n ? answer : -1;
     }
 };
