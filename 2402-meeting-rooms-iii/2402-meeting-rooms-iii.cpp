5e7db368e874af714eb56382d3ed55049f77c64f
Badhan Sen <badhan.cse@gmail.com>
2023-05-30 23:07:21 +0600

    Time: 588 ms (53.16%), Space: 96.7 MB (86.30%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -9,15 +9,15 @@ public:
         }  
     };
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        sort(meetings.begin(), meetings.end());
+        sort(meetings.begin(), meetings.end()); // O(M log M)
         priority_queue<pair<long long, int>, vector<pair<long long, int>>, compare> que;
-        vector<int> count(n, 0);
-        for (int i = 0; i < n; i++) {
+        vector<int> count(n, 0); // O(N)
+        for (int i = 0; i < n; i++) { // O(N)
             que.push({meetings[0][0], i});
         }
-        for (auto &meeting : meetings) {
+        for (auto &meeting : meetings) { // O(M)
             long long start = meeting[0], end = meeting[1];
-            while (que.top().first < start) {
+            while (que.top().first < start) { // O(N log N) ~ (100 * 2)
                 que.push({start, que.top().second});
                 que.pop();
             }
@@ -26,6 +26,8 @@ public:
             count[room]++;
             que.push({time + end - start, room});
         }
-        return max_element(count.begin(), count.end()) - count.begin();
+        return max_element(count.begin(), count.end()) - count.begin(); // O(N)
     }
 };
+// Time: O(M log M) ~ O(M log M + M * N) ~ (M * (N + log M))
+// Space: O(N)
