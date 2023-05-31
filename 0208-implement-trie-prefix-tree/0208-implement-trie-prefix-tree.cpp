9f5459d2743a29066fbb6c25598aaca118a3218f
Badhan Sen <badhan.cse@gmail.com>
2023-06-01 00:09:22 +0600

    Time: 84 ms (34.38%), Space: 43.1 MB (85.56%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,51 +1,54 @@
 class Trie {
-public:
-    bool isEnd;
-    Trie *child[26];
-    Trie() {
-        isEnd = false;
-        for(int i = 0; i < 26; i++){
-            child[i] = NULL;
+private:
+    struct TrieNode {
+        bool isend;
+        unordered_map<char, TrieNode*> child;
+        TrieNode() {
+            isend = false;
         }
-    }
+    };
+public:
+    TrieNode* root = new TrieNode();
     
     /** Inserts a word into the trie. */
     void insert(string word) {
-        Trie* temp = this;
-        for(int i = 0; i < word.size(); i++){
-            int id = word[i] - 'a';
-            if(temp->child[id] == NULL)
-                temp->child[id] = new Trie();
-            temp = temp->child[id];
+        TrieNode* curr = root;
+        for (auto c : word){
+            if (!curr->child.count(c)) {
+                curr->child[c] = new TrieNode();
+            }
+            curr = curr->child[c];
         }
-        temp->isEnd = true;
+        curr->isend = true;
     }
     
     /** Returns if the word is in the trie. */
     bool search(string word) {
-        Trie *temp = this;
-        for(int i = 0; i < word.size(); i++){
-            int id = word[i] - 'a';
-            if(temp->child[id] == NULL)
+        TrieNode *curr = root;
+        for (auto c : word){
+            if (!curr->child.count(c)) {
                 return false;
-            temp = temp->child[id];
+            }
+            curr = curr->child[c];
         }
-        if(temp->isEnd)
+        if (curr->isend) {
             return true;
+        }
         return false;
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
-        Trie *temp = this;
-        for(int i = 0; i < prefix.size(); i++){
-            int id = prefix[i] - 'a';
-            if(temp->child[id] == NULL)
+        TrieNode *curr = root;
+        for (auto c : prefix){
+            if (!curr->child.count(c)) {
                 return false;
-            temp = temp->child[id];
+            }
+            curr = curr->child[c];
         }
-        if(temp == NULL)
+        if (curr == NULL) {
             return false;
+        }
         return true;
     }
 };
