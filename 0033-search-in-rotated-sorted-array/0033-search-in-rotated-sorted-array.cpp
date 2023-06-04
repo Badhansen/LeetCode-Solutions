e9857ca03c80b903660af3321340c6aa914bb00b
Badhan Sen <badhan.cse@gmail.com>
2023-06-04 20:00:47 +0600

    Time: 5 ms (47.35%), Space: 11 MB (61.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int FindPivotIndex(const vector<int>& nums){
         int left = 0, right = nums.size() - 1;
-        while (left <= right) {
+        while (left < right) {
             int mid = (left + right) / 2;
             if (mid + 1 <= right) {
                 if (nums[mid] > nums[mid + 1]) {
                     return mid;
-                } else if (nums[left] > nums[mid]) {
+                } 
+                if (nums[left] > nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
-                right = mid - 1;
             }
         }
         return left;
