bf4ed8ca423c0fb2363a6064a4dd21cec170ceef
Badhan Sen <badhan.cse@gmail.com>
2023-06-04 23:53:08 +0600

    Time: 16 ms (84.45%), Space: 6.8 MB (97.74%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -29,3 +29,40 @@ public:
 Time: O(N*N)
 Space: O(1)
 */
+
+/*
+// @Author: KING-SEN
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, false));
+        int maxLength = 1;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        int start = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+        for (int p = 3; p <= n; p++) {
+            for (int i = 0; i < n - p + 1; i++) {
+                int j = i + p - 1;
+                if (dp[i + 1][j - 1] && s[i] == s[j]) {
+                    dp[i][j] = true;
+                    if (maxLength < p) {
+                        start = i;
+                        maxLength = p;
+                    }
+                }
+            }
+        }
+        return s.substr(start, maxLength);
+    }
+};
+*/
