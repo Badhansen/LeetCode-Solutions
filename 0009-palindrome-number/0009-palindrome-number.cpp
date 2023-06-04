4f172ac373ab3d82bf672ccadac1904c35e3921f
Badhan Sen <badhan.cse@gmail.com>
2023-06-04 14:40:20 +0600

    Time: 16 ms (57.23%), Space: 6 MB (50.23%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x < 0) return false;
-        long res = 0, t = x;
-        while(t) {
-            int r = t % 10;
-            res = res * 10 + r;
-            t /= 10;
-        }
-        return x == res ? true : false;
+        string rev = to_string(x);
+        reverse(rev.begin(), rev.end());
+        return rev == to_string(x);
+        // long res = 0, t = x;
+        // while(t) {
+        //     int r = t % 10;
+        //     res = res * 10 + r;
+        //     t /= 10;
+        // }
+        // return x == res ? true : false;
     }
 };
