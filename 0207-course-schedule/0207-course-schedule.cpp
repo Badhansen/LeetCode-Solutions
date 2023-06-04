feb5b349199a61627ba8599585eb3eb4a859a42d
Badhan Sen <badhan.cse@gmail.com>
2023-06-04 22:15:53 +0600

    Time: 20 ms (85.33%), Space: 12.9 MB (98.89%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,3 +1,37 @@
+//@Author: KING-SEN
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> graph[numCourses + 1];
+        vector<int> indeg(numCourses + 1, 0);
+        for(auto &el : prerequisites){
+            indeg[el[0]]++;
+            graph[el[1]].push_back(el[0]);
+        }
+        vector<bool> visited(numCourses + 1, false);
+        vector<int> course;
+        for(int i = 0; i < numCourses; i++){
+            if(indeg[i] == 0) course.push_back(i);
+        }
+        
+        int done = 0,id = 0;
+        while(id < course.size()){
+            done++;
+           
+            int curr = course[id];
+            for(auto &v: graph[curr]){
+                if(indeg[v] > 0){
+                    indeg[v]--;
+                    if(indeg[v] == 0) course.push_back(v);
+                }
+            }
+            id++;
+        }
+        return done == numCourses;
+    }
+};
+/*
 class Solution {
 private:
     vector<vector<int>> graph;
@@ -34,3 +68,4 @@ public:
         return true;
     }
 };
+*/
