71953f58e7e6537b9e8cb2a8e1515274c887bb21
Badhan Sen <badhan.cse@gmail.com>
2023-06-05 23:00:33 +0600

    Time: 31 ms (42.86%), Space: 46.4 MB (42.86%) - LeetHub

---
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int semiOrderedPermutation(vector<int>& nums) {
+        int f = 1, l = nums.size(), res = 0;
+        if (nums[0] != f) {
+            int pos = find(nums.begin(), nums.end(), f) - nums.begin();
+            if (pos != nums.size()) {
+                res += pos;
+                while (pos > 0) {
+                    cout << pos << endl;
+                    swap(nums[pos - 1], nums[pos]);
+                    pos--;
+                }
+            }
+        }
+        if (nums[nums.size() - 1] != l) {
+            int pos = find(nums.begin(), nums.end(), l) - nums.begin();
+            if (pos != nums.size()) {
+                while (pos < nums.size() - 1) {
+                    swap(nums[pos + 1], nums[pos]);
+                    pos++, res++;
+                }
+            }
+        }
+        return res;
+    }
+};
