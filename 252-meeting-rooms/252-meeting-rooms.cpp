9831c85cd43e4798cca83aa921cf768e4cd95d3e
Badhan Sen <badhan.cse@gmail.com>
2023-06-07 00:42:04 +0600

    Time: 28 ms (14.86%), Space: 13.4 MB (12.65%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.cpp
+++ 252-meeting-rooms/252-meeting-rooms.cpp
@@ -21,3 +21,23 @@ public:
         return answer == 1 ? true : false;
     }
 };
+/*
+// @Author: KING-SEN
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if(!intervals.size()) {
+            return true;
+        }
+        sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b){
+            return a[1] < b[1];
+        });
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+*/
