ed0c461cfdea84555b5d302fa1b6b4a860287d8d
Badhan Sen <badhan.cse@gmail.com>
2023-06-07 12:48:14 +0600

    Time: 5 ms (83.48%), Space: 18.9 MB (40.42%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -38,4 +38,13 @@ public:
         return height;
     }
 };
-
+/*
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+*/
