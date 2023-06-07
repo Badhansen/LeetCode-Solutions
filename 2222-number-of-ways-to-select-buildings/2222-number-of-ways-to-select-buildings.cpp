4bcc078637ace790d44a29ae433238baa54336c2
Badhan Sen <badhan.cse@gmail.com>
2023-06-07 23:29:52 +0600

    Time: 97 ms (86.00%), Space: 24.3 MB (80.15%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -1,38 +1,30 @@
-const int kMax = 1e5 + 5;
-
 class Solution {
-private:
-    long long dp[kMax][3][4];
 public:
-    long long solve(int pos, int type, int sum, string& s) {
-        //cout << pos << " " << type << " " << sum << endl;
-        if (sum == 3) {
-            return 1;
-        }
-        if (pos == s.size()) {
-            return 0;
-        }
-        long long &ret = dp[pos][type][sum];
-        if (ret != -1) {
-            return ret;
-        }
-        ret = 0;
-        if (s[pos] == '0') {
-            if (type == 1 ||  type == 2) {
-                ret += solve(pos + 1, 0, sum + 1, s);
-            }
-            ret += solve(pos + 1, type, sum, s);
-        } else {
-            if (type == 0 || type == 2) {
-                ret += solve(pos + 1, 1, sum + 1, s);
-            }
-            ret += solve(pos + 1, type, sum, s);
-        }
-        return ret;
-    }
     long long numberOfWays(string s) {
-        memset(dp, -1, sizeof dp);
-        long long answer = solve(0, 2, 0, s);
+        int ones = 0, zeros = 0;
+        for (auto c : s) {
+            if (c == '0') {
+                zeros++;
+            } else {
+                ones++;
+            }
+        }
+        int prevOnes = 0, prevZeros = 0;
+        long long answer = 0;
+        for (auto c : s) {
+            if (c == '0') {
+                zeros--;
+            } else {
+                ones--;
+            }
+            if (c == '0') {
+                answer += (ones * prevOnes);
+                prevZeros++;
+            } else {
+                answer += (zeros * prevZeros);
+                prevOnes++;
+            }
+        }
         return answer;
     }
 };
