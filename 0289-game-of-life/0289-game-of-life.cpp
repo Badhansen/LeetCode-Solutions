8f2e88d98d36fda5525369c5c8a03d4a34559b33
Badhan Sen <badhan.cse@gmail.com>
2023-06-08 23:22:34 +0600

    Time: 3 ms (39.77%), Space: 7.3 MB (6.61%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -5,29 +5,34 @@ public:
     
     void gameOfLife(vector<vector<int>>& board) {
         int rows = board.size(), cols = board[0].size();
-        vector<vector<int>> res(rows, vector<int>(cols, 0));
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 int count = 0;
                 for (auto dir : directions) {
                     int x = i + dir[0], y = j + dir[1];
                     if (x >= 0 && x < rows && y >= 0 && y < cols) {
-                        if (board[x][y]) {
+                        if (board[x][y] & 1) {
                             count++;
                         }
                     }
                 }
-                if (board[i][j] && count < 2) {
-                    res[i][j] = 0;
-                } else if (board[i][j] && (count == 2 || count == 3)) {
-                    res[i][j] = 1;
-                } else if (board[i][j] && count > 3) {
-                    res[i][j] = 0;
-                } else if (!board[i][j] && count == 3) {
-                    res[i][j] = 1;
-                } 
+                board[i][j] |= (count << 1);
             }
         }
-        board = res;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int value = board[i][j] & 1, count = (board[i][j] >> 1);
+                //cout << value << " " << count << endl;
+                if (value && (count < 2 || count > 3))  {
+                    board[i][j] = 0;
+                } else if (value && (count == 2 || count == 3)) {
+                    board[i][j] = value ? 1 : 0;
+                } else if (count == 3 && !value) {
+                    board[i][j] = 1;
+                } else {
+                    board[i][j] = 0;
+                }
+            }
+        }
     }
 };
