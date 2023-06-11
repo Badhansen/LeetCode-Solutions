45d901e435434b67caae0b388c7f9cede674bce3
Badhan Sen <badhan.cse@gmail.com>
2023-06-12 02:55:55 +0600

    Time: 448 ms (30.00%), Space: 133 MB (10.00%) - LeetHub

--- 2732-find-a-good-subset-of-the-matrix/2732-find-a-good-subset-of-the-matrix.cpp
+++ 2732-find-a-good-subset-of-the-matrix/2732-find-a-good-subset-of-the-matrix.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     vector<int> goodSubsetofBinaryMatrix(vector<vector<int>>& grid) {
@@ -25,3 +26,45 @@ public:
         return {};
     }
 };
+*/
+/*
+Time: O(Row ^ 2)
+Space: O(Row)
+
+*/
+class Solution {
+public:
+    vector<int> goodSubsetofBinaryMatrix(vector<vector<int>>& grid) {
+        int r = grid.size(), c = grid[0].size();
+        int n = (1 << c);
+        vector<int> maps(n, -1);
+        for (int i = 0; i < r; i++) {
+            int sum = 0;
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j]) {
+                    sum |= (1 << j);
+                }
+            }
+            if (!sum) {
+                return {i};
+            }
+            maps[sum] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            if (maps[i] == -1) {
+                continue;
+            }
+            for (int j = i + 1; j < n; j++) {
+                if (maps[j] == -1) {
+                    continue;
+                }
+                if ((i & j) == 0) {
+                    
+                    return {min(maps[i], maps[j]), max(maps[i], maps[j])};
+                }
+            }
+        }
+        return {};
+    }
+};
+
