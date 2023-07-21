d5a7b75b5b600fb4c1c3a9b4102bfd15b2d6d702
Badhan Sen <badhan.cse@gmail.com>
2023-07-22 01:00:51 +0600

    Time: 2869 ms (5.02%), Space: 706.5 MB (5.32%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -1,24 +1,20 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-        dp = {}
-        
+        @functools.lru_cache(None)
         def solve(i, prev):
             if i == len(nums):
                 return 0, 1
-            if (i, prev) in dp:
-                return dp[(i, prev)]
             len1, len2, cnt1, cnt2 = 0, 0, 0, 0
             if nums[i] > prev:
                 len1, cnt1 = solve(i + 1, nums[i])
                 len1 += 1
             len2, cnt2 = solve(i + 1, prev)
             if len1 == len2:
-                dp[(i, prev)] = len1, cnt1 + cnt2
+                return len1, cnt1 + cnt2
             elif len1 > len2:
-                dp[(i, prev)] = len1, cnt1
+                return len1, cnt1
             else:
-                dp[(i, prev)] = len2, cnt2
-            return dp[(i, prev)]
+                return len2, cnt2
         
         return solve(0, float('-inf'))[1]
             
