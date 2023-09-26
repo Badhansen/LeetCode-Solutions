4e21b5a3619111f7c905868832efa6ac4a7a88ac
Badhan Sen <badhan.cse@gmail.com>
2023-09-26 13:48:19 +0600

    Time: 5 ms (24.56%), Space: 7.1 MB (10.66%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> lastIndex(26, 0);
+        vector<bool> seen(26, false);
+        int len = s.size();
+        for(int i = 0; i < len; i++){
+            lastIndex[s[i] - 'a'] = i;
+        }
+        stack<char> result;
+        for(int i = 0; i < len; i++){
+            int id = s[i] - 'a';
+            if(seen[id]) continue;
+            while(!result.empty() && result.top() > s[i] && lastIndex[result.top() - 'a'] > i){
+                seen[result.top() - 'a'] = false;
+                result.pop();
+            }
+            result.push(s[i]);
+            seen[s[i] - 'a'] = true;
+        }
+        string answer;
+        while(!result.empty()){
+            answer.push_back(result.top());
+            result.pop();
+        }
+        reverse(answer.begin(), answer.end());
+        return answer;
+    }
+};
