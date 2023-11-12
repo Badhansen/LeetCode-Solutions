be2b58e52c685c4efedd3d427d9b343aa3fdcbb4
Badhan Sen <badhan.cse@gmail.com>
2023-11-12 00:19:27 +0000

    Time: 591 ms (16.28%), Space: 224 MB (5.00%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    unordered_map<string, bool> cache;
-    string hashState(int index, vector<int> sums) {
-        sort(sums.begin(), sums.end());
-        string state = to_string(index);
-        for (auto &s : sums) {
-            state.append("|" + to_string(s));
-        }
-        return state;
+    unordered_map<long long, bool> cache;
+    vector<int> matchsticks;
+    long long hashState(int i, int s1, int s2, int s3, int s4) {
+        return ((long long)i | ((long long)s1 << 15) | ((long long)s2 << 30) | ((long long)s3 << 45) | ((long long)s4 << 60));
     }
-    bool solve(int index, vector<int>& sums, int target, vector<int>& sticks) {
-        string state = hashState(index, sums);
-        if (index >= sticks.size()) {
-            return cache[state] = (sums[0] == sums[1] && sums[1] == sums[2] && sums[2] == sums[3]);
-        }
+    bool solve(int i, int s1, int s2, int s3, int s4) {
+        long long state = hashState(i, s1, s2, s3, s4);
         if (cache.count(state)) {
             return cache[state];
         }
-        cache[state] = false;
-        for (int i = 0; i < 4; i++) {
-            if (sums[i] + sticks[index] <= target) {
-                sums[i] += sticks[index];
-                cache[state] = cache[state] || solve(index + 1, sums, target, sticks);
-                sums[i] -= sticks[index];
-            }
+        if (i >= matchsticks.size()) {
+            return (s1 == s2 && s2 == s3 && s3 == s4);
         }
+        cache[state] = (s1 >= matchsticks[i] && solve(i + 1, s1 - matchsticks[i], s2, s3, s4))
+                    || (s2 >= matchsticks[i] && solve(i + 1, s1, s2 - matchsticks[i], s3, s4))
+                    || (s3 >= matchsticks[i] && solve(i + 1, s1, s2, s3 - matchsticks[i], s4))
+                    || (s4 >= matchsticks[i] && solve(i + 1, s1, s2, s3, s4 - matchsticks[i]));
         return cache[state];
     }
-    bool makesquare(vector<int>& matchsticks) {
-        int sum = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+    bool makesquare(vector<int>& m) {
+        int sum = accumulate(m.begin(), m.end(), 0);
+        this->matchsticks = m;
         if (sum % 4) {
             return false;
         }
         sort(matchsticks.rbegin(), matchsticks.rend());
-        vector<int> sums(4, 0);
-        return solve(0, sums, sum / 4, matchsticks);
+        return solve(0, sum / 4, sum / 4, sum / 4, sum / 4);
     }
 };
