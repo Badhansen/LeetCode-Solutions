9499b9eacbfa1f452da01ce38a146b16d913e4f0
Badhan Sen <badhan.cse@gmail.com>
2023-12-24 23:31:17 +0000

    Time: 4 ms (45.24%), Space: 7 MB (47.62%) - LeetHub

---
+++ 1864-minimum-number-of-swaps-to-make-the-binary-string-alternating/1864-minimum-number-of-swaps-to-make-the-binary-string-alternating.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int start0 = 0, start1 = 0, zeros = 0; 
+        for (int i = 0; i < s.size(); i++) {
+            if (i % 2 == 0) {
+                s[i] == '0' ? start1++ : start0++;
+            } else {
+                s[i] == '0' ? start0++ : start1++;
+            }
+            s[i] == '0' ? zeros++ : true;
+        }
+        int ones = (int)s.size() - zeros;
+        if (abs(ones - zeros) > 1) {
+            return -1;
+        }
+        if (zeros == ones) {
+            return min(start0, start1) / 2;
+        }
+        return zeros > ones ? start0 / 2 : start1 / 2;
+    }
+};
