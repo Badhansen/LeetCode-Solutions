bf444e73a96d74f9e1fe2568f7df475632b9205a
Badhan Sen <badhan.cse@gmail.com>
2023-12-25 13:14:22 +0000

    Time: 4 ms (23.71%), Space: 6.5 MB (87.76%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,31 +1,23 @@
+// Iterative solution
 class Solution {
-private:
-    vector<int> dp;
 public:
-    int ways(int pos, string &s) {
-        if (pos >= s.size()) {
-            return 1;
-        }
-        int &result = dp[pos];
-        if (result != -1) {
-            return result;
-        }
-        result = 0;
-        int take = s[pos] - '0';
-        if (take) {
-            result = ways(pos + 1, s);
-        }
-        if (pos + 1 < s.size()) {
-            take = stoi(s.substr(pos, 2));
-            if (take >= 10 && take <= 26) {
-                result += ways(pos + 2, s);
+    int numDecodings(string s) {
+        int len = s.size();
+        vector<int> dp(len + 1);
+        dp[0] = 1;
+        dp[1] = s[0] == '0' ?  0 : 1;
+        for (int i = 2; i <= len; i++) {
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];
+            }
+            int number = stoi(s.substr(i - 2, 2));
+            if (number >= 10 && number <= 26) {
+                dp[i] += dp[i - 2];
             }
         }
-        return result;
-    }
-    int numDecodings(string s) {
-        dp.clear();
-        dp.resize(s.size(), -1);
-        return ways(0, s);
+        return dp[len];
     }
 };
+
+// Time: O(N)
+// Space: O(N)
