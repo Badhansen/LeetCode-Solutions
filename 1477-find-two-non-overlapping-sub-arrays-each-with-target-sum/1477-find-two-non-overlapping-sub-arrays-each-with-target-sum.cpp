9e76f940255e4d2612ceff323f80c82e9bdb7ec6
Badhan Sen <badhan.cse@gmail.com>
2024-01-13 00:48:26 +0000

    Time: 516 ms (14.93%), Space: 198.7 MB (14.03%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,3 +1,16 @@
+/*
+
+    1. precalculate at each position, i, the min length of array with target sum from left and from right respectively.
+        a. fromLeft[i]: from the leftmost to position, i, (i.e. [0 .. i]), the min length of the array with target sum
+        b. fromRight[i]: from the rightmost to position, i, (i.e. [i .. n-1]), the min length of the array with target sum
+    2. use DP to find the min sum of length at each pisition, i. (i.e. , dp[i] = fromLeft[i] + fromRight[i+1])
+        a. consider the minimal sum of two arrays' length at each index, i. (i.e. min sum of length at index i = min length in the range of [0 .. i] + min length in the range of [i+1 .. n-1])
+        b. reverse the given array and apply the same subfunction to simply the code.
+    3. complexity
+        a. Time complexity : O(N)
+        b. Space complexity: O(N)
+
+*/
 class Solution {
 public:
     vector<int> minLen(vector<int> &nums, int target) {
