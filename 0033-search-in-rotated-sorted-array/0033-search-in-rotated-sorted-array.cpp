9af5a38830b8c5071fb4d5d58ed9670765359ea7
Badhan Sen <badhan.cse@gmail.com>
2024-01-28 22:08:42 +0000

    Time: 0 ms (100.00%), Space: 13.7 MB (5.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int FindPivotIndex(const vector<int>& nums){
         int left = 0, right = nums.size() - 1;
-        while (left <= right) {
+        while (left < right) {
             int mid = (left + right) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            // Left is sorted
-            if (nums[left] <= nums[mid]) {
-                if (target < nums[left] || target > nums[mid]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } else { // Right is sorted
-                if (target > nums[right] || target < nums[mid]) {
+            if (mid + 1 <= right) {
+                if (nums[mid] > nums[mid + 1]) {
+                    return mid;
+                } 
+                if (nums[left] > nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
+            } else {
+                right = mid - 1;
             }
+        }
+        return left;
+    }
+    int search(vector<int>& nums, int target) {
+        int index = FindPivotIndex(nums);
+        if (index == nums.size()) {
+            auto pos = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+            if (pos == nums.size() || pos == 0 && nums[pos] != target) {
+                return -1;
+            }
+            return pos;
+        }
+        auto pos = lower_bound(nums.begin(), nums.begin() + index + 1, target) - nums.begin();
+        if (pos < nums.size() && nums[pos] == target) {
+            return pos;
+        } 
+        pos = lower_bound(nums.begin() + index + 1, nums.end(), target) - nums.begin();
+        if (pos > index && pos < nums.size() && nums[pos] == target) {
+            return pos;
         }
         return -1;
     }
