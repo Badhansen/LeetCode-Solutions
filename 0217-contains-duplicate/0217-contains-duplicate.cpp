75ff8bc68296d9bf851fed4ee165876d97afb55f
Badhan Sen <badhan.cse@gmail.com>
2024-01-29 23:13:11 +0000

    Time: 85 ms (91.19%), Space: 73.1 MB (18.05%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,19 +1,53 @@
-// Programming Language Used: C++
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> seen;
-        for(auto &num : nums){
-            if(seen.count(num)) {
-                return true;
-            }
-            seen[num] = 1;
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)
+            return root;
+        
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        
+        root->left = right;
+        root->right = left;
+        
+        return root;
+    }
+};
+/*
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL) return NULL;
+        
+        stack<TreeNode*> seen;
+        seen.push(root);
+        
+        while(!seen.empty()){
+            TreeNode* node = seen.top();
+            seen.pop();
+            if(node->left) seen.push(node->left);
+            if(node->right) seen.push(node->right);
+            swap(node->left, node->right);
+            // TreeNode* temp = node->left;
+            // node->left = node->right;
+            // node->right = temp;
+            
         }
-        return false;
+        return root;
     }
 };
 
-// Here N = Number of elements
-// Time Complexity: O(N)
-// Space Complexity: O(N)
+// Time complexity: O(N), where N is the total number of nodes
+// Space complexity: O(N), since in the worst case, the queue will contain all nodes in one level of the binary tree.
+*/
