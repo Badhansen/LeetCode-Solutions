d3e1290cdc3041d29ac9047f915bd1042f3f451c
Badhan Sen <badhan.cse@gmail.com>
2024-01-30 12:08:44 +0000

    Time: 330 ms (94.01%), Space: 182.3 MB (7.59%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,8 +1,8 @@
-class Node{
+class Node {
 public:
     int key, value;
     Node* next, *prev;
-    Node(int key, int value){
+    Node(int key, int value) {
         this->key = key;
         this->value = value;
         next = prev = NULL;
@@ -21,41 +21,36 @@ public:
         head->next = tail;
         tail->prev = head;
     }
-    ~LRUCache(){
-        cache.clear();
-    }
-    void remove(Node* currNode){
-        Node* nextNode = currNode->next;
-        Node* prevNode = currNode->prev;
-        nextNode->prev = prevNode;
-        prevNode->next = nextNode;
-        cache.erase(currNode->key);
-    }
-    void add(Node* currNode){
+    void add(Node* currNode) {
         Node* nextNode = head->next;
-        nextNode->prev = currNode;
         currNode->next = nextNode;
+        nextNode->prev = currNode;
         currNode->prev = head;
         head->next = currNode;
         cache[currNode->key] = currNode;
     }
+    void remove(Node* currNode) {
+        Node* nextNode = currNode->next;
+        Node* prevNode = currNode->prev;
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+        cache.erase(currNode->key);
+    }
     int get(int key) {
-        if(cache.find(key) != cache.end()){
+        if (cache.find(key) != cache.end()) {
             Node* currNode = cache[key];
             remove(currNode);
             add(currNode);
             return currNode->value;
         }
-        else{
-            return -1;
-        }
+        return -1;
     }
     
     void put(int key, int value) {
-        if(cache.find(key) != cache.end()){
+        if (cache.find(key) != cache.end()) {
             remove(cache[key]);
         }
-        if(cache.size() == capacity){
+        if (cache.size() == capacity) {
             remove(tail->prev);
         }
         Node* newNode = new Node(key, value);
