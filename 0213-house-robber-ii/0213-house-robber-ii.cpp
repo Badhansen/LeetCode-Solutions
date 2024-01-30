ccb5457000429fbeed1d12e9601827de08d12956
Badhan Sen <badhan.cse@gmail.com>
2024-01-30 00:01:10 +0000

    Time: 4 ms (30.69%), Space: 9.4 MB (9.45%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -3,30 +3,55 @@
 
 class Solution {
 public:
-    int rob(vector<int>& nums, int start, int end){
-        int include = 0;
-        int exclude = 0;
-        
-        for(int i = start; i <= end; i++){
-            int in = include, ex = exclude;
-            include = ex + nums[i];
-            exclude = max(in, ex);
+    int solve(int pos, int n, vector<int>& nums, vector<int>& dp){
+        if(pos > n){
+            return 0;
         }
-        
-        return max(include, exclude);
+        if(dp[pos] != -1) return dp[pos];
+        int left = solve(pos + 1, n, nums, dp);
+        int right = nums[pos] + solve(pos + 2, n, nums, dp);
+        return dp[pos] = max(left, right);
     }
     
     int rob(vector<int>& nums) {
         int length = nums.size();
     
         if(length == 1) return nums[0];
-        
-        int maxOne = rob(nums, 1, length - 1);
-        int maxTwo = rob(nums, 0, length - 2);
+        vector<int> dp(length, -1);
+        int maxOne = solve(1, length - 1, nums, dp);
+        fill(dp.begin(), dp.end(), -1);
+        int maxTwo = solve(0, length - 2, nums, dp);
         
         return max(maxOne, maxTwo);
     }
 };
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums, int start, int end){
+//         int include = 0;
+//         int exclude = 0;
+        
+//         for(int i = start; i <= end; i++){
+//             int in = include, ex = exclude;
+//             include = ex + nums[i];
+//             exclude = max(in, ex);
+//         }
+        
+//         return max(include, exclude);
+//     }
+    
+//     int rob(vector<int>& nums) {
+//         int length = nums.size();
+    
+//         if(length == 1) return nums[0];
+        
+//         int maxOne = rob(nums, 1, length - 1);
+//         int maxTwo = rob(nums, 0, length - 2);
+        
+//         return max(maxOne, maxTwo);
+//     }
+// };
 
 // Time Complexity: O(N), here N = Length of the Given array
 // Space Complexity: O(1), we are using only constant space
