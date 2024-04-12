44a14c52634fe7b5699966715028747784338e31
Badhan Sen <badhan.cse@gmail.com>
2024-04-12 03:59:45 +0100

    Time: 467 ms (45.18%), Space: 29.8 MB (46.71%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,11 +1,27 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = {}
+        seen = dict()
         for i, n in enumerate(nums):
             if n in seen and abs(seen[n] - i) <= k:
                 return True
             seen[n] = i
+        if len(seen) > k:
+            seen.pop(nums[i - k]) # Removing the 1th item from (K + 1) length array
         return False
+
+'''
+Time Complexity: O(N)
+Space Complexity: O(min(N, K))
+'''
+
+# class Solution:
+#     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+#         seen = {}
+#         for i, n in enumerate(nums):
+#             if n in seen and abs(seen[n] - i) <= k:
+#                 return True
+#             seen[n] = i
+#         return False
 
 '''
 Time Complexity: O(N)
