ffe0b188513d224a8b6b3ee64b672f7b1107d2e0
Badhan Sen <badhan.cse@gmail.com>
2024-04-13 22:53:22 +0100

    Time: 82 ms (87.87%), Space: 22.2 MB (21.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,27 @@
+from collections import defaultdict, Counter
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
         for word in strs:
-            sword = ''.join(sorted(word))
-            anagrams[sword].append(word)
+            counts = [0] * 26
+            for c in word:
+                counts[ord(c) - ord('a')] += 1
+            anagrams[tuple(counts)].append(word)
         return anagrams.values()
+'''
+Time complexity: O(N * K), where N is the length of strs, and K is the maximum length of a string in strs.
+Counting each string is linear in the size of the string, and we count every string.
+Space complexity: O(N * K), total information store in anagrams
+'''
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         anagrams = defaultdict(list)
+#         for word in strs:
+#             sword = ''.join(sorted(word))
+#             anagrams[sword].append(word)
+#         return anagrams.values()
 
 '''
 Time complexity: O(N * K log K), where N is the length of strs, and K is the maximum length of a string in strs.
