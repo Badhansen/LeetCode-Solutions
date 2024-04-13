8edd176829c75ae0f28454941a11e50a22a8c3fb
Badhan Sen <badhan.cse@gmail.com>
2024-04-13 21:49:37 +0100

    Time: 45 ms (77.73%), Space: 17.1 MB (34.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,3 +4,8 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         a, b = Counter(s), Counter(t)
         return a == b
+    
+'''
+Time Complexity: O(N + M)
+Space Complexity: O(N + M)
+'''
