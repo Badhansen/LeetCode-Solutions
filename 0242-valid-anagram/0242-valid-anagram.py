9f22c38aa4c105fb88715303964d3e6628e40b39
Badhan Sen <badhan.cse@gmail.com>
2024-04-13 21:54:02 +0100

    Time: 55 ms (28.70%), Space: 16.9 MB (64.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,26 @@
-from collections import Counter
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        a, b = Counter(s), Counter(t)
-        return a == b
+        if len(s) != len(t):
+            return False
+        count_s, count_t = defaultdict(int), defaultdict(int)
+        for i in range(len(s)):
+            count_s[s[i]] = count_s[s[i]] + 1
+            count_t[t[i]] = count_t[t[i]] + 1
+        return count_s == count_t
     
 '''
 Time Complexity: O(N + M)
 Space Complexity: O(N + M)
 '''
+
+# from collections import Counter
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         a, b = Counter(s), Counter(t)
+#         return a == b
+    
+# '''
+# Time Complexity: O(N + M)
+# Space Complexity: O(N + M)
+# '''
