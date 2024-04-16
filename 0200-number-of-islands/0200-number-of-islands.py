4e21fe9f09fcd30a6591f729d2fcf1d9ecb387e4
Badhan Sen <badhan.cse@gmail.com>
2024-04-16 23:37:42 +0100

    Time: 259 ms (43.63%), Space: 24 MB (39.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,55 @@
 # @Author: KING-SEN
 # Skimming python code
 
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        row = len(grid); col = len(grid[0])
-        res = 0
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         row = len(grid); col = len(grid[0])
+#         res = 0
         
-        def dfs(r, c):
-            if (not 0 <= r < row) or (not 0 <= c < col) or (grid[r][c] == '0'):
-                return
-            grid[r][c] = '0'
-            dfs(r - 1, c)
-            dfs(r + 1, c)
-            dfs(r, c - 1)
-            dfs(r, c + 1)
+#         def dfs(r, c):
+#             if (not 0 <= r < row) or (not 0 <= c < col) or (grid[r][c] == '0'):
+#                 return
+#             grid[r][c] = '0'
+#             dfs(r - 1, c)
+#             dfs(r + 1, c)
+#             dfs(r, c - 1)
+#             dfs(r, c + 1)
         
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == '1':
-                    res += 1
-                    dfs(r, c)
-        return res
+#         for r in range(row):
+#             for c in range(col):
+#                 if grid[r][c] == '1':
+#                     res += 1
+#                     dfs(r, c)
+#         return res
+
     
 # Time: O(row * col)
 # Space: O(row * col), because of recursion call stack
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row = len(grid); col = len(grid[0])
+        res, visited = 0, set()
+        direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        
+        def bfs(r, c):
+            q = deque()
+            q.append((r, c))
+            while q:
+                a, b = q.popleft()
+                for dr, dc in direction:
+                    i, j = a + dr, b + dc
+                    if (0 <= i < row) and (0 <= j < col) and ((i, j) not in visited) and grid[i][j] == "1":
+                        q.append((i, j))
+                        visited.add((i, j)) 
+        
+        for r in range(row):
+            for c in range(col):
+                if (r, c) not in visited and grid[r][c] == '1':
+                    res += 1
+                    bfs(r, c)
+                
+        return res
+    
+# Time: O(row * col)
+# Space: O(min(row * col)), because in worst case where the grid is filled with lands, the size of queue can grow up to min(M, N).
