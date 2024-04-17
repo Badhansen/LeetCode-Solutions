c335aebecf59077f5eb77af3307ecd8f6bfb65e5
Badhan Sen <badhan.cse@gmail.com>
2024-04-17 14:48:46 +0100

    Time: 171 ms (89.89%), Space: 19 MB (84.21%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,29 +1,55 @@
-class ListNode:
-    def __init__(self, val, prev=None, next=None):
-        self.val = val
-        self.prev = prev
-        self.next = next
+# class ListNode:
+#     def __init__(self, val, prev=None, next=None):
+#         self.val = val
+#         self.prev = prev
+#         self.next = next
+
+# class BrowserHistory:
+#     def __init__(self, homepage: str):
+#         self.curr = ListNode(homepage)
+#     # O(1)
+#     def visit(self, url: str) -> None:
+#         self.curr.next = ListNode(url, self.curr)
+#         self.curr = self.curr.next
+    
+#     # O(n)
+#     def back(self, steps: int) -> str:
+#         while self.curr.prev and steps > 0:
+#             self.curr = self.curr.prev
+#             steps -= 1
+#         return self.curr.val
+
+#     # O(n)
+#     def forward(self, steps: int) -> str:
+#         while self.curr.next and steps > 0:
+#             self.curr = self.curr.next
+#             steps -= 1
+#         return self.curr.val
 
 class BrowserHistory:
     def __init__(self, homepage: str):
-        self.curr = ListNode(homepage)
-
+        self.i = 0
+        self.len = 1
+        self.history = [homepage]
+        
+    # O(1)
     def visit(self, url: str) -> None:
-        self.curr.next = ListNode(url, self.curr)
-        self.curr = self.curr.next
-
+        if self.i + 1 < len(self.history):
+            self.history[self.i + 1] = url
+        else:
+            self.history.append(url)
+        self.i += 1
+        self.len = self.i + 1
+    
+    # O(1)
     def back(self, steps: int) -> str:
-        while self.curr.prev and steps > 0:
-            self.curr = self.curr.prev
-            steps -= 1
-        return self.curr.val
+        self.i = max(0, self.i - steps)
+        return self.history[self.i]
 
+    # O(1)
     def forward(self, steps: int) -> str:
-        while self.curr.next and steps > 0:
-            self.curr = self.curr.next
-            steps -= 1
-        return self.curr.val
-
+        self.i = min(self.len - 1, self.i + steps)
+        return self.history[self.i]
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
