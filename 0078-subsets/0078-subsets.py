b8d55e9091f366e93bb68d90326cb05dd1509a51
Badhan Sen <badhan.cse@gmail.com>
2024-04-18 11:33:53 +0100

    Time: 30 ms (92.77%), Space: 16.6 MB (78.15%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -10,16 +10,32 @@
 #             result.append(res)
 #         return result
 
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         def dfs(i, subset):
+#             if i >= len(nums):
+#                 res.append(subset)
+#                 return
+#             dfs(i + 1, subset + [nums[i]])
+#             dfs(i + 1, subset)
+#         dfs(0, [])
+#         return res
+
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums):
         res = []
-        def dfs(i, subset):
+        subset = []  # Global variable to use in all dfs calls
+        def dfs(i):
             if i >= len(nums):
-                res.append(subset)
-                return
-            dfs(i + 1, subset + [nums[i]])
-            dfs(i + 1, subset)
-        dfs(0, [])
+                res.append(subset.copy())  # appending a copy of original global subset 
+                return  # to stop this call here so this will not call another 2 dfs
+            subset.append(nums[i]) # include nums[i]
+            dfs(i+1)  # calling dfs Including nums[i] in subset
+            subset.pop() # removing nums[i] as we will use subset to call dfs without including nums[i]
+            dfs(i+1) # calling dfs Excluding nums[i] in subset
+        
+        dfs(0)
         return res
 '''
 Time Complexity: O(N * 2^N)  ; because there will be 2^N different subsets, and we have to create a copy of each one, which is O(N).
