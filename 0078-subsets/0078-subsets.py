7e9b686da91de209479277e91844cf90530a2948
Badhan Sen <badhan.cse@gmail.com>
2024-04-18 11:32:19 +0100

    Time: 27 ms (97.76%), Space: 16.7 MB (78.15%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,23 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+#         total = 2 ** len(nums)
+#         for i in range(total):
+#             res = []
+#             for j in range(len(nums)):
+#                 if i & (1 << j):
+#                     res.append(nums[j])
+#             result.append(res)
+#         return result
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(i, subset):
+            if i >= len(nums):
+                res.append(subset)
+                return
+            dfs(i + 1, subset + [nums[i]])
+            dfs(i + 1, subset)
+        dfs(0, [])
+        return res
