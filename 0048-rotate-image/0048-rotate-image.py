0f1c218bfb23b081f19e1187213fca8b11b4c886
Badhan Sen <badhan.cse@gmail.com>
2024-05-18 00:15:38 +0100

    Time: 45 ms (14.95%), Space: 16.6 MB (19.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,9 +3,26 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        matrix.reverse()
+        # matrix.reverse()
+        # n = len(matrix)
+        # for r in range(n):
+        #     for c in range(r + 1, n):
+        #         matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
         n = len(matrix)
-        for r in range(n):
-            for c in range(r + 1, n):
-                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
         
+        for i in range(n // 2):
+            for j in range(i, n - i - 1):
+                # save the top element
+                top = matrix[i][j]
+                
+                # top <- left
+                matrix[i][j] = matrix[n - j - 1][i]
+                
+                # left <- bottom
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+                
+                # bottom <- right
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+                
+                # right <- top
+                matrix[j][n - i - 1] = top
