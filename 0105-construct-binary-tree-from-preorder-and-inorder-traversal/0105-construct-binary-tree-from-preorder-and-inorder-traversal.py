90795c8950cabec2c159884c72f355e89815d316
Badhan Sen <badhan.cse@gmail.com>
2024-05-21 00:03:09 +0100

    Time: 86 ms (72.31%), Space: 18 MB (94.65%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,19 +6,40 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        preIndex = 0
-        def build(left, right):
-            nonlocal preIndex
+        def build(left, right, preIndex):
             if left > right:
-                return None
+                return None, preIndex
             val = preorder[preIndex]
             root = TreeNode(val)
             preIndex += 1
             index = inorder.index(val)
-            root.left = build(left, index - 1)
-            root.right = build(index + 1, right)
-            return root
-        root = build(0, len(inorder) - 1)
+            root.left, preIndex = build(left, index - 1, preIndex)
+            root.right, preIndex = build(index + 1, right, preIndex)
+            return root, preIndex
+        root, p = build(0, len(inorder) - 1, 0)
         return root
+
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+#         preIndex = 0
+#         def build(left, right):
+#             nonlocal preIndex
+#             if left > right:
+#                 return None
+#             val = preorder[preIndex]
+#             root = TreeNode(val)
+#             preIndex += 1
+#             index = inorder.index(val)
+#             root.left = build(left, index - 1)
+#             root.right = build(index + 1, right)
+#             return root
+#         root = build(0, len(inorder) - 1)
+#         return root
             
             
