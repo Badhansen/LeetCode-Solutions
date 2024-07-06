8aa32a8a93787cd19efce8f172ff298a40537916
Badhan Sen <badhan.cse@gmail.com>
2024-07-06 23:29:35 +0100

    Time: 83 ms (13.83%), Space: 20.5 MB (8.77%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,12 +1,24 @@
 class Solution:
+    # def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+    #     intervals.sort(key=lambda x : (x[0], x[1]))
+    #     n = len(intervals)
+    #     res = [intervals[0][1]]
+    #     ans = 1
+    #     for i in range(1, n):
+    #         while res and res[0] <= intervals[i][0]:
+    #             heapq.heappop(res)
+    #         heapq.heappush(res, intervals[i][1])
+    #         ans = max(ans, len(res))
+    #     return ans
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x : (x[0], x[1]))
-        n = len(intervals)
-        res = [intervals[0][1]]
-        ans = 1
-        for i in range(1, n):
-            while res and res[0] <= intervals[i][0]:
-                heapq.heappop(res)
-            heapq.heappush(res, intervals[i][1])
-            ans = max(ans, len(res))
+        splitedIntervals = []
+        for start, end in intervals:
+            splitedIntervals.append((start, 1))
+            splitedIntervals.append((end, -1))
+        splitedIntervals.sort()
+        ans = 0
+        count = 0
+        for start, meeting in splitedIntervals:
+            count += meeting
+            ans = max(ans, count)
         return ans
