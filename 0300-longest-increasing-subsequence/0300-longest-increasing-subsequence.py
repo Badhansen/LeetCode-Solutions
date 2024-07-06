050343b0237016bad0d5cbe2cb28168af00a09b8
Badhan Sen <badhan.cse@gmail.com>
2024-07-06 23:46:11 +0100

    Time: 2410 ms (12.23%), Space: 17.2 MB (10.80%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,14 +1,17 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        @functools.lru_cache(None)
-        def solve(pos):
+        n = len(nums)
+        dp = [-1] * n
+        def solve(pos, nums):
             if pos == len(nums):
                 return 1
+            if dp[pos] != -1:
+                return dp[pos]
             res = 1
             for i in range(pos + 1, len(nums)):
                 if nums[pos] < nums[i]:
-                    res = max(res, 1 + solve(i))
+                    res = max(res, 1 + solve(i, nums))
+            dp[pos] = res
             return res
-        
-        return max([solve(i) for i in range(len(nums))])
-        
+        return max([solve(i, nums) for i in range(n)])
+    
