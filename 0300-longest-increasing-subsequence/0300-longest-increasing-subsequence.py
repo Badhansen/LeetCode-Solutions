1754bd4f6d189cb0df66f24cbe5e98ac6658e264
Badhan Sen <badhan.cse@gmail.com>
2024-07-07 00:07:43 +0100

    Time: 54 ms (98.94%), Space: 16.7 MB (92.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -24,22 +24,37 @@ class Solution:
     #                 dp[i] = max(dp[i], 1 + dp[j])
     #     return max(dp)
     
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        sub = [nums[0]]
-        for num in nums[1:]:
-            if num > sub[-1]:
-                sub.append(num)
-            else:
-                # Find the first element in sub that is greater than or equal to num
-                i = 0
-                while num > sub[i]:
-                    i += 1
-                sub[i] = num
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         sub = [nums[0]]
+#         for num in nums[1:]:
+#             if num > sub[-1]:
+#                 sub.append(num)
+#             else:
+#                 # Find the first element in sub that is greater than or equal to num
+#                 i = 0
+#                 while num > sub[i]:
+#                     i += 1
+#                 sub[i] = num
 
+#         return len(sub)
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for n in nums:
+            i = bisect_left(sub, n)
+            if i == len(sub):
+                sub.append(n)
+            else:
+                sub[i] = n
         return len(sub)
     
+# '''
+# Time complexity: O(N^2)
+# Space complexity: O(N)
+# '''
+
 '''
-Time complexity: O(N^2)
+Time complexity: O(N log N)
 Space complexity: O(N)
 '''
     
