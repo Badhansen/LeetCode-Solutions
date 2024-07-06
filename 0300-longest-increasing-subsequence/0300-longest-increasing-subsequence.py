6d69d4808886ed3ac8680e25eb38734fcc59be75
Badhan Sen <badhan.cse@gmail.com>
2024-07-07 00:05:14 +0100

    Time: 62 ms (92.84%), Space: 16.7 MB (98.27%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -14,12 +14,32 @@ class Solution:
     #         dp[pos] = res
     #         return res
     #     return max([solve(i, nums) for i in range(n)])
+    
+    # def lengthOfLIS(self, nums: List[int]) -> int:
+    #     n = len(nums)
+    #     dp = [1] * n
+    #     for i in range(n):
+    #         for j in range(i):
+    #             if nums[j] < nums[i]:
+    #                 dp[i] = max(dp[i], 1 + dp[j])
+    #     return max(dp)
+    
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1] * n
-        for i in range(n):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], 1 + dp[j])
-        return max(dp)
+        sub = [nums[0]]
+        for num in nums[1:]:
+            if num > sub[-1]:
+                sub.append(num)
+            else:
+                # Find the first element in sub that is greater than or equal to num
+                i = 0
+                while num > sub[i]:
+                    i += 1
+                sub[i] = num
+
+        return len(sub)
+    
+'''
+Time complexity: O(N^2)
+Space complexity: O(N)
+'''
     
