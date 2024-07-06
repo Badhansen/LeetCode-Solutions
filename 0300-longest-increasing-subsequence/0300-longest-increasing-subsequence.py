f8c8bf47f600a352f1e90490efbd70e10770c842
Badhan Sen <badhan.cse@gmail.com>
2024-07-06 23:51:03 +0100

    Time: 1742 ms (63.69%), Space: 16.8 MB (92.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,17 +1,25 @@
 class Solution:
+    # def lengthOfLIS(self, nums: List[int]) -> int:
+    #     n = len(nums)
+    #     dp = [-1] * n
+    #     def solve(pos, nums):
+    #         if pos == len(nums):
+    #             return 1
+    #         if dp[pos] != -1:
+    #             return dp[pos]
+    #         res = 1
+    #         for i in range(pos + 1, len(nums)):
+    #             if nums[pos] < nums[i]:
+    #                 res = max(res, 1 + solve(i, nums))
+    #         dp[pos] = res
+    #         return res
+    #     return max([solve(i, nums) for i in range(n)])
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [-1] * n
-        def solve(pos, nums):
-            if pos == len(nums):
-                return 1
-            if dp[pos] != -1:
-                return dp[pos]
-            res = 1
-            for i in range(pos + 1, len(nums)):
-                if nums[pos] < nums[i]:
-                    res = max(res, 1 + solve(i, nums))
-            dp[pos] = res
-            return res
-        return max([solve(i, nums) for i in range(n)])
+        dp = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+        return max(dp)
     
