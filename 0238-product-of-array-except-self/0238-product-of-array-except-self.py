af5bc3858335060147b1497164b7f2a76190f220
Badhan Sen <badhan.cse@gmail.com>
2024-07-06 15:34:06 +0100

    Time: 273 ms (47.45%), Space: 25.8 MB (50.71%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,30 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        left, right, res = [1] * n, [1] * n, [1] * n
+        res = [1] * n
         for i in range(1, n):
-            left[i] = left[i - 1] * nums[i - 1]
-        for i in range(n - 2, -1, -1):
-            right[i] = right[i + 1] * nums[i + 1]
-        for i in range(n):
-            res[i] = left[i] * right[i]
+            res[i] = res[i - 1] * nums[i - 1]
+        right = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
         return res
+'''
+Time complexity: O(n)
+Space complexity: O(1)
+'''
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         left, right, res = [1] * n, [1] * n, [1] * n
+#         for i in range(1, n):
+#             left[i] = left[i - 1] * nums[i - 1]
+#         for i in range(n - 2, -1, -1):
+#             right[i] = right[i + 1] * nums[i + 1]
+#         for i in range(n):
+#             res[i] = left[i] * right[i]
+#         return res
 '''
 Time complexity: O(n)
 Space complexity: O(n)
