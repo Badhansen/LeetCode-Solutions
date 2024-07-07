831d0f9fe728ac55ca1b5d77581e22ce9f0376a6
Badhan Sen <badhan.cse@gmail.com>
2024-07-07 12:45:32 +0100

    Time: 725 ms (82.88%), Space: 16.7 MB (94.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,18 +1,26 @@
 class Solution:
+    # def coinChange(self, coins: List[int], amount: int) -> int:
+    #     dp = defaultdict(lambda: -1)
+    #     def solve(amount):
+    #         if amount < 0:
+    #             return float("inf")
+    #         if amount == 0:
+    #             return 0
+    #         if dp[amount] != -1:
+    #             return dp[amount]
+    #         res = float("inf")
+    #         for coin in coins:
+    #             res = min(res, 1 + solve(amount - coin))
+    #         dp[amount] = res
+    #         return res
+    #     ans = solve(amount)
+    #     return -1 if ans >= float("inf") else ans
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = defaultdict(lambda: -1)
-        def solve(amount):
-            if amount < 0:
-                return float("inf")
-            if amount == 0:
-                return 0
-            if dp[amount] != -1:
-                return dp[amount]
-            res = float("inf")
-            for coin in coins:
-                res = min(res, 1 + solve(amount - coin))
-            dp[amount] = res
-            return res
-        ans = solve(amount)
-        return -1 if ans >= float("inf") else ans
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+        for i in range(amount + 1):
+            for c in coins:
+                if c <= i:
+                    dp[i] = min(dp[i], 1 + dp[i - c])
+        return -1 if dp[amount] >= float("inf") else dp[amount]
             
