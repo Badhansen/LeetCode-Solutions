33340eea9cc5e6c29bf3c29fbef47d70d037e2d1
Badhan Sen <badhan.cse@gmail.com>
2024-07-11 18:31:10 +0100

    Time: 46 ms (11.69%), Space: 16.6 MB (35.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -8,21 +8,36 @@ class Solution:
         # for r in range(n):
         #     for c in range(r + 1, n):
         #         matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
-        n = len(matrix)
         
-        for i in range(n // 2):
-            for j in range(i, n - i - 1):
-                # save the top element
-                top = matrix[i][j]
+        
+#         n = len(matrix)
+        
+#         for i in range(n // 2):
+#             for j in range(i, n - i - 1):
+#                 # save the top element
+#                 top = matrix[i][j]
                 
-                # top <- left
-                matrix[i][j] = matrix[n - j - 1][i]
+#                 # top <- left
+#                 matrix[i][j] = matrix[n - j - 1][i]
                 
-                # left <- bottom
-                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+#                 # left <- bottom
+#                 matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
                 
-                # bottom <- right
-                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+#                 # bottom <- right
+#                 matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
                 
-                # right <- top
-                matrix[j][n - i - 1] = top
+#                 # right <- top
+#                 matrix[j][n - i - 1] = top
+
+        n = len(matrix)
+
+        # Transpose the matrix
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # print(matrix)
+        # Reverse each row
+        for i in range(n):
+            matrix[i].reverse()
+
+        return matrix
