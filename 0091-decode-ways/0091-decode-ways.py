e5efea9bb3bee7631554d09d4b28b1898726c8b8
Badhan Sen <badhan.cse@gmail.com>
2024-07-13 01:27:14 +0100

    Time: 35 ms (65.57%), Space: 16.5 MB (75.69%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,19 +1,30 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = [-1] * len(s)
-        def ways(pos):
-            if pos >= len(s):
-                return 1
-            if dp[pos] != -1:
-                return dp[pos]
-            take1, take2 = 0, 0
-            if s[pos] != '0':
-                take1 = ways(pos + 1)
-                if pos + 1 < len(s):
-                    num = int(s[pos:pos + 2])
-                    if num <= 26:
-                        take2 = ways(pos + 2)
-            dp[pos] = take1 + take2
-            return dp[pos]
-        return ways(0)
+        # dp = [-1] * len(s)
+        # def ways(pos):
+        #     if pos >= len(s):
+        #         return 1
+        #     if dp[pos] != -1:
+        #         return dp[pos]
+        #     take1, take2 = 0, 0
+        #     if s[pos] != '0':
+        #         take1 = ways(pos + 1)
+        #         if pos + 1 < len(s):
+        #             num = int(s[pos:pos + 2])
+        #             if num <= 26:
+        #                 take2 = ways(pos + 2)
+        #     dp[pos] = take1 + take2
+        #     return dp[pos]
+        # return ways(0)
+        
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+        for i in range(2, len(s) + 1):
+            if s[i - 1] != '0':
+                dp[i] += dp[i - 1]
+            num = int(s[i - 2:i])
+            if 10 <= num <= 26:
+                dp[i] += dp[i - 2]
+        return dp[len(s)]
                     
