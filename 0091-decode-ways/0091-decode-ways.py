291f27eae953acd4629d4fddd2693140479e0a1e
Badhan Sen <badhan.cse@gmail.com>
2024-07-13 01:27:44 +0100

    Time: 36 ms (56.83%), Space: 16.5 MB (75.69%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -16,15 +16,15 @@ class Solution:
         #     dp[pos] = take1 + take2
         #     return dp[pos]
         # return ways(0)
-        
-        dp = [0] * (len(s) + 1)
+        n = len(s)
+        dp = [0] * (n + 1)
         dp[0] = 1
         dp[1] = 0 if s[0] == '0' else 1
-        for i in range(2, len(s) + 1):
+        for i in range(2, n + 1):
             if s[i - 1] != '0':
                 dp[i] += dp[i - 1]
             num = int(s[i - 2:i])
             if 10 <= num <= 26:
                 dp[i] += dp[i - 2]
-        return dp[len(s)]
+        return dp[n]
                     
