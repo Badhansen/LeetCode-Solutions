aaeb1022ae92c20a5acc6a97819c3244b122c496
Badhan Sen <badhan.cse@gmail.com>
2024-07-18 00:01:05 +0100

    Time: 93 ms (38.11%), Space: 18.2 MB (87.59%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(lambda: list())
+        # Create a graph and an indegree list
+        graph = defaultdict(list)
         indegree = [0] * numCourses
         
+        # Build the graph and indegree list
         for c in prerequisites:
             indegree[c[0]] += 1
             graph[c[1]].append(c[0])
         
-        visited = set()
-        course = list()
-        
+        # Initialize a queue with courses that have no prerequisites
+        queue = deque()
         for node in range(numCourses):
             if indegree[node] == 0:
-                course.append(node)
+                queue.append(node)
         
+        # Process the queue
         done = 0
-        
-        while course:
-            src = course.pop(0)
+        while queue:
+            src = queue.popleft()
             done += 1
             for dest in graph[src]:
                 indegree[dest] -= 1
                 if indegree[dest] == 0:
-                    course.append(dest)
+                    queue.append(dest)
         
-        return True if done == numCourses else False
+        # Check if all courses can be finished
+        return done == numCourses
