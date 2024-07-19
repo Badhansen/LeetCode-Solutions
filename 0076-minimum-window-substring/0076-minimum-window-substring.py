c5f2d5ac96d375bcd4c2d8af6a541356c97b2944
Badhan Sen <badhan.cse@gmail.com>
2024-07-20 00:39:40 +0100

    Time: 99 ms (84.82%), Space: 17.3 MB (58.08%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,25 +1,24 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        count = defaultdict(int)
-        uniqueCharacter = 0
+        hash = defaultdict(int)
         for c in t:
-            if c not in count:
-                uniqueCharacter += 1
-            count[c] += 1
-        start, end, answer = 0, 0, float("inf")
+            hash[c] += 1
+        n, m = len(s), len(t)
+        start, end, maxLen, count = 0, 0, float("inf"), 0
         index = 0
-        while end < len(s):
-            count[s[end]] -= 1
-            if count[s[end]] == 0:
-                uniqueCharacter -= 1
+        while end < n:
+            if s[end] in hash:
+                hash[s[end]] -= 1
+                if hash[s[end]] >= 0:
+                    count += 1
+            while count == m:
+                if end - start + 1 < maxLen:
+                    maxLen = end - start + 1
+                    index = start
+                if s[start] in hash:
+                    hash[s[start]] += 1
+                    if hash[s[start]] > 0:
+                        count -= 1
+                start += 1
             end += 1
-            if uniqueCharacter == 0:
-                while uniqueCharacter == 0:
-                    if answer > end - start:
-                        index = start
-                        answer = end - start
-                    count[s[start]] += 1
-                    if count[s[start]] > 0:
-                        uniqueCharacter += 1
-                    start += 1
-        return "" if answer == float('inf') else s[index:index+answer]
+        return "" if maxLen == float('inf') else s[index:index+maxLen]
