84dd82892c05e2853ed06d8a13af335604fb17df
Badhan Sen <badhan.cse@gmail.com>
2024-07-24 15:11:42 +0100

    Time: 40 ms (58.47%), Space: 16.9 MB (11.51%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,19 +1,17 @@
 class Solution:
-    def dfs(self, s, words, dp) -> bool:
-        if s in dp:
-            return dp[s]
-        if s in words:
-            return True
-        for i in range(1, len(s)):
-            prefix = s[:i]
-            if prefix in words and self.dfs(s[i:], words, dp):
-                dp[prefix] = True
-                return True
-        dp[s] = False
-        return False
-            
-    
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         words = set(wordDict)
-        dp = {}
-        return self.dfs(s, words, dp) 
+        dp = dict()
+        def dfs(s):
+            if s in dp:
+                return dp[s]
+            if s in words:
+                return True
+            for length in range(1, len(s) + 1):
+                prefix = s[:length]
+                if prefix in words and dfs(s[length:]):
+                    dp[prefix] = True
+                    return True
+            dp[s] = False
+            return dp[s]
+        return dfs(s)
