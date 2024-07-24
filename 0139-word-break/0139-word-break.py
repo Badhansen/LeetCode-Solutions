178dcfb97be9a87ce614da27883006714cf3edad
Badhan Sen <badhan.cse@gmail.com>
2024-07-24 18:20:58 +0100

    Time: 37 ms (75.49%), Space: 16.6 MB (76.08%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,17 +1,30 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        words = set(wordDict)
-        dp = dict()
-        def dfs(s):
-            if s in dp:
-                return dp[s]
-            if s in words:
-                return True
-            for length in range(1, len(s)):
-                prefix = s[:length]
-                if prefix in words and dfs(s[length:]):
-                    dp[prefix] = True
-                    return True
-            dp[s] = False
-            return dp[s]
-        return dfs(s)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[n] = True
+        for i in range(n - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= n and s[i:i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+        return dp[0]
+        
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         words = set(wordDict)
+#         dp = dict()
+#         def dfs(s):
+#             if s in dp:
+#                 return dp[s]
+#             if s in words:
+#                 return True
+#             for length in range(1, len(s)):
+#                 prefix = s[:length]
+#                 if prefix in words and dfs(s[length:]):
+#                     dp[prefix] = True
+#                     return True
+#             dp[s] = False
+#             return dp[s]
+#         return dfs(s)
