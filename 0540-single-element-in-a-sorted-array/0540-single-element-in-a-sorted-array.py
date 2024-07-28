03e2f083f70f62328535e291970d697c32bd08bc
Badhan Sen <badhan.cse@gmail.com>
2024-07-28 01:25:12 +0100

    Time: 130 ms (91.50%), Space: 26.6 MB (47.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        maxMask = 0xFFFFFFFF
         mask = 0
         for n in nums:
             mask ^= n
-            # mask &= maxMask
         return mask
