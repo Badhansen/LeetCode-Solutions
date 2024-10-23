da4fbabf1d94dc09cd0290a812b73baf2f11dbd1
Badhan Sen <badhan.cse@gmail.com>
2024-10-23 22:51:57 +0100

    Time: 0 ms (100.00%), Space: 20.1 MB (6.95%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -5,23 +5,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        # Initialize instance variables
-        self.res = None
-        self.k = 0
+#     def __init__(self):
+#         # Initialize instance variables
+#         self.res = None
+#         self.k = 0
     
-    def dfs(self, root):
-        if root is None:
-            return
-        self.dfs(root.left)
-        self.k -= 1
-        if self.k == 0:
-            self.res = root.val
-            return
-        self.dfs(root.right)
+#     def dfs(self, root):
+#         if root is None:
+#             return
+#         self.dfs(root.left)
+#         self.k -= 1
+#         if self.k == 0:
+#             self.res = root.val
+#             return
+#         self.dfs(root.right)
         
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.res = None
-        self.k = k
-        self.dfs(root)
-        return self.res
+        # self.res = None
+        # self.k = k
+        # self.dfs(root)
+        # return self.res
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
