3044bb01f8135e504f829f4922c2b65fc5d1de9c
Badhan Sen <badhan.cse@gmail.com>
2024-10-23 12:52:06 +0100

    Time: 0 ms (100.00%), Space: 16.6 MB (7.23%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -6,14 +6,25 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        result = []
-
-        def dfs(root):
-            if root is None:
-                return
-            result.append(root.val)
-            dfs(root.left)
-            dfs(root.right)
+#         # Recursive
+#         result = []
+#         def dfs(root):
+#             if root is None:
+#                 return
+#             result.append(root.val)
+#             dfs(root.left)
+#             dfs(root.right)
             
-        dfs(root)
-        return result
+#         dfs(root)
+#         return result
+        res, stack = [], []
+        curr = root
+        while curr or stack:
+            if curr:
+                res.append(curr.val)
+                stack.append(curr.right)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+        return res
+            
