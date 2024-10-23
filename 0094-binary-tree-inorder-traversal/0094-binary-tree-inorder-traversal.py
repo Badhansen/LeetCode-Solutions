980d4782a80239fdeea30b6d27bed32c25c4c26c
Badhan Sen <badhan.cse@gmail.com>
2024-10-23 12:29:04 +0100

    Time: 0 ms (100.00%), Space: 16.6 MB (7.60%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -4,17 +4,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def __init__(self):
+#         self.path = []
+        
+#     def dfs(self, root):
+#         if root is None:
+#             return
+#         self.dfs(root.left)
+#         self.path.append(root.val)
+#         self.dfs(root.right)
+        
+#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         self.dfs(root)
+#         return self.path
 class Solution:
-    def __init__(self):
-        self.path = []
-        
-    def dfs(self, root):
-        if root is None:
-            return
-        self.dfs(root.left)
-        self.path.append(root.val)
-        self.dfs(root.right)
-        
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        self.dfs(root)
-        return self.path
+    def inorderTraversal(self, root):
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper(self, root, res):
+        if root is not None:
+            self.helper(root.left, res)
+            res.append(root.val)
+            self.helper(root.right, res)
