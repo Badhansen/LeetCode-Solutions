e20734cb833688e37c76c30d45c9bf88210e6dff
Badhan Sen <badhan.cse@gmail.com>
2024-10-26 14:58:03 +0100

    Time: 3 ms (92.13%), Space: 16.8 MB (57.29%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def generate(pos, n, path, open, close):
+            if pos == n:
+                if open == close:
+                    res.append(path)
+                return
+            if open < n:
+                generate(pos + 1, n, path + '(', open + 1, close)
+            if close < open:
+                generate(pos + 1, n, path + ')', open, close + 1)
+        generate(0, 2 * n, "", 0, 0)
+        return res
+            
+        
