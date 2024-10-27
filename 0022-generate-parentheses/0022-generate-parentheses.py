d728bb179c1fbd81cc1a001ed2e17f48cbee89bc
Badhan Sen <badhan.cse@gmail.com>
2024-10-27 10:10:18 +0000

    Time: 0 ms (100.00%), Space: 16.8 MB (86.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,13 +4,17 @@ class Solution:
         def generate(pos, n, path, open, close):
             if pos == 2 * n:
                 if open == close:
-                    res.append(path)
+                    res.append("".join(path))
                 return
             if open < n:
-                generate(pos + 1, n, path + '(', open + 1, close)
+                path.append('(')
+                generate(pos + 1, n, path, open + 1, close)
+                path.pop()
             if close < open:
-                generate(pos + 1, n, path + ')', open, close + 1)
-        generate(0, n, "", 0, 0)
+                path.append(')')
+                generate(pos + 1, n, path, open, close + 1)
+                path.pop()
+        generate(0, n, [], 0, 0)
         return res
             
         
