cc1b5427d99171d9f497ea94bc0b9e564911502f
Badhan Sen <badhan.cse@gmail.com>
2024-10-27 10:24:01 +0000

    Time: 35 ms (5.19%), Space: 16.8 MB (57.22%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,20 +1,38 @@
 class Solution:
+    # def generateParenthesis(self, n: int) -> List[str]:
+    #     res = []
+    #     def generate(pos, n, path, open, close):
+    #         if pos == 2 * n:
+    #             if open == close:
+    #                 res.append("".join(path))
+    #             return
+    #         if open < n:
+    #             path.append('(')
+    #             generate(pos + 1, n, path, open + 1, close)
+    #             path.pop()
+    #         if close < open:
+    #             path.append(')')
+    #             generate(pos + 1, n, path, open, close + 1)
+    #             path.pop()
+    #     generate(0, n, [], 0, 0)
+    #     return res
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        def generate(pos, n, path, open, close):
-            if pos == 2 * n:
-                if open == close:
-                    res.append("".join(path))
-                return
-            if open < n:
-                path.append('(')
-                generate(pos + 1, n, path, open + 1, close)
-                path.pop()
-            if close < open:
-                path.append(')')
-                generate(pos + 1, n, path, open, close + 1)
-                path.pop()
-        generate(0, n, [], 0, 0)
-        return res
+        total = 1 << ( 2 * n)
+        answer = []
+        for i in range(total):
+            res, brackets = [], 0
+            for j in range(2 * n):
+                if i & (1 << j):
+                    brackets += 1
+                    res.append('(')
+                else:
+                    brackets -= 1
+                    res.append(')')
+                if brackets < 0:
+                    break
+                        
+            if brackets == 0:
+                answer.append("".join(res))
+        return answer
             
         
