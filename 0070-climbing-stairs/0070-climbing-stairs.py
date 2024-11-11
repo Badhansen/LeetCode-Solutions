8333ab50ace6fdf5de9c19c37d145c4510e4455c
Badhan Sen <badhan.cse@gmail.com>
2024-11-11 05:32:55 +0000

    Time: 0 ms (100.00%), Space: 16.6 MB (18.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -19,17 +19,33 @@
 #         for i in range(2, n + 1):
 #             dp[i] = dp[i - 1] + dp[i - 2]
 #         return dp[n]
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n <= 3:
-            return n
-        n1, n2 = 2, 3
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n <= 3:
+#             return n
+#         n1, n2 = 2, 3
 
-        for i in range(4, n + 1):
-            temp = n1 + n2
-            n1 = n2
-            n2 = temp
-        return n2
+#         for i in range(4, n + 1):
+#             temp = n1 + n2
+#             n1 = n2
+#             n2 = temp
+#         return n2
+class Solution:
+    def __init__(self):
+        self.dp = defaultdict(int)
+        
+    def ways(self, i, n):
+        if i == n:
+            return 1
+        if i > n:
+            return 0
+        if i in self.dp:
+            return self.dp[i]
+        self.dp[i] = self.ways(i + 1, n) + self.ways(i + 2, n)
+        return self.dp[i]
+    
+    def climbStairs(self, n: int) -> int:
+        return self.ways(0, n)
 
     
     
