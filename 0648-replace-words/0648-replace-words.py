bbc6dcf4e78902291dee431f53e103fffd6e007b
Badhan Sen <badhan.cse@gmail.com>
2024-11-11 16:36:16 +0000

    Time: 52 ms (75.51%), Space: 33.4 MB (29.24%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end = True
+        
+    def start_with(self, word: str) -> bool:
+        curr = self.root
+        res = []
+        for c in word:
+            if c not in curr.children:
+                return [False, ""]
+            curr = curr.children[c]
+            res.append(c)
+            if curr.end:
+                return [True, "".join(res)]
+        return [False, ""]
+        
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie()
+        for word in dictionary:
+            trie.insert(word)
+        words = sentence.split(" ")
+        res = []
+        for word in words:
+            has_prefix, w = trie.start_with(word)
+            if has_prefix:
+                res.append(w)
+            else:
+                res.append(word)
+        return " ".join(res)
