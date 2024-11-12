45d3ea63f13fa2afccbc68f9916d868a3b94200e
Badhan Sen <badhan.cse@gmail.com>
2024-11-12 00:25:17 +0000

    Time: 3 ms (78.41%), Space: 17.9 MB (94.87%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+        for u, v in prerequisites:
+            graph[v].append(u)
+            indegree[u] += 1
+        sources = deque([i for i in range(numCourses) if indegree[i] == 0])
+        res = []
+        while sources:
+            size = len(sources)
+            for i in range(size):
+                src = sources.popleft()
+                res.append(src)
+                for dest in graph[src]:
+                    indegree[dest] -= 1
+                    if indegree[dest] == 0:
+                        sources.append(dest)
+        return res if len(res) == numCourses else []
