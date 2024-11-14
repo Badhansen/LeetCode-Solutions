d2606a257680289a68607417a6864f209585b1ad
Badhan Sen <badhan.cse@gmail.com>
2024-11-14 03:24:07 +0000

    Time: 0 ms (100.00%), Space: 16.9 MB (8.68%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,18 +4,35 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def __init__(self):
+#         self.res = []
+        
+#     def dfs(self, root, level):
+#         if root is None:
+#             return
+#         if level == len(self.res):
+#             self.res.append(root.val)
+#         self.dfs(root.right, level + 1)
+#         self.dfs(root.left, level + 1)
+        
+#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         self.dfs(root, 0)
+#         return self.res
 class Solution:
-    def __init__(self):
-        self.res = []
-        
-    def dfs(self, root, level):
-        if root is None:
-            return
-        if level == len(self.res):
-            self.res.append(root.val)
-        self.dfs(root.right, level + 1)
-        self.dfs(root.left, level + 1)
-        
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        self.dfs(root, 0)
-        return self.res
+        res = []
+        queue = deque([root])
+        while queue:
+            qlen = len(queue)
+            right_view = None
+            for i in range(qlen):
+                node = queue.popleft()
+                if node:
+                    right_view = node
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if right_view:
+                res.append(right_view.val)
+        return res
+                
