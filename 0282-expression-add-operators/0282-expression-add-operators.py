9155447bfffb790d9122e1827c43d97c55359b40
Badhan Sen <badhan.cse@gmail.com>
2024-11-15 03:05:00 +0000

    Time: 7649 ms (5.02%), Space: 17.1 MB (67.47%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.py
+++ 0282-expression-add-operators/0282-expression-add-operators.py
@@ -1,22 +1,28 @@
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
         res = []
-        def dfs(start, path, total, last):
-            if start == len(num) and total == target:
-                res.append("".join(path))
+        def dfs(pos, path):
+            if pos == len(num):
+                try:
+                    result = eval("".join(path))
+                except (SyntaxError, NameError) as e:
+                    result = None
+                except Exception as e:
+                    result = None
+                if result == target:
+                    res.append("".join(path))
                 return
-            if start > len(num):
+            if pos > len(num):
                 return
-            for i in range(start, len(num)):
-                if i > start and num[start] == '0':
+            for i in range(pos, len(num)):
+                if i > pos and num[pos] == '0':
                     break
-                n = int(num[start: i + 1])
-                if total is None:
-                    dfs(i + 1, [str(n)], n, n)
+                n = int(num[pos:i + 1])
+                if pos == 0:
+                    dfs(i + 1, path + [str(n)])
                 else:
-                    dfs(i + 1, path + ['*', str(n)], total - last + last * n, last * n)
-                    dfs(i + 1, path + ['+', str(n)], total + n, n)
-                    dfs(i + 1, path + ['-', str(n)], total - n, -n)
-                    
-        dfs(0, [], None, None)
+                    dfs(i + 1, path + ['*', str(n)])
+                    dfs(i + 1, path + ['+', str(n)])
+                    dfs(i + 1, path + ['-', str(n)])
+        dfs(0, [])
         return res
