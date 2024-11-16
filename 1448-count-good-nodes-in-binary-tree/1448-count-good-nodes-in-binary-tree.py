99b0141bf1dcf2e34ed55d0f7b8af8f9edea7126
Badhan Sen <badhan.cse@gmail.com>
2024-11-16 21:49:46 +0000

    Time: 129 ms (64.39%), Space: 31 MB (88.17%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -4,14 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def goodNodes(self, root: TreeNode) -> int:
+#         def dfs(root, maxValue):
+#             if not root:
+#                 return 0
+#             res = 1 if root.val >= maxValue else 0
+#             maxValue = max(maxValue, root.val)
+#             res += dfs(root.left, maxValue)
+#             res += dfs(root.right, maxValue)
+#             return res
+#         return dfs(root, root.val)
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def dfs(root, maxValue):
-            if not root:
-                return 0
-            res = 1 if root.val >= maxValue else 0
-            maxValue = max(maxValue, root.val)
-            res += dfs(root.left, maxValue)
-            res += dfs(root.right, maxValue)
-            return res
-        return dfs(root, root.val)
+        res = 0
+        q = deque()
+        q.append((root,-float('inf')))
+        while q:
+            node,maxval = q.popleft()
+            if node.val >= maxval:  
+                res += 1
+            if node.left:    
+                q.append((node.left,max(maxval,node.val)))
+            if node.right:
+                q.append((node.right,max(maxval,node.val)))
+        return res
