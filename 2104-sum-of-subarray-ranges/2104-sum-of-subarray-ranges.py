e53976d94e6fadc729dd5827dd7bc69cb928ca81
Badhan Sen <badhan.cse@gmail.com>
2024-11-19 22:34:00 +0000

    Time: 11 ms (96.33%), Space: 16.8 MB (43.96%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,11 +1,23 @@
+# KING-SEN
+
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
         n = len(nums)
-        result = 0
-        for i in range(n):
-            min_value, max_value = nums[i], nums[i]
-            for j in range(i, n):
-                min_value = min(min_value, nums[j])
-                max_value = max(max_value, nums[j])
-                result += (max_value - min_value)
-        return result
+        res = 0
+        stack = []
+        # Decresing stack for maximum element in the range
+        for right in range(n + 1):
+            while stack and (right == n or nums[stack[-1]] <= nums[right]):
+                j = stack.pop()
+                left = -1 if not stack else stack[-1]
+                res += (j - left) * (right - j) * nums[j]
+            stack.append(right)
+        stack = []
+        # Increasing stack for minimum element in the range
+        for right in range(n + 1):
+            while stack and (right == n or nums[stack[-1]] >= nums[right]):
+                j = stack.pop()
+                left = -1 if not stack else stack[-1]
+                res -= (j - left) * (right - j) * nums[j]
+            stack.append(right)
+        return res
