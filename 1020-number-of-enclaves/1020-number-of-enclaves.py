7426a0205b885730a726438b6d39ed8769f034a3
Badhan Sen <badhan.cse@gmail.com>
2024-11-19 22:57:55 +0000

    Time: 127 ms (34.57%), Space: 55.6 MB (10.82%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,51 +1,30 @@
-class Solution {
-public:
-    bool isValid(int x, int y, int r, int c) {
-        if (x >= 0 && x < r && y >= 0 && y < c) {
-            return true;
-        }
-        return false;
-    }
-    int dfs(int x, int y, vector<vector<int>>& grid) {
-        if (!isValid(x, y, grid.size(), grid[0].size()) || grid[x][y] == 0) {
-            return 0;
-        }
-        grid[x][y] = 0;
-        int result = dfs(x + 1, y, grid);
-        result += dfs(x, y + 1, grid);
-        result += dfs(x - 1, y, grid);
-        result += dfs(x, y - 1, grid);
-        return result + 1;
-    }
-
-    int numEnclaves(vector<vector<int>>& grid) {
-        int r = grid.size(), c = grid[0].size();
-        // 1st and last Row visit
-        for (int i = 0; i < c; i++) {
-            if (grid[0][i] == 1) {
-                dfs(0, i, grid);
-            }
-            if (grid[r - 1][i] == 1) {
-                dfs(r - 1, i, grid);
-            }
-        }
-        // 1st and last Column visit
-        for (int i = 0; i < r; i++) {
-            if (grid[i][0] == 1) {
-                dfs(i, 0, grid);
-            }
-            if (grid[i][c - 1] == 1) {
-                dfs(i, c - 1, grid);
-            }
-        }
-        int answer = 0;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 1) {
-                    answer += dfs(i, j, grid);
-                }
-            }
-        }
-        return answer;
-    }
-};
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        def dfs(r, c, marked):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] != marked:
+                return
+            grid[r][c] = 2
+            for x, y in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                dfs(r + x, c + y, marked)
+        
+        # 1. Mark the 1 connected to the boundary using DFS
+        # Traverse first and last row
+        rows, cols = len(grid), len(grid[0])
+        marked = 1
+        for i in range(cols):
+            if grid[0][i] == marked:
+                dfs(0, i, marked)
+            if grid[rows - 1][i] == marked:
+                dfs(rows - 1, i, marked)
+        # Traverse first and last column
+        for i in range(rows):
+            if grid[i][0] == marked:
+                dfs(i, 0, marked)
+            if grid[i][cols - 1] == marked:
+                dfs(i, cols - 1, marked)
+        res = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == marked:
+                    res += 1
+        return res
