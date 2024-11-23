56cac34cd60832d52b897781300c3416048faaa8
Badhan Sen <badhan.cse@gmail.com>
2024-11-23 14:57:09 +0000

    Time: 8 ms (11.47%), Space: 22.4 MB (81.99%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,6 +3,29 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def reorderList(self, head: Optional[ListNode]) -> None:
+#         fast = slow = head
+#         while fast.next and fast.next.next:
+#             slow = slow.next
+#             fast = fast.next.next
+
+#         cur = slow.next
+#         slow.next = None
+
+#         pre = None
+#         while cur:
+#             t = cur.next
+#             cur.next = pre
+#             pre, cur = cur, t
+#         cur = head
+
+#         while pre:
+#             t = pre.next
+#             pre.next = cur.next
+#             cur.next = pre
+#             cur, pre = pre.next, t
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
