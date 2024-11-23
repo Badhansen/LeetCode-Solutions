1b13ffedbc3daabbd60be1afdf11eaea9e82598a
Badhan Sen <badhan.cse@gmail.com>
2024-11-23 17:29:19 +0000

    Time: 59 ms (55.74%), Space: 20.6 MB (28.03%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
@@ -6,35 +6,59 @@
 #         self.right = None
 
 class Codec:
-
-    def serialize(self, root: Optional[TreeNode]) -> str:
-        res = []
-        def dfs(root):
-            if not root:
-                res.append("N")
-                return
-            res.append(str(root.val))
-            dfs(root.left)
-            dfs(root.right)
-        dfs(root)
-        return ",".join(res)
+    def dfs1(self, root, res):
+        if root is None:
+            res.append('N')
+            return
+        res.append(str(root.val))
+        self.dfs1(root.left, res)
+        self.dfs1(root.right, res)
+    
+    def dfs2(self, pos, values):
         
+        if values[pos[0]] == 'N':
+            pos[0] += 1
+            return None
+        root = TreeNode(int(values[pos[0]]))
+        pos[0] += 1
+        root.left = self.dfs2(pos, values)
+        root.right = self.dfs2(pos, values)
+        return root
+    
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        # res = []
+        # def dfs(root):
+        #     if not root:
+        #         res.append("N")
+        #         return
+        #     res.append(str(root.val))
+        #     dfs(root.left)
+        #     dfs(root.right)
+        # dfs(root)
+        # return ",".join(res)
+        res = []
+        self.dfs1(root, res)
+        return ",".join(res)
 
     def deserialize(self, data: str) -> Optional[TreeNode]:
+        # if not data:
+        #     return None
+        # vals = data.split(",")
+        # self.i = 0
+        # def dfs():
+        #     if vals[self.i] == "N":
+        #         self.i += 1
+        #         return None
+        #     root = TreeNode(int(vals[self.i]))
+        #     self.i += 1
+        #     root.left = dfs()
+        #     root.right = dfs()
+        #     return root
+        # return dfs()
         if not data:
             return None
-        vals = data.split(",")
-        self.i = 0
-        def dfs():
-            if vals[self.i] == "N":
-                self.i += 1
-                return None
-            root = TreeNode(int(vals[self.i]))
-            self.i += 1
-            root.left = dfs()
-            root.right = dfs()
-            return root
-        return dfs()
+        values = data.split(',')
+        return self.dfs2([0], values)
 
 # Your Codec object will be instantiated and called as such:
 # Your Codec object will be instantiated and called as such:
