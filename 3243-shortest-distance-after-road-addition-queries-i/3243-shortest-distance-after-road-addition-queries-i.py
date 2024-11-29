c23a1c24f24a7c433d6f972812caa01e4d89927d
Badhan Sen <badhan.cse@gmail.com>
2024-11-29 01:08:51 +0000

    Time: 59 ms (84.35%), Space: 17.8 MB (41.39%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,24 +1,20 @@
 class Solution:
-    def bfs(self, src, dest, graph):
-        queue = deque([(src, 0)])
-        visited = set([src])
-        while queue:
-            node, distance = queue.popleft()
-            if node == dest:
-                return distance
-            for nei in graph[node]:
-                if nei not in visited:
-                    visited.add(nei)
-                    queue.append((nei, distance + 1))
-        return -1
-            
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
-        answer = []
         for i in range(1, n):
             graph[i - 1].append(i)
+        dist = list(range(n))
+        answer = []
         for u, v in queries:
             graph[u].append(v)
-            res = self.bfs(0, n - 1, graph)
-            answer.append(res)
+            if dist[u] + 1 < dist[v]:
+                dist[v] = dist[u] + 1
+                queue = deque([v])
+                while queue:
+                    src = queue.popleft()
+                    for dest in graph[src]:
+                        if dist[src] + 1 < dist[dest]:
+                            dist[dest] = dist[src] + 1
+                            queue.append(dest)
+            answer.append(dist[n - 1])
         return answer
