b8271d957e0273e34299e9b7592efeac9fabd3c4
Badhan Sen <badhan.cse@gmail.com>
2024-12-06 22:27:06 +0000

    Time: 135 ms (64.20%), Space: 77.7 MB (38.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -8,49 +8,43 @@ class Node:
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = dict()
-        self.head = Node()
-        self.tail = Node()
-        
+        self.dic = dict()
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
         
     def add(self, node):
-        next_node = self.head.next
-        node.next = next_node
-        next_node.prev = node
-        node.prev = self.head
-        self.head.next = node
-        self.cache[node.key] = node
+        prev_end = self.tail.prev
+        prev_end.next = node
+        node.prev = prev_end
+        node.next = self.tail
+        self.tail.prev = node       
         
     def remove(self, node):
-        next_node = node.next
-        prev_node = node.prev
-        prev_node.next = next_node
-        next_node.prev = prev_node
-        del self.cache[node.key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
         
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
+        if key in self.dic:
+            node = self.dic[key]
             self.remove(node)
             self.add(node)
             return node.value
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.cache[key]
+        if key in self.dic:
+            node = self.dic[key]
             self.remove(node)
-        if len(self.cache) == self.capacity:
-            self.remove(self.tail.prev)
+            
         new_node = Node(key, value)
+        self.dic[key] = new_node
         self.add(new_node)
         
-# Time Complexity: O(1) for get and put on average
-# Space Complexity: O(Capacity)
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        if len(self.dic) > self.capacity:
+            del_node = self.head.next
+            self.remove(del_node)
+            del self.dic[del_node.key]
+        
+        
