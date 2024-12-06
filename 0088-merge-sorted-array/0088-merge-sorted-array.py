8c8d1f700d13c45a988fd46f333c6a62c4b2b27a
Badhan Sen <badhan.cse@gmail.com>
2024-12-06 22:49:40 +0000

    Time: 0 ms (100.00%), Space: 17.3 MB (10.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,17 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        idx = m + n - 1
+        end = m + n - 1
         m -= 1
         n -= 1
         while n >= 0:
             if m >= 0 and nums1[m] > nums2[n]:
-                nums1[idx] = nums1[m]
+                nums1[end] = nums1[m]
                 m -= 1
             else:
-                nums1[idx] = nums2[n]
+                nums1[end] = nums2[n]
                 n -= 1
-            idx -= 1
+            end -= 1
             
 
 
