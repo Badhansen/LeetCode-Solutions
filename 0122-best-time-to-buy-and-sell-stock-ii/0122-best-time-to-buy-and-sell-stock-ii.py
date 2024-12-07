e6518b1b886995910061651b0d54d6b16fef2360
Badhan Sen <badhan.cse@gmail.com>
2024-12-07 02:35:11 +0000

    Time: 0 ms (100.00%), Space: 18.5 MB (8.06%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            diff = prices[i] - prices[i - 1]
+            if diff > 0:
+                profit += diff
+        return profit
