681be905cc0d38b8d3627886e3e0b98eb48a77e7
Badhan Sen <badhan.cse@gmail.com>
2024-12-08 01:35:21 +0000

    Time: 0 ms (100.00%), Space: 17.4 MB (7.57%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -1,12 +1,23 @@
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
-        lines = [0] * 102
-        covered = 0
+        # lines = [0] * 102
+        # covered = 0
+        # for s, e in nums:
+        #     lines[s] += 1
+        #     lines[e + 1] -= 1
+        # for i in range(1, 101):
+        #     lines[i] += lines[i - 1]
+        #     if lines[i]:
+        #         covered += 1
+        # return covered
+        nums.sort()
+        res, end = 0, 0
         for s, e in nums:
-            lines[s] += 1
-            lines[e + 1] -= 1
-        for i in range(1, 101):
-            lines[i] += lines[i - 1]
-            if lines[i]:
-                covered += 1
-        return covered
+            if e <= end:
+                continue
+            res += e - s + 1
+            if s <= end:
+                res -= end - s + 1
+            end = e
+        return res
+            
