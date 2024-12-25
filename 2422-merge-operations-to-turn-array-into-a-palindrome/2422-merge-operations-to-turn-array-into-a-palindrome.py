d64ac5ca806d16c08c28fa36417ddbd400528bef
Badhan Sen <badhan.cse@gmail.com>
2024-12-25 09:07:14 +0000

    Time: 50 ms (82.13%), Space: 30.1 MB (45.43%) - LeetHub

--- 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
@@ -1,19 +1,38 @@
 class Solution:
+    # def minimumOperations(self, nums: List[int]) -> int:
+    #     left, right = 0, len(nums) - 1
+    #     res, lsum, rsum = 0, nums[left], nums[right]
+    #     while left < right:
+    #         if lsum < rsum:
+    #             left += 1
+    #             lsum += nums[left]
+    #             res += 1
+    #         elif lsum > rsum:
+    #             right -= 1
+    #             rsum += nums[right]
+    #             res += 1
+    #         else:
+    #             left += 1
+    #             right -= 1
+    #             lsum = nums[left]
+    #             rsum = nums[right]
+    #     return res
+    
     def minimumOperations(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
-        res, lsum, rsum = 0, nums[left], nums[right]
+        res = 0
         while left < right:
-            if lsum < rsum:
-                left += 1
-                lsum += nums[left]
+            lsum, rsum = nums[left], nums[right]
+            while left < right and lsum != rsum:
                 res += 1
-            elif lsum > rsum:
-                right -= 1
-                rsum += nums[right]
-                res += 1
-            else:
+                if lsum < rsum:
+                    left += 1
+                    lsum += nums[left]
+                else:
+                    right -= 1
+                    rsum += nums[right]
+            if lsum == rsum:
                 left += 1
                 right -= 1
-                lsum = nums[left]
-                rsum = nums[right]
+        return res
         return res
