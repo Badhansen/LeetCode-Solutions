8cec87c52dea8a07b06b0ec8cd427b4029e435a1
Badhan Sen <badhan.cse@gmail.com>
2024-12-27 00:09:24 +0000

    Time: 66 ms (34.93%), Space: 17.8 MB (27.63%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        seen = [-1] * 26
+        res, last_starting = 1, 0
+        for i, c in enumerate(s):
+            if seen[ord(c) - ord('a')] >= last_starting:
+                res += 1
+                last_starting = i
+            seen[ord(c) - ord('a')] = i
+        return res
