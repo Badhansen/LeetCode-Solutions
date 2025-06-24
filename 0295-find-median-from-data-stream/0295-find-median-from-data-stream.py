ca4264a9cfe19eef0cdd0b6600dcaf70afe0967c
Badhan Sen <badhan.cse@gmail.com>
2025-06-24 18:11:38 +0100

    Time: 167 ms (33.79%), Space: 39.7 MB (43.66%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,26 +1,21 @@
 class MedianFinder:
-
     def __init__(self):
-        self.left = []    # max heap (invert values to simulate max heap)
-        self.right = []   # min heap
+        self.left = [] # Max heap
+        self.right = [] # Min heap
 
     def addNum(self, num: int) -> None:
-        # Push negative of num to left (max heap)
         heapq.heappush(self.left, -1 * num)
+        n = -1 * heapq.heappop(self.left)
+        heapq.heappush(self.right, n)
+        if len(self.right) > len(self.left):
+            n = heapq.heappop(self.right)
+            heapq.heappush(self.left, -1 * n)
         
-        # Push smallest of left (max heap) to right (min heap)
-        heapq.heappush(self.right, -1 * heapq.heappop(self.left))
-        
-        # Balance heaps
-        if len(self.left) < len(self.right):
-            heapq.heappush(self.left, -1 * heapq.heappop(self.right))
 
     def findMedian(self) -> float:
-        if len(self.left) > len(self.right):
-            return -1 * self.left[0]
-        else:
+        if (len(self.left) + len(self.right)) % 2 == 0:
             return (-1 * self.left[0] + self.right[0]) / 2.0
-        
+        return -1 * self.left[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
