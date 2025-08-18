ba6270b6c5b478fa780bb085f7509cad81d288a6
Badhan Sen <badhan.cse@gmail.com>
2025-08-18 16:54:28 +0100

    Time: 7 ms (83.15%), Space: 17.9 MB (39.69%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,45 @@
 class Solution:
+    def dfs(self, pos, candidates, target, subset, res):
+        if target == 0:
+            res.append(subset[:])
+            return
+        if target < 0:
+            return
+        for i in range(pos, len(candidates)):
+            subset.append(candidates[i])
+            self.dfs(i, candidates, target - candidates[i], subset, res)
+            subset.pop()
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         subset = []
-        def dfs(pos, target):
-            if target == 0:
-                res.append(subset[:])
-                return
-            if target < 0:
-                return
-            
-            for i in range(pos, len(candidates)):
-                subset.append(candidates[i])
-                dfs(i, target - candidates[i])
-                subset.pop()
-            
-        dfs(0, target)
+        self.dfs(0, candidates, target, subset, res)
         return res
+
+''' 
+At each index we have 2 options. Either keep the current element or skip the current element.
+Make a recursion tree of this selection. We can select an element multiple times so one call 
+with same index another call from next index.
+'''
+
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         res = []
+        
+#         def dfs(i, target, subset):
+#             if target == 0:
+#                 res.append(subset)
+#                 return
+#             if target < 0 or i >= len(candidates):
+#                 return
+#             dfs(i, target - candidates[i], subset + [candidates[i]])
+#             dfs(i + 1, target, subset)
+        
+#         dfs(0, target, [])
+#         return res
+
+'''
+Time: O(2^T * K), T = Target value, K = Average length of the combinations
+Space: O(K * x), hypothatical space complexity
+'''
             
                     
