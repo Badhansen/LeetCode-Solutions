2b7e1de6a276e2fbeec0649042f8bdff660abfd9
Badhan Sen <badhan.cse@gmail.com>
2025-08-18 11:32:49 +0100

    Time: 78 ms (27.57%), Space: 18.3 MB (40.39%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,28 +2,26 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        res = []
+        result = []
         for s in strs:
-            res.append(f"{len(s)}#{s}")
-        return "".join(res)
-        
+            result.append(f"{len(s)}#{s}")
+        return "".join(result)
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        res = []
+        result = []
         i = 0
         while i < len(s):
             j = i
-            while s[j] != '#':
+            while s[j] !='#':
                 j += 1
             length = int(s[i:j])
             i = j + 1
             j = i + length
-            res.append(s[i:j])
+            result.append(s[i:j])
             i = j
-        return res
-        
+        return result
 
 
 # Your Codec object will be instantiated and called as such:
